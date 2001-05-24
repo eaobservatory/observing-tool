85e54ddea208921e5b535362eeff40fcc9e15c95
Martin Folger <mfo@roe.ac.uk>
2001-05-24 15:11:42 +0000

    General Constants for the OT.

---
+++ OT/src/ot/OtConstants.java
@@ -0,0 +1,11 @@
+package ot;
+
+/**
+ * Constants for the OT
+ * 
+ * @author Martin Folger (M.Folger@roe.ac.uk)
+ */
+public interface OtConstants {
+  public static final String UKIRT = "ukirt";
+  public static final String JCMT  = "jcmt";
+}
