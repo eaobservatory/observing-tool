49a71405aca26c75e547f344e775a3c0cfd51bc7
Martin Folger <mfo@roe.ac.uk>
2001-05-24 16:22:31 +0000

    Various changes.

--- OT/src/ot/ukirt/iter/editor/IterBiasObsGUI.java
+++ OT/src/ot/ukirt/iter/editor/IterBiasObsGUI.java
@@ -11,7 +11,7 @@ package ot.ukirt.iter.editor;
 
 import java.awt.*;
 import javax.swing.*;
-import jsky.app.ot.gui.TextBoxWidgetExt;
+import jsky.app.ot.gui.*;
 
 public class IterBiasObsGUI extends JPanel {
     GridBagLayout gridBagLayout1 = new GridBagLayout();
@@ -21,6 +21,9 @@ public class IterBiasObsGUI extends JPanel {
     JLabel jLabel4 = new JLabel();
     JComboBox repeatComboBox = new JComboBox();
     TextBoxWidgetExt coadds = new TextBoxWidgetExt();
+  TextBoxWidgetExt exposureTime = new TextBoxWidgetExt();
+  JLabel jLabel5 = new JLabel();
+  JLabel jLabel6 = new JLabel();
 
     public IterBiasObsGUI() {
         try {
@@ -49,18 +52,37 @@ public class IterBiasObsGUI extends JPanel {
         jLabel4.setFont(new java.awt.Font("Dialog", 0, 12));
         repeatComboBox.setPreferredSize(new Dimension(50, 26));
         repeatComboBox.setAutoscrolls(true);
-        coadds.setBorder(BorderFactory.createLoweredBevelBorder());
-        this.add(jLabel2, new GridBagConstraints(2, 2, 1, 1, 0.0, 0.0
+        coadds.setBackground(new Color(204, 204, 204));
+    coadds.setEnabled(false);
+    coadds.setBorder(BorderFactory.createLoweredBevelBorder());
+    coadds.setDisabledTextColor(Color.black);
+    jLabel5.setFont(new java.awt.Font("Dialog", 0, 12));
+    jLabel5.setForeground(Color.black);
+    jLabel5.setText("Exp. Time");
+    jLabel6.setFont(new java.awt.Font("Dialog", 0, 10));
+    jLabel6.setForeground(Color.black);
+    jLabel6.setText("(sec)");
+    exposureTime.setBackground(new Color(204, 204, 204));
+    exposureTime.setEnabled(false);
+    exposureTime.setBorder(BorderFactory.createLoweredBevelBorder());
+    exposureTime.setDisabledTextColor(Color.black);
+    this.add(jLabel2, new GridBagConstraints(2, 5, 1, 1, 0.0, 0.0
             ,GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 0, 0));
-        this.add(jLabel1, new GridBagConstraints(0, 2, 1, 1, 0.0, 0.0
+        this.add(jLabel1, new GridBagConstraints(0, 5, 1, 1, 0.0, 0.0
             ,GridBagConstraints.EAST, GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 0, 0));
-        this.add(jLabel3, new GridBagConstraints(2, 0, 1, 1, 0.0, 0.0
+        this.add(jLabel3, new GridBagConstraints(2, 0, 1, 3, 0.0, 0.0
             ,GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 0, 0));
-        this.add(jLabel4, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0
-            ,GridBagConstraints.EAST, GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 0, 0));
+        this.add(jLabel4, new GridBagConstraints(0, 0, 1, 2, 0.0, 0.0
+            ,GridBagConstraints.EAST, GridBagConstraints.NONE, new Insets(5, 5, 0, 5), 0, 0));
         this.add(repeatComboBox, new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0
-            ,GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(5, 0, 5, 0), 0, 0));
-        this.add(coadds, new GridBagConstraints(1, 2, 1, 1, 0.0, 0.0
+            ,GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(5, 0, 0, 0), 0, 0));
+        this.add(coadds, new GridBagConstraints(1, 5, 1, 1, 0.0, 0.0
             ,GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 0), 0, 0));
+    this.add(exposureTime, new GridBagConstraints(1, 1, 1, 3, 0.0, 0.0
+            ,GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 0), 0, 0));
+    this.add(jLabel5, new GridBagConstraints(0, 2, 1, 2, 0.0, 0.0
+            ,GridBagConstraints.EAST, GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 0, 0));
+    this.add(jLabel6, new GridBagConstraints(2, 3, 1, 1, 0.0, 0.0
+            ,GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 0, 0));
     }
 }
