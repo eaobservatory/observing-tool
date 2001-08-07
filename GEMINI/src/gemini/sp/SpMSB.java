587956c3cf1eaa024275491113bcf453b5ce0e45
Martin Folger <mfo@roe.ac.uk>
2001-08-07 14:52:45 +0000

    OMP changes: priority, status.

--- GEMINI/src/gemini/sp/SpMSB.java
+++ GEMINI/src/gemini/sp/SpMSB.java
@@ -24,5 +24,71 @@ public class SpMSB extends SpObsContextItem {
   protected SpMSB() {
     super(SpType.MSB_FOLDER);
   }
+
+/**
+ * Get the observation priority.
+ */
+public int
+getPriority()
+{
+   String str = _avTable.get(SpObs.ATTR_PRIORITY);
+   if (str == null) {
+      return SpObs.PRIORITY_LOW;
+   }
+
+   for (int i=0; i<SpObs.PRIORITIES.length; ++i) {
+      if (str.equals(SpObs.PRIORITIES[i])) {
+         return i;
+      }
+   }
+   return SpObs.PRIORITY_LOW;
+}
+
+/**
+ * Get the observation priority as a human readable String.
+ */
+public String
+getPriorityString()
+{
+   String str = _avTable.get(SpObs.ATTR_PRIORITY);
+   if (str == null) {
+      return SpObs.PRIORITIES[0];
+   }
+   return str;
+}
+ 
+/**
+ * Set the Observation type.
+ */
+public void
+setPriority(int priority)
+{
+   if ((priority < 0) || (priority > SpObs.PRIORITIES.length)) {
+      return;
+   }
+ 
+   _avTable.set(SpObs.ATTR_PRIORITY, SpObs.PRIORITIES[priority]);
+}
+
+/**
+ * Get status attribute.
+ *
+ * @return country or "" if attribute hasn't been set.
+ */
+public boolean
+isDone()
+{
+   return _avTable.getBool(SpObs.ATTR_STATUS_DONE);
+}
+
+/**
+ * Set status attribute.
+ */
+public void
+setDone(boolean status)
+{
+   _avTable.set(SpObs.ATTR_STATUS_DONE, status);
+}
+
 }
 
