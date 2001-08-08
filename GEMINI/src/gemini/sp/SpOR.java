0d417b5a06a9916dc6238109ea56f03c82f1039c
Martin Folger <mfo@roe.ac.uk>
2001-08-08 14:26:13 +0000

    Number of items attribute added (plus methods)

--- GEMINI/src/gemini/sp/SpOR.java
+++ GEMINI/src/gemini/sp/SpOR.java
@@ -18,11 +18,32 @@ package gemini.sp;
  */
 public class SpOR extends SpObsContextItem {
 
+  /** This attribute records the number of items in the OR folder. */
+  public static final String ATTR_NUMBER_OF_ITEMS = "numberOfItems";
+
+
   /**
    * Default constructor.
    */
   protected SpOR() {
     super(SpType.OR_FOLDER);
+    _avTable.noNotifySet(ATTR_NUMBER_OF_ITEMS, "1", 0);
+  }
+
+  /**
+   * Set number of items in OR folder (such as MSBs, AND folder) that are to be selected.
+   */
+  public void setNumberOfItems(int numberOfItems) {
+   _avTable.set(ATTR_NUMBER_OF_ITEMS, numberOfItems);
+  }
+
+  /**
+   * Get number of items in OR folder (such as MSBs, AND folder) that are to be selected.
+   *
+   * @return number of items (default 1)
+   */
+  public int getNumberOfItems() {
+    return _avTable.getInt(ATTR_NUMBER_OF_ITEMS, 1);
   }
 }
 
