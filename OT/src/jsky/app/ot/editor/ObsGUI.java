08bc6b6694b8a3932d72b61c55d86b0a5ca7914c
Martin Folger <mfo@roe.ac.uk>
2001-08-08 14:38:40 +0000

    estimatedTime text field added.

--- OT/src/jsky/app/ot/editor/ObsGUI.java
+++ OT/src/jsky/app/ot/editor/ObsGUI.java
@@ -10,8 +10,7 @@ package jsky.app.ot.editor;
 
 import java.awt.*;
 import javax.swing.*;
-import jsky.app.ot.gui.TextBoxWidgetExt;
-import jsky.app.ot.gui.CheckBoxWidgetExt;
+import jsky.app.ot.gui.*;
 
 public class ObsGUI extends JPanel {
     GridBagLayout gridBagLayout1 = new GridBagLayout();
@@ -32,6 +31,8 @@ public class ObsGUI extends JPanel {
   JLabel jLabel2 = new JLabel();
   GridBagLayout gridBagLayout2 = new GridBagLayout();
   JLabel obsState = new JLabel();
+  JLabel jLabel3 = new JLabel();
+  TextBoxWidgetExt estimatedTime = new TextBoxWidgetExt();
 
     public ObsGUI() {
         try {
@@ -77,6 +78,10 @@ public class ObsGUI extends JPanel {
     obsState.setFont(new java.awt.Font("Dialog", 0, 12));
     obsState.setForeground(Color.black);
     obsState.setText("Not in Active Database");
+    jLabel3.setFont(new java.awt.Font("Dialog", 0, 12));
+    jLabel3.setForeground(Color.black);
+    jLabel3.setText("Estimated Time");
+    estimatedTime.setEditable(false);
     this.add(jLabel1, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0
             ,GridBagConstraints.EAST, GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 0, 0));
         this.add(obsTitle, new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0
@@ -84,7 +89,7 @@ public class ObsGUI extends JPanel {
 
         // Modified for OMP (MFO, 1 August 2001)
 	if(System.getProperty("OMP") != null) {
-          msbPanel.add(obsDone, new GridBagConstraints(2, 0, 3, 1, 0.0, 0.0
+          msbPanel.add(obsDone, new GridBagConstraints(1, 0, 3, 1, 0.0, 0.0
             ,GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
 	}
 	else {
@@ -101,14 +106,18 @@ public class ObsGUI extends JPanel {
     this.add(msbPanel, new GridBagConstraints(0, 1, 2, 1, 0.0, 0.0
             ,GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
     msbPanel.add(jLabel4, new GridBagConstraints(0, 1, 1, 1, 0.0, 0.0
-            ,GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(5, 5, 5, 10), 0, 0));
-    msbPanel.add(priorityHigh, new GridBagConstraints(1, 1, 2, 1, 0.0, 0.0
+            ,GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(5, 5, 0, 10), 0, 0));
+    msbPanel.add(priorityHigh, new GridBagConstraints(1, 1, 1, 1, 0.0, 0.0
             ,GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
-    msbPanel.add(priorityMedium, new GridBagConstraints(3, 1, 1, 1, 0.0, 0.0
+    msbPanel.add(priorityMedium, new GridBagConstraints(2, 1, 1, 1, 0.0, 0.0
             ,GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
-    msbPanel.add(priorityLow, new GridBagConstraints(4, 1, 1, 1, 1.0, 0.0
+    msbPanel.add(priorityLow, new GridBagConstraints(3, 1, 1, 1, 1.0, 0.0
             ,GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
     msbPanel.add(jLabel2, new GridBagConstraints(0, 0, 2, 1, -1.0, 0.0
-            ,GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(10, 4, 10, 10), 0, 0));
+            ,GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(10, 5, 10, 10), 0, 0));
+    msbPanel.add(jLabel3, new GridBagConstraints(0, 2, 1, 1, 0.0, 0.0
+            ,GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(10, 5, 0, 5), 0, 0));
+    msbPanel.add(estimatedTime, new GridBagConstraints(1, 2, 3, 1, 0.0, 0.0
+            ,GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(15, 0, 5, 5), 0, 0));
     }
 }
