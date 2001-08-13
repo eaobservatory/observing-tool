834dbc59fff141495aa1512c1f544c54081a6484
Martin Folger <mfo@roe.ac.uk>
2001-08-13 15:11:20 +0000

    findDRRecipe and findSiteQuality added. setReferenceIDs applied to SpObs too.

--- ORAC/src/orac/util/SpItemUtilities.java
+++ ORAC/src/orac/util/SpItemUtilities.java
@@ -12,6 +12,7 @@ package orac.util;
 
 import gemini.sp.SpItem;
 import gemini.sp.SpMSB;
+import gemini.sp.SpObs;
 import gemini.sp.SpObsContextItem;
 import gemini.sp.SpAvTable;
 import gemini.sp.SpRootItem;
@@ -25,6 +26,7 @@ import gemini.sp.SpTelescopePos;
 import gemini.sp.obsComp.SpObsComp;
 import gemini.sp.obsComp.SpTelescopeObsComp;
 import java.util.Vector;
+import java.util.Enumeration;
 
 /**
  * Utilities for SpItem trees.
@@ -60,15 +62,15 @@ public class SpItemUtilities {
 
 
   /**
-   * Find SpMSB's, recursively, depth first.
+   * Find SpMSB's and SpObs's recursively and set references.
    */
   public void setReferenceIDs(SpItem spItem) {
     if(spItem instanceof SpObsContextItem) {
-      if(spItem instanceof SpMSB) { 
+      if((spItem instanceof SpMSB) || (spItem instanceof SpObs)) { 
 	_insertReferenceIDsFor(SpTreeMan.findTargetList(spItem), spItem);
 	_insertReferenceIDsFor(SpTreeMan.findInstrument(spItem), spItem);
-	_insertReferenceIDsFor(SpTreeMan.findObsCompSubtype(spItem, SpSiteQualityObsComp.SP_TYPE), spItem);
-	_insertReferenceIDsFor(SpTreeMan.findObsCompSubtype(spItem, SpDRRecipe.SP_TYPE), spItem);
+	_insertReferenceIDsFor(findSiteQuality(spItem), spItem);
+	_insertReferenceIDsFor(findDRRecipe(spItem), spItem);
       }
       else {
         SpItem firstChild;
@@ -117,5 +119,106 @@ public class SpItemUtilities {
       _idCounter++;
     }  
   }
+
+/**
+ * Find the SpSiteQualityObsComp assoicated with this context, if any.  Only
+ * searches the given scope.  It does not navigate the tree hierarchy.
+ */
+public static SpSiteQualityObsComp
+findSiteQualityInContext(SpItem spItem)
+{
+   SpSiteQualityObsComp sqc = null;
+   Enumeration e = spItem.children();
+   while (e.hasMoreElements()) {
+      SpItem child = (SpItem) e.nextElement();
+      if (child instanceof SpSiteQualityObsComp) {
+         sqc = (SpSiteQualityObsComp) child;
+         break;
+      }
+   }
+   return sqc;
+}
+
+/**
+ * Find the SpDRRecipe assoicated with this context, if any.  Only
+ * searches the given scope.  It does not navigate the tree hierarchy.
+ */
+public static SpDRRecipe
+findDRRecipeInContext(SpItem spItem)
+{
+   SpDRRecipe drr = null;
+   Enumeration e = spItem.children();
+   while (e.hasMoreElements()) {
+      SpItem child = (SpItem) e.nextElement();
+      if (child instanceof SpDRRecipe) {
+         drr = (SpDRRecipe) child;
+         break;
+      }
+   }
+   return drr;
+}
+
+/**
+ * Find the site quality component associated with the given scope
+ * scope of the given item.
+ *
+ * @param spItem the SpItem defining the scope to search
+ */
+public static SpSiteQualityObsComp
+findSiteQuality(SpItem spItem)
+{
+   if (spItem instanceof SpSiteQualityObsComp) {
+      return (SpSiteQualityObsComp) spItem;
+   }
+
+   SpItem parent = spItem.parent();
+
+   SpSiteQualityObsComp sqc;
+   if (!(spItem instanceof SpObsContextItem)) {
+      if (parent == null) {
+         return null;
+      }
+      sqc = findSiteQualityInContext(parent);
+   } else {
+      sqc = findSiteQualityInContext(spItem);
+   }
+
+   if ((sqc == null) && (parent != null)) {
+      return findSiteQuality(parent);
+   }
+   return sqc;
+}
+
+/**
+ * Find the DR recipe component associated with the given scope
+ * scope of the given item.
+ *
+ * @param spItem the SpItem defining the scope to search
+ */
+public static SpDRRecipe
+findDRRecipe(SpItem spItem)
+{
+   if (spItem instanceof SpDRRecipe) {
+      return (SpDRRecipe) spItem;
+   }
+
+   SpItem parent = spItem.parent();
+
+   SpDRRecipe drr;
+   if (!(spItem instanceof SpObsContextItem)) {
+      if (parent == null) {
+         return null;
+      }
+      drr = findDRRecipeInContext(parent);
+   } else {
+      drr = findDRRecipeInContext(spItem);
+   }
+
+   if ((drr == null) && (parent != null)) {
+      return findDRRecipe(parent);
+   }
+   return drr;
+}
+  
 }
 
