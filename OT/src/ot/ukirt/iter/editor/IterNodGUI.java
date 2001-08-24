a65c0fe3ea6c3c207a6789632e50ff1498eab988
Martin Folger <mfo@roe.ac.uk>
2001-08-24 16:51:20 +0000

    Editor GUI for Nod iterator component.

---
+++ OT/src/ot/ukirt/iter/editor/IterNodGUI.java
@@ -0,0 +1,44 @@
+// $Id$
+
+/**
+ * Title:        JSky<p>
+ * Description:  <p>
+ * Copyright:    Copyright (c) Allan Brighton<p>
+ * Company:      <p>
+ * @author Allan Brighton (modified for Nod Iterator by Martin Folger)
+ * @version 1.0
+ */
+package ot.ukirt.iter.editor;
+
+import java.awt.*;
+import javax.swing.*;
+import jsky.app.ot.gui.*;
+
+public class IterNodGUI extends JPanel {
+    GridBagLayout gridBagLayout1 = new GridBagLayout();
+  JLabel jLabel5 = new JLabel();
+  DropDownListBoxWidgetExt nodPattern = new DropDownListBoxWidgetExt();
+
+    public IterNodGUI() {
+        try {
+            jbInit();
+        }
+        catch(Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    void jbInit() throws Exception {
+        this.setMinimumSize(new Dimension(280, 278));
+        this.setPreferredSize(new Dimension(280, 278));
+        this.setLayout(gridBagLayout1);
+    jLabel5.setFont(new java.awt.Font("Dialog", 0, 12));
+    jLabel5.setForeground(Color.black);
+    jLabel5.setText("Nod Pattern");
+        nodPattern.setFont(new java.awt.Font("Dialog", 0, 12));
+    this.add(jLabel5, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0
+            ,GridBagConstraints.EAST, GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 0, 0));
+    this.add(nodPattern, new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0
+            ,GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(5, 0, 5, 0), 0, 0));
+    }
+}
