411ac0863df38cf46f05056675aa8344d90022af
Martin Folger <mfo@roe.ac.uk>
2001-11-07 10:44:23 +0000

    Modifications.

--- OT/src/ot/ReportBox.java
+++ OT/src/ot/ReportBox.java
@@ -18,6 +18,7 @@ import javax.swing.text.Style;
 import javax.swing.text.StyleContext;
 import javax.swing.text.StyleConstants;
 import javax.swing.text.rtf.RTFEditorKit;
+import javax.swing.text.JTextComponent;
 import javax.swing.filechooser.FileFilter;
 
 import java.io.PrintWriter;
@@ -27,30 +28,34 @@ import java.io.IOException;
 import java.io.FileOutputStream;
 import java.util.StringTokenizer;
 
-//import orac.util.TextPrint;
-
 /**
  * ReportBox has been rewritten to allow displaying different fonts and using java print routines.
- * TODO: The code is in an experimental stage and needs tidying up.
+ * 
+ * The code still contains some experimental bits which are mostly commented out.
  *
  * @author Martin Folger (M.Folger@roe.ac.uk)
  */
 public class ReportBox extends JFrame {
+  
   protected JPanel jPanel1 = new JPanel();
   protected JButton _dismissButton = new JButton();
   protected JButton _printButton = new JButton();
   protected JButton _saveButton = new JButton();
   protected JScrollPane jScrollPane1 = new JScrollPane();
   protected JTextPane _textPane = new JTextPane();
+
+  /**
+   * This is a referred to the text component whose text should be saved.
+   *
+   * It can be reset in sub classes.
+   */
   protected JFileChooser _fileChooser = new JFileChooser();
 
   protected String _asciiDescription          = "Text Only (*.txt)";
 //  protected String _asciiLineBreakDescription = "Text Only with Line Breaks (*.txt)";
   protected String _richTextFormatDescription = "Rich Text Format (*.rtf)";
-//  protected JPanel jPanel2 = new JPanel();
-//  protected JLabel _label = new JLabel();
 
-  public ReportBox() {
+  protected ReportBox() {
     try {
       jbInit();
     }
@@ -58,7 +63,7 @@ public class ReportBox extends JFrame {
       e.printStackTrace();
     }
 
-    setBounds(100, 100, 480, 640);
+    //setBounds(100, 100, 480, 640);
     setVisible(true);
 
     _fileChooser.addChoosableFileFilter(new FileFilter() {
@@ -75,7 +80,9 @@ public class ReportBox extends JFrame {
         return _asciiDescription;
       }
     });
-/*
+
+/* 
+   // ASCII with kine breaks
    _fileChooser.addChoosableFileFilter(new FileFilter() {
       public boolean accept(File file) {
         if(file.getName().endsWith(".txt")) {
@@ -129,6 +136,17 @@ public class ReportBox extends JFrame {
     //_textPane.setText(message);
     //_textPane.setLineWrap(true);
     _textPane.setEditable(false);
+
+    // Set frame bounds.
+    // Choose frame height depending on the number of lines of the docuument given frame width 480.
+   if(getNumberOfLines(_textPane.getText()) < 34) {
+      // Multiplying the number of lines by 16 and adding 100 gives roughly the right height.
+      setBounds(100, 100, 480, 100 + (getNumberOfLines(_textPane.getText()) * 16));
+    }
+    else {
+      setBounds(100, 100, 480, 640);
+    }
+    pack();
   }
 
   public ReportBox(String message, String title) {
@@ -160,52 +178,33 @@ public class ReportBox extends JFrame {
         saveButton_actionPerformed(e);
       }
     });
-    //_label.setText("Validation Report");
+
     this.getContentPane().add(jPanel1, BorderLayout.SOUTH);
     jPanel1.add(_dismissButton, null);
     jPanel1.add(_printButton, null);
     jPanel1.add(_saveButton, null);
     this.getContentPane().add(jScrollPane1, BorderLayout.CENTER);
-//    this.getContentPane().add(jPanel2, BorderLayout.WEST);
-    //jPanel2.add(_label, null);
     jScrollPane1.getViewport().add(_textPane, null);
   }
 
-//  public void setLabel(JLabel label) {
-//    _label = label;
-//  }
-  
-  public void dismissButton_actionPerformed(ActionEvent e) {
-    dispose();
+
+  public void print() {  
+    JobAttributes  jobAttributes  = new JobAttributes();
+    PageAttributes pageAttributes = new PageAttributes();
+    pageAttributes.setOrigin(PageAttributes.OriginType.PRINTABLE);
+    PrintJob pj = getToolkit().getPrintJob(ReportBox.this, "OT Report (" + System.getProperty("user.name") + ")",
+                                           jobAttributes, pageAttributes);
+    Graphics pg = pj.getGraphics();
+    //pg.setFont(fnt);
+
+
+    _textPane.printAll(pg);
+    pg.dispose(); 
+    pj.end();
   }
 
-  public void printButton_actionPerformed(ActionEvent e) {
-	    JobAttributes  jobAttributes  = new JobAttributes();
-	    PageAttributes pageAttributes = new PageAttributes();
-	    pageAttributes.setOrigin(PageAttributes.OriginType.PRINTABLE);
-	    PrintJob pj = getToolkit().getPrintJob(ReportBox.this, "OT validation report",
-	                                           jobAttributes, pageAttributes);
-	    Graphics pg = pj.getGraphics();
-	    //pg.setFont(fnt);
-	    
-	    
-	    _textPane.printAll(pg); 
-            pg.dispose(); 
-            pj.end();
 
-// ---
-/*  
-    TextPrint textPrint = new TextPrint();
-
-    if(textPrint.init(this)) {
-      textPrint.printMultiLine(_textPane.getText());
-      textPrint.finish();
-    }
-*/    
-  }
-
-  public void saveButton_actionPerformed(ActionEvent e) {
-  
+  public void save() {
     //_fileChooser.setDialogTitle("Save Error/Warning Report");
     
     if(_fileChooser.showSaveDialog(this) != JFileChooser.APPROVE_OPTION) {
@@ -278,5 +277,29 @@ public class ReportBox extends JFrame {
 
   public static void main(String [] args) {
     new ReportBox(args[0]);
+  }
+
+  public static int getNumberOfLines(String string) {
+    int result = 0;
+
+    for(int i = 0; i < string.length(); i++) {
+      if(string.charAt(i) == '\n') {
+        result++;
+      }
+    }
+
+    return result;
+  }
+
+  public void printButton_actionPerformed(ActionEvent e) {
+    print();
+  }
+
+  public void saveButton_actionPerformed(ActionEvent e) {
+    save();
+  }
+
+  public void dismissButton_actionPerformed(ActionEvent e) {
+    dispose();
   }
 }
