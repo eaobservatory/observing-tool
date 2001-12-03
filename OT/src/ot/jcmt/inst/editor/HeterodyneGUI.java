b1639e51aa7a2d06541b98b00eb421c706e68eeb
Martin Folger <mfo@roe.ac.uk>
2001-12-03 11:45:10 +0000

    Frequency Editor turned into separate application, a bit like a plug-in.

--- OT/src/ot/jcmt/inst/editor/HeterodyneGUI.java
+++ OT/src/ot/jcmt/inst/editor/HeterodyneGUI.java
@@ -10,33 +10,43 @@
 
 package ot.jcmt.inst.editor;
 
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JLabel;
 import javax.swing.border.TitledBorder;
-import java.awt.BorderLayout;
+import jsky.app.ot.gui.TableWidgetExt;
 import ot.jcmt.inst.editor.edfreq.FrontEnd;
+import java.awt.*;
+import javax.swing.*;
 
 /**
  * Panel containing frequency editor and data reduction editor.
- * 
+ *
  * @author M.Folger@roe.ac.uk
  */
 public class HeterodyneGUI extends JPanel {
+  BorderLayout borderLayout1 = new BorderLayout();
+  JPanel jPanel1 = new JPanel();
+  JButton editButton = new JButton();
+  FlowLayout flowLayout1 = new FlowLayout();
+  JScrollPane jScrollPane1 = new JScrollPane();
+  JTextPane summaryTextPane = new JTextPane();
 
-  FrontEnd frontEnd = new FrontEnd();
-
-  HeterodyneGUI() { //FrontEnd frontEnd, DataReductionScreen dataReductionScreen) {
-    //FrontEnd frontEnd = new FrontEnd();
-    //frontEnd.setBorder(new TitledBorder("Frequency"));
-
-    //DataReductionScreen dataReductionScreen = new DataReductionScreen();
-    //dataReductionScreen.setBorder(new TitledBorder("Data Reduction"));
-
-    setLayout(new BorderLayout());
-
-    add(new JScrollPane(frontEnd), BorderLayout.CENTER);
-    add(new JLabel("Layout will be tidied up."), BorderLayout.SOUTH);
-    //add(dataReductionScreen, BorderLayout.SOUTH);
+  public HeterodyneGUI() {
+    try {
+      jbInit();
+    }
+    catch(Exception e) {
+      e.printStackTrace();
+    }
   }
+
+  private void jbInit() throws Exception {
+    this.setLayout(borderLayout1);
+    editButton.setText("Edit");
+    jPanel1.setLayout(flowLayout1);
+    flowLayout1.setAlignment(FlowLayout.RIGHT);
+    this.add(jPanel1, BorderLayout.SOUTH);
+    jPanel1.add(editButton, null);
+    this.add(jScrollPane1, BorderLayout.CENTER);
+    jScrollPane1.getViewport().add(summaryTextPane, null);
+  }
+
 }
