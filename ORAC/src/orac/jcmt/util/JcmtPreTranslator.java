437a7c5fd814c6b4ad0d8e2a2c3208764b1f534c
Martin Folger <mfo@roe.ac.uk>
2002-01-10 14:12:07 +0000

    JCMT specific features: XML pre-translation

---
+++ ORAC/src/orac/jcmt/util/JcmtPreTranslator.java
@@ -0,0 +1,37 @@
+/*==============================================================*/
+/*                                                              */
+/*                UK Astronomy Technology Centre                */
+/*                 Royal Observatory, Edinburgh                 */
+/*                 Joint Astronomy Centre, Hilo                 */
+/*                   Copyright (c) PPARC 2001                   */
+/*                                                              */
+/*==============================================================*/
+// $Id$
+
+package orac.jcmt.util;
+
+import orac.util.TcsPreTranslator;
+
+/**
+ * XML PreTranslator for JCMT.
+ *
+ * Applies/removes JCMT specific changes to/from {@link orac.util.SpItemDOM} xml.
+ *
+ * @author Martin Folger
+ */
+public class JcmtPreTranslator extends TcsPreTranslator {
+
+  private static final String [] TCS_TARGET_TYPES      = { "science", "reference" };
+
+  public JcmtPreTranslator() throws Exception { }
+
+  /**
+   * Target types used by the JAC OCS TCS for JCMT XML.
+   *
+   * The target types are "science" and "reference".
+   */
+  protected String [] getTcsTargetTypes() {
+    return TCS_TARGET_TYPES;
+  }
+}
+
