1318ce6e10a1fad0a2d3e539000cdd21167b1aa3
Martin Folger <mfo@roe.ac.uk>
2002-01-11 14:18:12 +0000

    POL iterator for JCMT.

---
+++ ORAC/src/orac/jcmt/iter/SpIterPOL.java
@@ -0,0 +1,89 @@
+/*==============================================================*/
+/*                                                              */
+/*                UK Astronomy Technology Centre                */
+/*                 Royal Observatory, Edinburgh                 */
+/*                 Joint Astronomy Centre, Hilo                 */
+/*                   Copyright (c) PPARC 2000                   */
+/*                                                              */
+/*==============================================================*/
+// $Id$
+
+package orac.jcmt.iter;
+
+import gemini.sp.SpFactory;
+import gemini.sp.SpType;
+import gemini.sp.iter.IterConfigItem;
+import gemini.sp.iter.SpIterConfigObs;
+
+import java.util.*;
+
+// This is basically a crude copy of the UKIRT IRPOL iterator.
+// The hard wired values might need changing. (Added by MFO, 11 January 2002)
+/**
+ * The POL configuration iterator for JCMT (SCUBA).
+ */
+public class SpIterPOL extends SpIterConfigObs {
+
+   public static final SpType SP_TYPE =
+     SpType.create( SpType.ITERATOR_COMPONENT_TYPE, "instPOL", "POL" );
+
+   private IterConfigItem iciWPLAngle;
+
+// Register the prototype.
+   static {
+      SpFactory.registerPrototype( new SpIterPOL() );
+   }
+
+// Hardwire the allowed angles, so that they are accessible to all
+// instruments.
+   static final String ALLOWED_ANGLES[] = {"0", "22.5", "45", "67.5", "90", "135", "157.5", "180.0", "202.5", "225.0", "247.5", "270.0", "292.5", "315.0", "337.5"};
+
+/**
+ * Default constructor.
+ */
+   public SpIterPOL() {
+      super( SP_TYPE );
+   }
+
+/**
+ * Get the name of the item being iterated over.  Subclasses must
+ * define.
+ */
+   public String getItemName() {
+      return "POL";
+   }
+
+/**
+ * Override adding a configuration item to use the no default version.
+ */
+public void
+addConfigItem(IterConfigItem ici, int size)
+{
+
+  super.addConfigItem (ici, size);
+
+  // Then set a default value
+  setConfigStep (ici.attribute, ALLOWED_ANGLES[0], 0);
+
+}
+
+
+/**
+ * Get the array containing the IterConfigItems offered by POL.
+ */
+   public IterConfigItem[] getAvailableItems() {
+
+// Hardwire the allowed angles.
+      IterConfigItem iciWPLAngle = new IterConfigItem(
+        "Waveplate Angle",
+        "POLIter",
+        ALLOWED_ANGLES );
+
+      IterConfigItem[] iciA = {
+         iciWPLAngle
+      };
+
+      return iciA;
+   }
+
+}
