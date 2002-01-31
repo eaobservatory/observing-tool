933f281094ab99903b8c425dbf68d59f5f93d911
Martin Folger <mfo@roe.ac.uk>
2002-01-31 15:32:34 +0000

    JCMT implementation.

--- OT/src/ot/jcmt/inst/editor/DRRecipeGUI.java
+++ OT/src/ot/jcmt/inst/editor/DRRecipeGUI.java
@@ -13,37 +13,33 @@ import java.awt.*;
 import javax.swing.*;
 import jsky.app.ot.gui.*;
 import java.awt.event.*;
+import javax.swing.border.*;
 
 public class DRRecipeGUI extends JPanel {
-  BorderLayout borderLayout1 = new BorderLayout();
-  JPanel jPanel1 = new JPanel();
+  JPanel scubaPanel = new JPanel();
+  CommandButtonWidgetExt scuba_objectSet = new CommandButtonWidgetExt();
   GridBagLayout gridBagLayout1 = new GridBagLayout();
-  JLabel jLabel2 = new JLabel();
-  JLabel jLabel3 = new JLabel();
-  JLabel jLabel4 = new JLabel();
-  JLabel jLabel5 = new JLabel();
-  JLabel jLabel6 = new JLabel();
-  CommandButtonWidgetExt setBias = new CommandButtonWidgetExt();
-  CommandButtonWidgetExt setDark = new CommandButtonWidgetExt();
-  CommandButtonWidgetExt setSky = new CommandButtonWidgetExt();
-  CommandButtonWidgetExt setObject = new CommandButtonWidgetExt();
-  TextBoxWidgetExt biasRecipe = new TextBoxWidgetExt();
-  TextBoxWidgetExt darkRecipe = new TextBoxWidgetExt();
-  TextBoxWidgetExt skyRecipe = new TextBoxWidgetExt();
-  TextBoxWidgetExt objectRecipe = new TextBoxWidgetExt();
-  CheckBoxWidgetExt includeBias = new CheckBoxWidgetExt();
-  CheckBoxWidgetExt includeDark = new CheckBoxWidgetExt();
-  CheckBoxWidgetExt includeSky = new CheckBoxWidgetExt();
-  CheckBoxWidgetExt includeObject = new CheckBoxWidgetExt();
-  JLabel jLabel7 = new JLabel();
+  TextBoxWidgetExt scuba_objectRecipe = new TextBoxWidgetExt();
+  JPanel jPanel1 = new JPanel();
+  BorderLayout borderLayout3 = new BorderLayout();
   JLabel jLabel8 = new JLabel();
+  JLabel jLabel6 = new JLabel();
+  JLabel jLabel2 = new JLabel();
+  JPanel heterodynePanel = new JPanel();
+  CardLayout cardLayout1 = new CardLayout();
+  BorderLayout borderLayout1 = new BorderLayout();
+  JPanel jPanel3 = new JPanel();
+  TitledBorder titledBorder1;
+  JLabel jLabel1 = new JLabel();
+  JPanel jPanel4 = new JPanel();
+  BorderLayout borderLayout2 = new BorderLayout();
+  GridBagLayout gridBagLayout2 = new GridBagLayout();
+  TextBoxWidgetExt scuba_userRecipe = new TextBoxWidgetExt();
   JPanel jPanel2 = new JPanel();
-  CommandButtonWidgetExt userSpecified = new CommandButtonWidgetExt();
-  CommandButtonWidgetExt resetDefaults = new CommandButtonWidgetExt();
-  TextBoxWidgetExt textBoxWidgetExt1 = new TextBoxWidgetExt();
-  FlowLayout flowLayout1 = new FlowLayout();
+  CommandButtonWidgetExt scuba_userSpec = new CommandButtonWidgetExt();
+  CommandButtonWidgetExt scuba_defaultName = new CommandButtonWidgetExt();
   JScrollPane jScrollPane1 = new JScrollPane();
-  TableWidgetExt tableWidgetExt1 = new TableWidgetExt();
+  TableWidgetExt scuba_recipeTable = new TableWidgetExt();
 
   public DRRecipeGUI() {
     try {
@@ -55,106 +51,55 @@ public class DRRecipeGUI extends JPanel {
   }
 
   private void jbInit() throws Exception {
-    this.setLayout(borderLayout1);
+    titledBorder1 = new TitledBorder("");
+    this.setLayout(cardLayout1);
+    scubaPanel.setLayout(borderLayout3);
+    scuba_objectSet.setText("Set");
     jPanel1.setLayout(gridBagLayout1);
-    jLabel2.setFont(new java.awt.Font("Dialog", 0, 12));
-    jLabel2.setForeground(Color.black);
-    jLabel2.setText("Observation Type");
-    jLabel3.setFont(new java.awt.Font("Dialog", 0, 12));
-    jLabel3.setForeground(Color.black);
-    jLabel3.setText("BIAS");
-    jLabel4.setFont(new java.awt.Font("Dialog", 0, 12));
-    jLabel4.setForeground(Color.black);
-    jLabel4.setText("DARK");
-    jLabel5.setFont(new java.awt.Font("Dialog", 0, 12));
-    jLabel5.setForeground(Color.black);
-    jLabel5.setText("SKY");
-    jLabel6.setFont(new java.awt.Font("Dialog", 0, 12));
-    jLabel6.setForeground(Color.black);
-    jLabel6.setText("OBJECT");
-    setBias.setText("Set");
-    setDark.setText("Set");
-    setSky.setText("Set");
-    setObject.setText("Set");
-    biasRecipe.addActionListener(new java.awt.event.ActionListener() {
-
-      public void actionPerformed(ActionEvent e) {
-        biasRecipe_actionPerformed(e);
-      }
-    });
-    objectRecipe.addActionListener(new java.awt.event.ActionListener() {
-
-      public void actionPerformed(ActionEvent e) {
-        objectRecipe_actionPerformed(e);
-      }
-    });
-    jLabel7.setFont(new java.awt.Font("Dialog", 0, 12));
-    jLabel7.setForeground(Color.black);
-    jLabel7.setText("Include in Group?");
     jLabel8.setFont(new java.awt.Font("Dialog", 0, 12));
     jLabel8.setForeground(Color.black);
     jLabel8.setText("Recipe Name");
-    userSpecified.setText("User Specified:");
-    resetDefaults.setText("Reset Defaults");
-    textBoxWidgetExt1.setEditable(false);
-    textBoxWidgetExt1.setText("ENTER_YOUR_OWN_RECIPE_HERE");
-    jPanel2.setLayout(flowLayout1);
-    flowLayout1.setAlignment(FlowLayout.LEFT);
-    jPanel2.setMinimumSize(new Dimension(260, 60));
-    //jPanel2.setPreferredSize(new Dimension(471, 60));
-    //jScrollPane1.setPreferredSize(new Dimension(453, 200));
-    this.add(jPanel1, BorderLayout.NORTH);
-    jPanel1.add(jLabel6, new GridBagConstraints(0, 5, 1, 1, 0.0, 0.0
-            ,GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 15, 0));
-    jPanel1.add(setBias, new GridBagConstraints(1, 2, 1, 1, 0.0, 0.0
-            ,GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 17), 0, 0));
-    jPanel1.add(biasRecipe, new GridBagConstraints(2, 2, 1, 1, 0.0, 0.0
-            ,GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 0), 52, 0));
-    jPanel1.add(darkRecipe, new GridBagConstraints(2, 3, 1, 1, 0.0, 0.0
-            ,GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 0), 42, 0));
-    jPanel1.add(skyRecipe, new GridBagConstraints(2, 4, 1, 1, 0.0, 0.0
-            ,GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 0), 0, 0));
-    jPanel1.add(objectRecipe, new GridBagConstraints(2, 5, 1, 1, 0.0, 0.0
-            ,GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 0), 0, 0));
-    jPanel1.add(setDark, new GridBagConstraints(1, 3, 1, 1, 0.0, 0.0
-            ,GridBagConstraints.SOUTHWEST, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
-    jPanel1.add(setSky, new GridBagConstraints(1, 4, 1, 1, 0.0, 0.0
-            ,GridBagConstraints.SOUTHWEST, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
-    jPanel1.add(setObject, new GridBagConstraints(1, 5, 1, 1, 0.0, 0.0
-            ,GridBagConstraints.SOUTHWEST, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
-    jPanel1.add(includeBias, new GridBagConstraints(3, 2, 1, 1, 0.0, 0.0
-            ,GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
-    jPanel1.add(includeDark, new GridBagConstraints(3, 3, 1, 1, 0.0, 0.0
-            ,GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
-    jPanel1.add(includeSky, new GridBagConstraints(3, 4, 1, 1, 0.0, 0.0
-            ,GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
-    jPanel1.add(includeObject, new GridBagConstraints(3, 5, 1, 1, 0.0, 0.0
-            ,GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
-    jPanel1.add(jLabel7, new GridBagConstraints(3, 1, 1, 1, 0.0, 0.0
-            ,GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
-    jPanel1.add(jLabel8, new GridBagConstraints(2, 1, 1, 1, 1.0, 0.0
-            ,GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(0, -2, 0, 27), 0, 0));
-    jPanel1.add(jLabel2, new GridBagConstraints(0, 1, 2, 1, 0.0, 0.0
-            ,GridBagConstraints.SOUTH, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
-    jPanel1.add(jLabel3, new GridBagConstraints(0, 2, 1, 1, 0.0, 0.0
-            ,GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
-    jPanel1.add(jLabel4, new GridBagConstraints(0, 3, 1, 1, 0.0, 0.0
-            ,GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
-    jPanel1.add(jLabel5, new GridBagConstraints(0, 4, 1, 1, 0.0, 0.0
-            ,GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
-    this.add(jPanel2, BorderLayout.CENTER);
-    jPanel2.add(resetDefaults, null);
-    jPanel2.add(userSpecified, null);
-    jPanel2.add(textBoxWidgetExt1, null);
-    this.add(jScrollPane1, BorderLayout.SOUTH);
-    jScrollPane1.add(tableWidgetExt1, null);
-  }
-
-  void biasRecipe_actionPerformed(ActionEvent e) {
-
-  }
-
-  void objectRecipe_actionPerformed(ActionEvent e) {
-
+    jLabel6.setFont(new java.awt.Font("Dialog", 0, 12));
+    jLabel6.setForeground(Color.black);
+    jLabel6.setText("OBJECT");
+    jLabel2.setFont(new java.awt.Font("Dialog", 0, 12));
+    jLabel2.setForeground(Color.black);
+    jLabel2.setText("Observation Type");
+    heterodynePanel.setLayout(borderLayout1);
+    jPanel3.setBorder(new TitledBorder(BorderFactory.createLineBorder(new Color(153, 153, 153),2),"ORAC DR ACSIS"));
+    jLabel1.setFont(new java.awt.Font("Dialog", 0, 12));
+    jLabel1.setForeground(Color.black);
+    jLabel1.setText("To be defined.");
+    jPanel4.setLayout(borderLayout2);
+    scuba_userRecipe.setEditable(false);
+    scuba_userRecipe.setText("ENTER_YOUR_OWN_RECIPE_HERE");
+    jPanel2.setLayout(gridBagLayout2);
+    scuba_userSpec.setText("User Specified:");
+    scuba_defaultName.setText("Default");
+    this.add(scubaPanel, "scuba");
+    scubaPanel.add(jPanel1, BorderLayout.NORTH);
+    jPanel1.add(jLabel6, new GridBagConstraints(0, 1, 1, 1, 0.0, 0.0
+            ,GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 15, 0));
+    jPanel1.add(scuba_objectRecipe, new GridBagConstraints(2, 1, 1, 1, 0.0, 0.0
+            ,GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(5, 5, 5, 5), 0, 0));
+    jPanel1.add(scuba_objectSet, new GridBagConstraints(1, 1, 1, 1, 0.0, 0.0
+            ,GridBagConstraints.SOUTHWEST, GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 0, 0));
+    jPanel1.add(jLabel8, new GridBagConstraints(2, 0, 1, 1, 1.0, 0.0
+            ,GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 0, 0));
+    jPanel1.add(jLabel2, new GridBagConstraints(0, 0, 2, 1, 0.0, 0.0
+            ,GridBagConstraints.SOUTHWEST, GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 0, 0));
+    scubaPanel.add(jPanel4, BorderLayout.CENTER);
+    jPanel4.add(jPanel2, BorderLayout.NORTH);
+    jPanel2.add(scuba_defaultName, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0
+            ,GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 0, 0));
+    jPanel2.add(scuba_userSpec, new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0
+            ,GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 0, 0));
+    jPanel2.add(scuba_userRecipe, new GridBagConstraints(2, 0, 1, 1, 1.0, 0.0
+            ,GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL, new Insets(5, 5, 5, 5), 0, 0));
+    jPanel4.add(jScrollPane1, BorderLayout.CENTER);
+    jScrollPane1.getViewport().add(scuba_recipeTable, null);
+    this.add(heterodynePanel, "heterodyne");
+    heterodynePanel.add(jPanel3, BorderLayout.NORTH);
+    jPanel3.add(jLabel1, null);
   }
 }
--- OT/src/ot/jcmt/inst/editor/EdDRRecipe.java
+++ OT/src/ot/jcmt/inst/editor/EdDRRecipe.java
@@ -11,6 +11,7 @@ package ot.jcmt.inst.editor;
 
 import gemini.sp.*;
 import gemini.sp.obsComp.*;
+import orac.jcmt.inst.SpInstHeterodyne;
 import jsky.app.ot.gui.KeyPressWatcher;
 import jsky.app.ot.gui.TextBoxWidgetExt;
 import jsky.app.ot.gui.TextBoxWidgetWatcher;
@@ -21,57 +22,413 @@ import jsky.app.ot.gui.CommandButtonWidgetWatcher;
 import jsky.app.ot.gui.CheckBoxWidgetExt;
 import jsky.app.ot.gui.CheckBoxWidgetWatcher;
 //import jsky.app.ot.gui.util.ErrorBox;
+import ot.util.DialogUtil;
 
 //import ot_ukirt.util.*;
 import jsky.app.ot.editor.OtItemEditor;
-import orac.ukirt.inst.SpDRRecipe;
-import ot.jcmt.inst.editor.DataReductionScreen;
+import orac.jcmt.inst.SpDRRecipe;
+import orac.util.LookUpTable;
 
-import java.awt.Event;
+import java.awt.event.KeyEvent;
+import java.awt.CardLayout;
 import java.util.Vector;
 
 // MFO: Preliminary. Get rid of GUI stuff inside this class.
 import javax.swing.border.TitledBorder;
-import javax.swing.JPanel;
+import javax.swing.JComponent;
 import java.awt.BorderLayout;
 
 
 
 /**
  * This is the editor for the DRRecipe item.
- *
- * This class should be re-written. There should be a DRRecipeGUI class that contains the orac DR GUI
- * (currently DRRecipeGUI) and the DataReductionScreen.
  */
-public final class EdDRRecipe extends OtItemEditor {
-// MFO SWING empty shell so far
+public final class EdDRRecipe extends OtItemEditor
+                    implements KeyPressWatcher, TextBoxWidgetWatcher, TableWidgetWatcher {
 
-    //private DRRecipeGUI _w;		// the GUI layout
-    private javax.swing.JPanel _w;		// the GUI layout
+  private static final String INST_STR_SCUBA      = "scuba";
+  private static final String INST_STR_HETERODYNE = "heterodyne";
+
+  private SpDRRecipe  _spDRRecipe;
+  private String      _currentRecipeSelected;
+  private String      _instStr;
+
+  private DRRecipeGUI _w;		// the GUI layout
+
+  private DataReductionScreen _dataReductionScreen = new DataReductionScreen();
 
 
-    /**
-     * The constructor initializes the title, description, and presentation source.
-     */
-    public EdDRRecipe() {
-	_title       ="DR Recipe";
+/**
+ * The constructor initializes the title, description, and presentation source.
+ */
+public EdDRRecipe()
+{
+   _title       = "DR Recipe";
+   _presSource  = _w = new DRRecipeGUI();;
+   _description = "Enter the Data Reduction recipe to be used"; // for each observation type";
+   _resizable   = true;
 
-        // MFO: Preliminary. Get rid of GUI stuff inside this class.
-	DRRecipeGUI         drRecipeGUI         = new DRRecipeGUI();
-	DataReductionScreen dataReductionScreen = new DataReductionScreen();
+   _dataReductionScreen.setBorder(new TitledBorder("ACSIS DR"));
+   _w.heterodynePanel.add(_dataReductionScreen, BorderLayout.CENTER);
+}
 
-        drRecipeGUI.setBorder(new TitledBorder("ORAC DR"));
-        dataReductionScreen.setBorder(new TitledBorder("ACSIS DR"));
+/**
+ * This method initializes the widgets in the presentation to reflect the
+ * current values of the items attributes.
+ * Don't do anything in init at the moment.  The initialisation requires a  reference
+ * to the instrument component. This isn't known until after setup is invoked, which
+ * is after init.  Therefore do the initialisation as part of setup (see initInstWidgets).
+ */
+protected void
+_init()
+{
 
-	javax.swing.JPanel panel = new javax.swing.JPanel();
-	panel.setLayout(new java.awt.BorderLayout());
-	panel.add(drRecipeGUI, java.awt.BorderLayout.NORTH);
-	panel.add(dataReductionScreen, java.awt.BorderLayout.CENTER);
-	
-	_presSource  = _w = panel; //new DRRecipeGUI();
-	_description ="The DR Recipe is configured with this component.";
+
+}
+
+/**
+ * This method initializes the instrument speific widgets in the presentation 
+ * to reflect the current values of the items attributes and set the watchers.
+ */
+protected void
+_initInstWidgets()
+{
+
+   CommandButtonWidgetExt cbw = null;
+   CheckBoxWidgetExt ckbw = null;
+
+   SpInstObsComp inst = ((SpInstObsComp) SpTreeMan.findInstrument(_spDRRecipe));
+
+   if(inst == null) {
+     // MFO: "empty" is hard-wired in DRRecipeGUI (as constraint strings of the
+     // respective panels managed my the CardLayout of DRRecipeGUI).
+     // Might not be elegant but is done in a similar way with instrument specific panels
+     // (see below).  
+     ((CardLayout)(_w.getLayout())).show(_w, "empty");
+    
+     DialogUtil.error(_w, "Can't identify instrument: probably none in scope?");
+     return;
+   }
+
+
+   if(inst instanceof SpInstHeterodyne) {
+     _instStr = INST_STR_HETERODYNE;
+   }
+   else {
+     _instStr = INST_STR_SCUBA;
+   }  
+
+   System.out.println ("Inst is "+_instStr);
+
+   // MFO: inst.type().getReadable() is hard-wired in DRRecipeGUI (as constraint strings of the
+   // respective panels managed my the CardLayout of DRRecipeGUI).
+   // Might not be elegant but has always been hard-wired in a similar way.
+   ((CardLayout)(_w.getLayout())).show(_w, _instStr.toLowerCase());
+     
+
+   if(_instStr.equalsIgnoreCase(INST_STR_HETERODYNE)) {
+     return;
+   }
+
+  // The recipes
+  TextBoxWidgetExt rtbw; 
+
+  rtbw = (TextBoxWidgetExt) getWidget(_instStr, "objectRecipe");
+  // Disable it so it the user cannot use it.
+  // _disableRecipeEntry(true);
+  rtbw.setEditable(false);
+  cbw = (CommandButtonWidgetExt) getWidget(_instStr, "objectSet");
+  cbw.addWatcher( new CommandButtonWidgetWatcher() {
+    public void commandButtonAction(CommandButtonWidgetExt cbw) {
+      // Set the selected table value
+      _spDRRecipe.setObjectRecipeName(_currentRecipeSelected);
+      _spDRRecipe.setTitleAttr(_currentRecipeSelected);
+  
+      TextBoxWidgetExt tbwe = (TextBoxWidgetExt) getWidget(_instStr, "objectRecipe");
+      tbwe.setText(_currentRecipeSelected);
+      _disableRecipeEntry(true);
+      }
+  });
+
+  // The table of possible recipes
+  TableWidgetExt twe;
+  twe = (TableWidgetExt) getWidget(_instStr, "recipeTable");
+  twe.setColumnHeaders(new String[]{"Recipe Name", "Description"});
+  twe.addWatcher(this);
+
+  // Button to allow user to enter own names
+  TextBoxWidgetExt tbwe = (TextBoxWidgetExt) getWidget(_instStr, "userRecipe");
+  tbwe.setText("ENTER_YOUR_OWN_RECIPE_HERE");
+  _disableRecipeEntry(true);
+  tbwe.addWatcher(this);
+
+  cbw = (CommandButtonWidgetExt) getWidget(_instStr, "userSpec");
+  cbw.addWatcher (new CommandButtonWidgetWatcher() {
+      public void commandButtonAction (CommandButtonWidgetExt cbw) {
+	_disableRecipeEntry(false);
+      }
+  });
+
+  // button to reset the recipe to default
+  cbw = (CommandButtonWidgetExt) getWidget(_instStr, "defaultName");
+  cbw.addWatcher( new CommandButtonWidgetWatcher() {
+    public void commandButtonAction(CommandButtonWidgetExt cbw) {
+      _spDRRecipe.useDefaults(_instStr);
+      _currentRecipeSelected = "QUICK_LOOK";
+      _updateWidgets();
+    }
+  });
+}
+
+private void
+_disableRecipeEntry (boolean tf) {
+
+   TextBoxWidgetExt tbwe = (TextBoxWidgetExt) getWidget(_instStr, "userRecipe");
+   tbwe.setEditable(!tf);
+}
+
+/**
+ * Initialize the Recipe table widget according to the selected
+ * recipe category.
+ */
+private void
+_showRecipeType(LookUpTable recipes)
+{
+   Vector[] rowsV = new Vector[recipes.getNumRows()];
+   rowsV = recipes.getAsVectorArray();
+   TableWidgetExt tw = (TableWidgetExt) getWidget(_instStr, "recipeTable");
+   tw.setRows(rowsV);
+}
+
+/**
+ * Get the index of the recipe in the given array, or -1 if the recipe
+ * isn't in the array.
+ */
+   private int _getRecipeIndex( String recipe, LookUpTable rarray ) {
+      int ri = -1;
+      try {
+         ri = rarray.indexInColumn( recipe, 0 );
+      } catch (Exception ex) {
+      }
+      return ri;
+   }
+
+/**
+ * Update the recipe choice related widgets.
+ */
+private void
+_updateRecipeWidgets()
+{
+  String recipe = null;
+  String instStr;
+
+   SpInstObsComp inst = ((SpInstObsComp) SpTreeMan.findInstrument(_spDRRecipe));
+
+   if(inst == null) {
+     // MFO: "empty" is hard-wired in DRRecipeGUI (as constraint strings of the
+     // respective panels managed my the CardLayout of DRRecipeGUI).
+     // Might not be elegant but is done in a similar way with instrument specific panels
+     // (see below).  
+     ((CardLayout)(_w.getLayout())).show(_w, "empty");
+    
+     System.out.println ("No instrument found in scope");
+     return;
+   }
+
+
+   if(inst instanceof SpInstHeterodyne) {
+     instStr = INST_STR_HETERODYNE;
+   }
+   else {
+     instStr = INST_STR_SCUBA;
+   }  
+
+//   System.out.println ("Inst is "+instStr);
+
+   // MFO: inst.type().getReadable() is hard-wired in DRRecipeGUI (as constraint strings of the
+   // respective panels managed my the CardLayout of DRRecipeGUI).
+   // Might not be elegant but has always been hard-wired in a similar way.
+   ((CardLayout)(_w.getLayout())).show(_w, instStr.toLowerCase());
+     
+
+   if(instStr.equalsIgnoreCase(INST_STR_HETERODYNE)) {
+     return;
+   }
+
+  // First fill in the text box.
+  TextBoxWidgetExt tbwe;
+  CheckBoxWidgetExt cbwe;
+
+  tbwe = (TextBoxWidgetExt) getWidget(_instStr, "objectRecipe");
+  try {
+    recipe = _spDRRecipe.getObjectRecipeName();
+    tbwe.setValue(recipe);
+  }catch (NullPointerException ex) { }
+  
+  // See which type of recipe the selected recipe is, if any.
+  // Get the instrument and display the relevant options.
+  // Then look for the recipe in these options. If its there highlight it.
+
+  // What I really need to do is introduce imaging/spec capabilities into
+  // instruments which I can then get.  Imager-spectrometers will be a 
+  // special case
+
+   LookUpTable  rarray = null;
+   int index = -1;
+
+   if (instStr.equalsIgnoreCase(INST_STR_SCUBA)) {
+
+     rarray = SpDRRecipe.SCUBA;
+	   
+   }else if (instStr.equalsIgnoreCase(INST_STR_HETERODYNE)) {
+
+     rarray = SpDRRecipe.HETERODYNE;
+
+   }
+   
+
+   // Show the correct recipes, and select the option widget for the type
+   _showRecipeType(rarray);
+
+}
+
+/**
+ * Override setup to store away a reference to the SpDRRecipe item. Also initialise the widgets
+ * Here.
+ */
+public void
+setup(SpItem spItem)
+{
+  _spDRRecipe = (SpDRRecipe) spItem;
+  _initInstWidgets();
+   super.setup(spItem);
+}
+
+/**
+ * Implements the _updateWidgets method from OtItemEditor in order to
+ * setup the widgets to show the current values of the item.
+ */
+protected void
+_updateWidgets()
+{
+   _updateRecipeWidgets();
+
+}
+
+/**
+ * A key has been pressed in the text box widget.
+ * @see KeyPressWatcher
+ */
+   public void keyPressed( KeyEvent evt ) {
+     System.out.println( "In kp" );
+
+   }
+
+/**
+ * Watch changes to the text box.
+ * @see TextBoxWidgetWatcher
+ */
+public void
+textBoxKeyPress(TextBoxWidgetExt tbwe)
+{
+  // This is the watcher entered hen the user types a character in a text box
+  // widget, Currently the user specified recipe is the only such widget.
+  _currentRecipeSelected = tbwe.getText().trim();
+
+}
+ 
+/**
+ * Text box action, ignore.
+ * @see TextBoxWidgetWatcher
+ */
+   public void textBoxAction( TextBoxWidgetExt tbwe ) {
+     System.out.println ( "In tba" );
+   }
+
+
+public void
+tableRowSelected(TableWidgetExt twe, int rowIndex)
+{
+   _currentRecipeSelected = (String) twe.getCell(0, rowIndex);
+   String _defaultRecipe = "QUICK_LOOK";
+
+// Allow for blank lines and headings.  The latter is defined to contain
+// at least two lowercase letters or any space (testing for a colon
+// might also be useful).  Merely substitute the default recipe.   
+   if ( _currentRecipeSelected.length() == 0 ) {
+      _currentRecipeSelected = _defaultRecipe;
+
+   } else {
+      int count = 0;
+      for ( int i = 0; i < _currentRecipeSelected.length(); i++ ) {
+          if ( Character.isWhitespace( _currentRecipeSelected.charAt( i ) ) ) {
+             _currentRecipeSelected = _defaultRecipe;
+             break;
+
+          } else if ( Character.isLowerCase( _currentRecipeSelected.charAt( i ) ) ) {
+             count++;
+             if ( count > 1 ) {
+                _currentRecipeSelected = _defaultRecipe;
+                break;
+             }
+          }
+       }
     }
 
+   // Don't set the value if the new selection is the same as the old
+   // (otherwise, we'd fool the OT into thinking a change had been made)
+//    String curValue = _spDRRecipe.getRecipeName();
+//    if ((curValue != null) && (curValue.equals(recipe))) {
+//        //     System.out.println ("New same as old");
+//       return;
+//    }
 
-  protected void _updateWidgets() { }
+}
+
+/**
+ * Must watch table widget actions as part of the TableWidgetWatcher
+ * interface, but don't care about them.
+ */
+   public void tableAction( TableWidgetExt twe, int colIndex, int rowIndex ) {}
+
+   /**
+    * @see #getWidget(java.lang.String)
+    */
+   protected JComponent getWidget(String instrument, String widgetName) {
+     return getWidget(instrument.toLowerCase() + "_" + widgetName);
+   }
+
+   /**
+    * Helper method.
+    *
+    * In EdDRRecipe the widgets of the associated GUI class cannot as easily be referred to directly
+    * as in the subclasses of EdCompInstBase because the widget names in the freebongo version often comprised an
+    * instrument part and a paramater part (e.g. _instStr, "flatInGroup"). In freebongo something like this is possible
+    * because one gets hold of a widget by calling the getWidget(String) method on the presentation. If this method is
+    * called with a String that refers to a widget that does not exist (e.g. "IRCAM3.flatInGroup") then a
+    * NullPointerException occurres which can be called and ignored. This was used in the freebongo version of this class
+    * to handle every widget for every instrument. Whenever a widget was addressed in the context of an instrument whose GUI
+    * panel does not have this widget that a NullPointerException occurrs and is ignored. To use a similar approach in
+    * the swing version of the OT this helper method getWidget has been introduced in the EdDRRecipe class.
+    *
+    * ('_' are used instead of '.' to separate instrument name from widget name.)
+    * 
+    * @return widget in DRRecipeGUI or null if specified widget does not exist.
+    */
+   protected JComponent getWidget(String widgetName) {
+     try {
+       return (JComponent)(_w.getClass().getDeclaredField(widgetName).get(_w));
+     }
+     catch(NoSuchFieldException e) {
+       if((System.getProperty("DEBUG") != null) && System.getProperty("DEBUG").equalsIgnoreCase("ON")) {
+         System.out.println ("Could not find widget / component \"" + widgetName + "\".");
+       }
+
+       return null;
+     }
+     catch(Exception e) {
+       e.printStackTrace();
+       return null;
+     }
+   }
 }
