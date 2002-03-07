2981669a11a6fe9f2505be5e714b8778acc5ad7c
Martin Folger <mfo@roe.ac.uk>
2002-03-07 11:41:02 +0000

    Exception thrown during storeProgram.

---
+++ OMP/src/omp/SpChangedOnDiskException.java
@@ -0,0 +1,24 @@
+/*==============================================================*/
+/*                                                              */
+/*                UK Astronomy Technology Centre                */
+/*                 Royal Observatory, Edinburgh                 */
+/*                 Joint Astronomy Centre, Hilo                 */
+/*                   Copyright (c) PPARC 2001                   */
+/*                                                              */
+/*==============================================================*/
+// $Id$
+
+package omp;
+
+/**
+ * This Exception is thrown when the Server throws a Server.SpChangedOnDisk
+ * Exception during storeProgram.
+ * 
+ * @author Martin Folger (M.Folger@roe.ac.uk)
+ */
+public class SpChangedOnDiskException extends Exception {
+  SpChangedOnDiskException(String message) {
+    super(message);
+  }
+}
+
