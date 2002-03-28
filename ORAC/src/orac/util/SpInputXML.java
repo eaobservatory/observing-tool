329aa257cf3f776ceee511a12534197ed0d5ffbd
Martin Folger <mfo@roe.ac.uk>
2002-03-28 17:17:40 +0000

    Check for SpItemDOM based XML moved added.

--- ORAC/src/orac/util/SpInputXML.java
+++ ORAC/src/orac/util/SpInputXML.java
@@ -19,6 +19,8 @@ import gemini.sp.SpTreeMan;
 import java.io.Reader;
 import java.io.StringReader;
 import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
 import javax.swing.event.DocumentListener;
 import javax.swing.event.DocumentEvent;
 import javax.xml.parsers.SAXParserFactory;
@@ -66,6 +68,13 @@ import org.xml.sax.InputSource;
  * @author Martin Folger (M.Folger@roe.ac.uk)
  */
 public class SpInputXML extends DefaultHandler {
+
+  /**
+   * Used to check whether XML format is old (SpItemDOM based) or new.
+   *
+   * First 200 characters of XML file are read into this buffer to check format.
+   */
+  private static char [] _xmlBuffer = new char[200];
 
   public static final String XML_ATTR_TYPE    = "type";
   public static final String XML_ATTR_SUBTYPE = "subtype";
@@ -206,6 +215,20 @@ public class SpInputXML extends DefaultHandler {
   }
 
   public SpItem xmlToSpItem(Reader reader) throws Exception {
+  
+    // A BufferedReader is only need to reset the stream
+    // inside isOldXmlFormat(BufferedReader). Once there
+    // are no Science Programs in the old XML format in
+    // circulation anymore things like
+    // isOldXmlFormat, SpItemDOM can go and the normal
+    // Reader can be used instead of the BufferedReader.
+    BufferedReader bufferedReader = new BufferedReader(reader);
+    if(isOldXmlFormat(bufferedReader)) {
+      System.out.println("Converting from old XML format.");
+      return (new SpItemDOM(bufferedReader)).getSpItem();
+    }
+
+  
     XMLReader xmlReader = SAXParserFactory.newInstance().newSAXParser().getXMLReader();
 
     xmlReader.setContentHandler(this);
@@ -213,6 +236,33 @@ public class SpInputXML extends DefaultHandler {
     xmlReader.parse(new InputSource(reader));
 
     return _currentSpItem;
+  }
+
+  /**
+   * Checks whether XML format is old (based on SpItemDOM conversion).
+   *
+   * @param reader buffered reader to read XML.
+   *
+   * @return true if file contains old XML format (based on SpItemDOM conversion)
+   */
+  public static boolean isOldXmlFormat(BufferedReader reader) {
+
+    try {
+      reader.mark(_xmlBuffer.length + 10);
+      reader.read(_xmlBuffer, 0, _xmlBuffer.length);
+      reader.reset();
+
+      String xmlStart = new String(_xmlBuffer);
+
+      if((xmlStart != null) && (xmlStart.indexOf("ItemData") >= 0)) {
+        return true;
+      }
+    }
+    catch(IOException e) {
+      System.out.println("Problem while checking XML format: " + e + ". Using new XML format.");
+    }
+    
+    return false;
   }
 
   /** Test method. */
