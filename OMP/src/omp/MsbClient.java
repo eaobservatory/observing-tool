6ca2062feb9f4bbbc954f4c912c1da413de9916f
Martin Folger <mfo@roe.ac.uk>
2002-04-11 14:15:06 +0000

    Latest version of this file from Matthew Rippa (JAC).

--- OMP/src/omp/MsbClient.java
+++ OMP/src/omp/MsbClient.java
@@ -1,62 +1,139 @@
 package omp;
 
+import java.io.*;
+import java.net.*;
+import gemini.sp.SpItem;
+import gemini.sp.SpProg;
+import orac.util.SpItemDOM;
+import orac.util.SpItemUtilities;
+import orac.util.SpInputXML;
+
 import java.lang.reflect.*;
-import java.net.URL;
+import orac.util.*;
 
 /**
  * MsbClient.java
  *
+ * This is a utility class used to send SOAP messages to the MsbServer.
  *
  * Created: Mon Aug 27 18:30:31 2001
  *
- * @author Mathew Rippa, modified by Martin Folger
- * @version
+ * @author <a href="mailto:mrippa@jach.hawaii.edu">Mathew Rippa</a>,
+ * modified by Martin Folger
+
+ * $Id$ 
  */
 public class MsbClient extends SoapClient {
 
-   public MsbClient () {
-      // default url
-      endpoint = "http://www.jach.hawaii.edu/JAClocal/cgi-bin/msbsrv.pl";
-      soapAction = "urn:OMP::MSBServer";
-      //object_uri = "http://www.jach.hawaii.edu/OMP::MSBServer";
-
-      // reset to specified url if there is one
-      if(System.getProperty("MSB_URL") != null) {
-         endpoint = System.getProperty("MSB_URL");
-      }
-
+  /**
+   * <code>queryMSB</code>
+   * Perform a query to the MsbServer with the given query
+   * String. Success will return a true value and write the msbSummary
+   * xml to file.
+   *
+   * @param xmlQueryString a <code>String</code> value. The xml representing the query.
+   * @return a <code>boolean</code> value indicating success.
+   */
+  public static boolean queryMSB(String xmlQueryString) {
       try {
-	 url = new URL(endpoint);
-      }catch(Exception e) {
-	 e.printStackTrace();
+	 URL url = new URL(System.getProperty("msbServer"));
+	 flushParameter();
+	 addParameter("xmlquery", String.class, xmlQueryString);
+
+	 FileWriter fw = new FileWriter(System.getProperty("msbSummary"));
+	 Object tmp = doCall(url, "urn:OMP::MSBServer", "queryMSB");
+
+	 if (tmp != null ) {
+	   fw.write( (String)tmp );
+	   fw.close();
+	 }
+
+	 else 
+	   return false;
+	 
+      } catch (Exception e) {
+	e.printStackTrace();
+	return false;
       }
+      return true;
+
    }
 
-   public static void main(String[] args) {
-      MsbClient msbc = new MsbClient();
-      msbc.queryMSB("<Query><Moon>Dark</Moon></Query>");
-      msbc.fetchMSB(new Integer(96));
-      SpClient spc = new SpClient();
-   }
+  /**
+   * <code>fetchMSB</code> Fetch the msb indicated by the msbid. The
+   * SpItem will return null on failure. In the future this should
+   * throw an exception instead.
+   *
+   * @param msbid an <code>Integer</code> value of the MSB
+   * @return a <code>SpItem</code> value representing the MSB.
+   */
+  public static SpItem fetchMSB(Integer msbid) {
 
-   public void queryMSB(String xmlQueryString) {
-      addParameter("xmlquery", String.class, xmlQueryString);
-      addParameter("maxreturn", Integer.class, new Integer(2));
+     SpItem spItem = null;
       try {
-        System.out.println(doCall(url, "queryMSB"));
+	System.out.println(""+msbid);
+	
+	URL url = new URL(System.getProperty("msbServer"));
+	flushParameter();
+	addParameter("key", Integer.class, msbid);
+	
+	FileWriter fw = new FileWriter(System.getProperty("msbFile"));
+	String spXML = (String) doCall(url, "urn:OMP::MSBServer", "fetchMSB");
+	
+	if (spXML != null ) {
+	  StringReader r = new StringReader(spXML);
+	  spItem = (SpItem)(new SpInputXML()).xmlToSpItem(r);
+	  
+	  fw.write( (String)spXML );
+	  fw.close();
+	}
+	
+	else 
+	  return spItem;
+	
+      } catch (Exception e) {
+	e.printStackTrace();
+	return spItem;
       }
-      catch(Exception e) {
-        e.printStackTrace();
-      }
+      
+      return spItem;
    }
 
-   public void fetchMSB(Integer msbid) {
-      addParameter("key", Integer.class, msbid);
-      try {
-        System.out.println(doCall(url, "fetchMSB"));
+  /**
+   * <code>doneMSB</code> Mark the given project ID as done in the database.
+   *
+   * @param projID a <code>String</code> the project ID.
+   * @param checksum a <code>String</code> the checksum for this project.
+   */
+  public static void doneMSB(String projID, String checksum) {
+    try {
+	
+      URL url = new URL(System.getProperty("msbServer"));
+      flushParameter();
+      addParameter("projID", String.class, projID);
+      addParameter("checksum", String.class, checksum);
+
+      Object tmp = doCall(url, "urn:OMP::MSBServer", "doneMSB");
+
+      if (tmp != null ) {
+	// tmp has something with success
       }
-      catch(Exception e) {
-        e.printStackTrace();
-      }	
-   }
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return;
+  }
+
+
+  /**
+   * Describe <code>main</code> method here.
+   *
+   * @param args a <code>String[]</code> value
+   */
+  public static void main(String[] args) {
+    MsbClient.queryMSB("<Query><Moon>Dark</Moon></Query>");
+    MsbClient.fetchMSB(new Integer(96));
+  }
+
 }
--- OMP/src/omp/SoapClient.java
+++ OMP/src/omp/SoapClient.java
@@ -1,12 +1,15 @@
 package omp;
 
-
 import java.io.*;
 import java.lang.reflect.*;
 import java.net.URL;
 import java.util.Vector;
+import javax.swing.JOptionPane;
 import org.apache.soap.*;
+import org.apache.soap.encoding.SOAPMappingRegistry;
+import org.apache.soap.encoding.literalxml.XMLParameterSerializer;
 import org.apache.soap.rpc.*;
+import org.apache.soap.util.xml.QName;
 
 
 /**
@@ -15,70 +18,115 @@ import org.apache.soap.rpc.*;
  *
  * Created: Mon Aug 27 18:30:31 2001
  *
- * @author Mathew Rippa, modified by Martin Folger
- * @version
+ * @author <a href="mailto:mrippa@jach.hawaii.edu">Mathew Rippa</a>,
+ * modified by Martin Folger
+ *
+ * $Id$ 
  */
 public class SoapClient {
 
-   URL url = null;
+  private static Header header = null;
+  private static Vector params = new Vector();
    
-   // set in subclasses
-   protected String endpoint;
-   protected String soapAction;
-   //protected String object_uri;
+  /**
+   * <code>addParameter</code>. Add a Parameter to the next Call that
+   * is to take place.
+   *
+   * @param name a <code>String</code> value. The name of the
+   * parameter to be added to next soap Call.
 
-   private Header header = null;
-   private Vector params = new Vector();
+   * @param type a <code>Class</code> value. The explicit Class of
+   * this parameter.
 
-   protected void addParameter(String name, Class type, Object val) {
-      params.add(new Parameter(name, type, val, null));
-   }
+   * @param val an <code>Object</code> value. The object to register this Parameter with.
+   */
+  protected static void addParameter(String name, Class type, Object val) {
+    params.add(new Parameter(name, type, val, null));
+  }
+    
+  /**
+   * <code>flushParameter</code> Clear the Vector of
+   * <code>Parameters</code> Objects to be sent in the next Call.
+   * 
+   */
+  protected static void flushParameter() {
+    params.clear();
+  }
+    
+  /**
+   * <code>doCall</code> Send the Call with various configurations.
+   *
+   * @param url an <code>URL</code> val indicating the soap server to
+   * connect to.
+   * @param soapAction a <code>String</code>. The URN like
+   * "urn:OMP::MSBServer"
+   * @param methodName a <code>String</code> The name of the method to
+   * call in the server.
+   * @return an <code>Object</code> returned by the method called in
+   * the server .
+   */
+  protected static Object doCall(URL url, String soapAction, String methodName)  {
+    //String result = "";
+    Object obj = new Object();
 
-   protected Object doCall(URL url, String methodName) throws Exception {
-      Object result = null;
-      //try {
-	 Call call = new Call();
-	 call.setTargetObjectURI(soapAction);
-	 call.setEncodingStyleURI(Constants.NS_URI_SOAP_ENC);
-	 call.setMethodName(methodName);
-	 call.setParams(params);
-	 if (header != null)
-	    call.setHeader(header);
+    //Next 3 statements for getting a struct?
+    //SOAPMappingRegistry smr = new SOAPMappingRegistry();
+    //XMLParameterSerializer xmlSer = new XMLParameterSerializer();
+    //smr.mapTypes(Constants.NS_URI_SOAP_ENC,new
+    //	  QName("http://www.w3.org/1999/XMLSchema", "Struct"),
+    //	    org.w3c.dom.Element.class, xmlSer, xmlSer);
+       
+    try {
+      Call call = new Call();
 
-	 soapAction +="#" + methodName;
-	 
-	 Response resp = call.invoke(url, soapAction);
-      
-	 // check response 
-	 if (!resp.generatedFault()) {
-	    Parameter ret = resp.getReturnValue();
-	    result = ret.getValue();
-	 }
-	 else {
-	    Fault fault = resp.getFault ();
+      //The next line with the above code to get a struct
+      //call.setSOAPMappingRegistry(smr);
+
+      call.setTargetObjectURI(soapAction);
+      call.setEncodingStyleURI(Constants.NS_URI_SOAP_ENC);
+      call.setMethodName(methodName);
+      call.setParams(params);
+      if (header != null)
+	call.setHeader(header);
+      soapAction +="#" + methodName;
+
+      //Let's dump the envelope
+      //Envelope env = call.buildEnvelope();
+      //System.out.println(env.toString());
 	    
-	    // Check for "SOAP-ENV:Server.SpChangedOnDisk" fault code does not seem to work at the
-	    // moment. Check for SpChangedOnDisk substring in fault.toString() instead. (MFO)
-	    // if(fault.getFaultCode().equals("SOAP-ENV:Server.SpChangedOnDisk")) {
-	    if(fault.toString().indexOf("SpChangedOnDisk") >= 0) {
-	       throw new SpChangedOnDiskException(fault.getFaultString());
-	    }
+      Response resp = call.invoke(url, soapAction);
 
-	    // proper Exceptions will be implemented later.
-	    throw new Exception(fault.getFaultString());
-	    //System.err.println (methodName + " generated fault: ");
-	    //System.out.println ("  Fault Code   = " + fault.getFaultCode());  
-	    //System.out.println ("  Fault String = " + fault.getFaultString());
-	 }
-      
-      //} catch (Exception e) {
-      //  e.printStackTrace();
-      //}
+      // check response 
+      if (!resp.generatedFault()) {
 
-      //Reset the params vector.
-      params.clear();
+	Parameter ret = resp.getReturnValue();
+	//result = (String) ret.getValue();
+	obj = ret.getValue();
 
-      return result;
-   }
-} // SoapClient
+	//Reset the params vector.
+	params.clear();
+	
+	//return result;
+	return obj;
+
+      }
+      else {
+	Fault fault = resp.getFault ();
+	JOptionPane.showMessageDialog(null,
+				      "Code:    "+fault.getFaultCode()+"\n" + 
+				      "Problem: "+fault.getFaultString(), 
+				      "Error Message",
+				      JOptionPane.ERROR_MESSAGE);
+
+	//System.out.println ("  Fault Code   = " + fault.getFaultCode());  
+	//System.out.println ("  Fault String = " + fault.getFaultString());
+      }
+	    
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+	
+    return null;
+  }
+}
 
