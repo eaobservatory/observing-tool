3d552e771fcb3f88c949dc52f18e8f9fcc203db1
Martin Folger <mfo@roe.ac.uk>
2002-09-06 13:07:20 +0000

    BandSpec changes: array with numbers of hybrid subbands per subsystem added.

--- EDFREQ/src/edfreq/ReceiverList.java
+++ EDFREQ/src/edfreq/ReceiverList.java
@@ -28,45 +28,54 @@ public class ReceiverList
 
       r = new Receiver ( "HARP-B", 325.0E9, 375.0E9, 5.0E9, 2.0E9 );
 
-      r.bandspecs.add ( new BandSpec ( "1-system", 1, new double[] { 0.25E9, 1.0E9 }, new int[] { 8192, 2048 } ) );
+      r.bandspecs.add ( new BandSpec ( "1-system", 1, new double[] { 0.25E9, 0.5E9, 1.0E9, 2.0E9 },     // band widths
+                                                      new int[]    {   8192,  8192,  2048,  2048 },     // channels
+                                                      new int[]    {      1,     2,     1,     2 } ) ); // hybrid sub bands
       r.bandspecs.add ( new BandSpec ( "2-system", 2, new double[] { 0.25E9, 1.0E9 }, new int[] { 4096, 1024 } ) );
-      r.bandspecs.add ( new BandSpec ( "hybrid", 1,   new double[] { 0.5E9,  2.0E9 }, new int[] { 8192, 2048 } ) );
 
       receivers.put ( "HARP-B", r );
 
 
       r = new Receiver ( "A3", 215.0E9, 272.0E9, 4.0E9, 1.8E9 );
 
+      r.bandspecs.add ( new BandSpec ( "1-system", 1, new double[] { 1.0E9,  4.0E9 },
+						      new int[]    { 32768,   8192 },
+						      new int[]    {     4,      4 }) );
       r.bandspecs.add ( new BandSpec ( "4-system", 4, new double[] { 0.25E9, 1.0E9 }, new int[] {  8192, 2048 } ) );
       r.bandspecs.add ( new BandSpec ( "8-system", 8, new double[] { 0.25E9, 1.0E9 }, new int[] {  4096, 1024 } ) );
-      r.bandspecs.add ( new BandSpec ( "hybrid", 1,   new double[] { 1.0E9,  4.0E9 }, new int[] { 32768, 8192 } ) );
 
       receivers.put ( "A3", r );
 
 
       r = new Receiver ( "B3", 322.0E9, 373.0E9, 4.0E9, 1.8E9 );
 
+      r.bandspecs.add ( new BandSpec ( "1-system", 1, new double[] { 1.0E9,  4.0E9 },
+						      new int[]    { 32768,   8192 },
+						      new int[]    {     4,      4 }) );
       r.bandspecs.add ( new BandSpec ( "4-system", 4, new double[] { 0.25E9, 1.0E9 }, new int[] {  8192, 2048 } ) );
       r.bandspecs.add ( new BandSpec ( "8-system", 8, new double[] { 0.25E9, 1.0E9 }, new int[] {  4096, 1024 } ) );
-      r.bandspecs.add ( new BandSpec ( "hybrid", 1,   new double[] { 1.0E9,  4.0E9 }, new int[] { 32768, 8192 } ) );
 
       receivers.put ( "B3", r );
 
 
       r = new Receiver ( "WC", 430.0E9, 510.0E9, 4.0E9, 1.8E9 );
 
+      r.bandspecs.add ( new BandSpec ( "1-system", 1, new double[] { 1.0E9,  4.0E9 },
+						      new int[]    { 32768,   8192 },
+						      new int[]    {     4,      4 }) );
       r.bandspecs.add ( new BandSpec ( "4-system", 4, new double[] { 0.25E9, 1.0E9 }, new int[] {  8192, 2048 } ) );
       r.bandspecs.add ( new BandSpec ( "8-system", 8, new double[] { 0.25E9, 1.0E9 }, new int[] {  4096, 1024 } ) );
-      r.bandspecs.add ( new BandSpec ( "hybrid", 1,   new double[] { 1.0E9,  4.0E9 }, new int[] { 32768, 8192 } ) );
 
       receivers.put ( "WC", r );
 
 
       r = new Receiver ( "WD", 630.0E9, 710.0E9, 4.0E9, 1.8E9 );
 
+      r.bandspecs.add ( new BandSpec ( "1-system", 1, new double[] { 1.0E9,  4.0E9 },
+						      new int[]    { 32768,   8192 },
+						      new int[]    {     4,      4 }) );
       r.bandspecs.add ( new BandSpec ( "4-system", 4, new double[] { 0.25E9, 1.0E9 }, new int[] {  8192, 2048 } ) );
       r.bandspecs.add ( new BandSpec ( "8-system", 8, new double[] { 0.25E9, 1.0E9 }, new int[] {  4096, 1024 } ) );
-      r.bandspecs.add ( new BandSpec ( "hybrid", 1,   new double[] { 1.0E9,  4.0E9 }, new int[] { 32768, 8192 } ) );
 
       receivers.put ( "WD", r );
 
