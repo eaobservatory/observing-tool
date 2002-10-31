686b28a09b0326f8c97b730dd70c2f291897958e
Martin Folger <mfo@roe.ac.uk>
2002-10-31 17:05:16 +0000

    Major changes in connection with introduction of the HeterodyneEditor interface: all save methods have been removed. The update methods of the HeterodyneEditor are called instead when the FrequencyTable changes.

--- EDFREQ/src/edfreq/FrequencyTable.java
+++ EDFREQ/src/edfreq/FrequencyTable.java
@@ -19,38 +19,41 @@ import javax.swing.table.*;
 
 import java.util.Vector;
 
-import javax.xml.parsers.SAXParserFactory;
-import org.xml.sax.helpers.DefaultHandler;
-import org.xml.sax.XMLReader;
-import org.xml.sax.Attributes;
-import org.xml.sax.SAXException;
-import org.xml.sax.InputSource;
 
 /**
  * @author Dennis Kelly ( bdk@roe.ac.uk ), modified by Martin Folger (M.Folger@roe.ac.uk)
  */
-public class FrequencyTable extends JPanel implements FrequencyEditorConstants
+public class FrequencyTable extends JPanel implements ActionListener
 {
 
    private double lLowLimit;
    private double lHighLimit;
    private double uLowLimit;
    private double uHighLimit;
-   Object[] samplers;
-   Object[][] data;
+   private Sampler[] samplers;
+   private Object[][] data;
 
-   /** Parser for XML input/output. (MFO, 29 November 2001) */
-   private XMLReader _xmlReader = null;
+   private JButton [] lineButtons;
+
+   private SideBandDisplay sideBandDisplay;
+   private HeterodyneEditor hetEditor;
+   private EmissionLines emissionLines;
+
+   private boolean _notifyHeterodyneEditor = true;
 
    public FrequencyTable ( double feIF, double feBandWidth,
      double [] bandWidths, int [] channels, 
      int samplerCount, int displayWidth,
      SideBandDisplay sideBandDisplay,
-     FrontEnd frontEnd ) 
+     HeterodyneEditor hetEditor,
+     EmissionLines emissionLines)
    {
 
       super ( );
 
+      this.sideBandDisplay = sideBandDisplay;
+      this.hetEditor         = hetEditor;
+      this.emissionLines   = emissionLines;
 
       JPanel[] columns = new JPanel[6];
 
@@ -74,6 +77,7 @@ public class FrequencyTable extends JPanel implements FrequencyEditorConstants
       setLayout ( new BoxLayout ( this, BoxLayout.X_AXIS ) );
 
       JLabel lsbTitle = new JLabel ( "LSB", SwingConstants.CENTER );
+      JLabel lineTitle = new JLabel(" Line", SwingConstants.CENTER);
       JLabel loTitle = new JLabel ( "IF", SwingConstants.CENTER );
       JLabel bwTitle = new JLabel ( "BW", SwingConstants.CENTER );
       JLabel resTitle = new JLabel ( "Res (KHz)", SwingConstants.CENTER );
@@ -87,6 +91,7 @@ public class FrequencyTable extends JPanel implements FrequencyEditorConstants
       }
 
       columns[0].add ( lsbTitle );
+      columns[1].add ( lineTitle );
       columns[2].add ( loTitle );
       columns[3].add ( bwTitle );
       columns[4].add ( resTitle );
@@ -124,9 +129,9 @@ public class FrequencyTable extends JPanel implements FrequencyEditorConstants
 /* Create the samplers and sidebands and their associated displays */
 
       data = new Object [samplerCount][3];
-      samplers = new Object [samplerCount];
+      samplers = new Sampler [samplerCount];
 
-      frontEnd.clearSamplerList();
+      lineButtons = new JButton[samplerCount];
 
       for ( j=0; j<samplerCount; j++ )
       {
@@ -144,27 +149,43 @@ public class FrequencyTable extends JPanel implements FrequencyEditorConstants
            (int)( gigToPix * uHighLimit ) - 20 );
          highBar.setUnitIncrement ( 1 );
 
+         // Line display added by MFO (October 16, 2002)
+         if(j == 0) {
+            lineButtons[j] = new JButton("See Heterodyne Editor");
+            lineButtons[j].setEnabled(false);
+	 }
+	 else {
+            lineButtons[j] = new JButton(HeterodyneEditor.NO_LINE);
+	 }
+
+	 lineButtons[j].setForeground(Color.black);
+         lineButtons[j].setFont(new java.awt.Font("Dialog", 0, 10));
+	 lineButtons[j].setActionCommand("" + j);
+	 lineButtons[j].addActionListener(this);
+
+
          samplerDisplay = new SamplerDisplay ( String.valueOf ( feIF ) );
          resolutionDisplay = new ResolutionDisplay ( channels[0],
            bandWidths[0] );
 	 Vector bandWidthItems = new Vector();
 	 for(int k = 0; k < bandWidths.length; k++) {
-            bandWidthItems.add("" + bandWidths[k] * 1.0E-9);
+            bandWidthItems.add("" + (Math.rint(bandWidths[k] * 1.0E-6) / 1000.0));
 	 }
          widthChoice = new JComboBox ( bandWidthItems );
+	 widthChoice.addItemListener(new NumberedBandWidthListener(j));
          samplers[j] = new Sampler ( feIF, bandWidths, channels, widthChoice );
-	 frontEnd.addToSamplerList( (Sampler)samplers[j] );
 
          data[j][0] = new SideBand ( lLowLimit, lHighLimit, 
            bandWidths[0], -feIF, (Sampler)samplers[j], 
-           lowBar, gigToPix );
+           lowBar, gigToPix, emissionLines );
          data[j][1] = samplers[j];
          data[j][2] = new SideBand ( uLowLimit, uHighLimit, 
            bandWidths[0], feIF, (Sampler)samplers[j],
-            highBar, gigToPix );
-
+            highBar, gigToPix, emissionLines );
          ((Sampler)samplers[j]).addSamplerWatcher ( 
            (SamplerWatcher)data[j][0] );
+
+
          ((Sampler)samplers[j]).addSamplerWatcher ( 
            (SamplerWatcher)samplerDisplay );
          ((Sampler)samplers[j]).addSamplerWatcher ( 
@@ -172,183 +193,145 @@ public class FrequencyTable extends JPanel implements FrequencyEditorConstants
          ((Sampler)samplers[j]).addSamplerWatcher ( 
            (SamplerWatcher)data[j][2] );
 
+         NumberedSideBandListener numberedSideBandListener = new NumberedSideBandListener(j);
+	 lowBar.addMouseListener(numberedSideBandListener);
+	 highBar.addMouseListener(numberedSideBandListener);
+	 ((SideBand)data[j][0]).addAdjustmentListener(numberedSideBandListener);
+	 
+
          columns[0].add ( lowBar );
+         columns[1].add ( lineButtons[j] );
          columns[2].add ( samplerDisplay );
          columns[3].add ( widthChoice );
          columns[4].add ( resolutionDisplay );
          columns[5].add ( highBar );
       }
 
+
       // Added by MFO (8 January 2002)
       // Only the top pair of SideBands need to have references of
-      // SideBandDisplay and FrontEnd because when one of them is changed
+      // SideBandDisplay and HeterodyneEditor because when one of them is changed
       // the LO1 needs adjusting, depending on whether "usb" or "lsb"
       // has been selected.
-      ((SideBand)data[0][0]).connectTopSideBand(sideBandDisplay, frontEnd);
-      ((SideBand)data[0][2]).connectTopSideBand(sideBandDisplay, frontEnd);
+      ((SideBand)data[0][0]).connectTopSideBand(sideBandDisplay, hetEditor);
+      ((SideBand)data[0][2]).connectTopSideBand(sideBandDisplay, hetEditor);
+   }
 
 
-      // Initialize parser (MFO, 29 November 2001)
-      try {
-         _xmlReader = SAXParserFactory.newInstance().newSAXParser().getXMLReader();
-	 
-	 _xmlReader.setContentHandler(new DefaultHandler() {
-	 
-            public void startElement(String namespaceURI,
-                                     String localName,
-                                     String qName,
-                                     Attributes atts) throws SAXException {
-               if(qName.equals(XML_ELEMENT_BANDSYSTEM)) {
-                  // ignore count attribute
-	          return;
-	       }
+   public Sampler [] getSamplers()
+   {
+      return samplers;   
+   }
 
-               if(qName.equals(XML_ELEMENT_SUBSYSTEM)) {
-	          int i                  = Integer.parseInt(atts.getValue(XML_ATTRIBUTE_BANDNUM));
-	          double centreFrequency = Double.parseDouble(atts.getValue(XML_ATTRIBUTE_CENTRE_FREQUENCY));
-	          double bandWidth       = Double.parseDouble(atts.getValue(XML_ATTRIBUTE_BANDWIDTH));
-                  SideBand sideband = (SideBand)data[i][0];
+   public void setLineText(String lineText, int subsystem) {
+      lineButtons[subsystem].setText(lineText);
+   }
 
-                  sideband.setSubBandCentre(centreFrequency);
-                  sideband.setSubBandWidth(bandWidth);
-	          sideband.updateCentreFrequency();
-                  // channels (-1) is not used
-	          sideband.updateSamplerValues(centreFrequency, bandWidth, 0);
-	          //sideband.updateScrollBarValue();
-               
-	          ((Sampler)samplers[i]).setBandWidthAndGui(atts.getValue(XML_ATTRIBUTE_BANDWIDTH));
-               }	 
+
+   public void actionPerformed(ActionEvent e)
+   {
+      int i = Integer.parseInt(e.getActionCommand());
+
+      // emissionLines.getSelectedLine() can be null if no line has been selected.
+      LineDetails lineDetails = emissionLines.getSelectedLine();
+
+      if(lineDetails != null) {
+
+         double obsFrequency = (lineDetails.frequency * 1.0E6) / 
+              ( 1.0 + hetEditor.getRedshift() );
+
+         double centreFrequency = Math.abs(obsFrequency - sideBandDisplay.getLO1());
+
+         samplers[i].setCentreFrequency(centreFrequency);
+
+         hetEditor.updateLineDetails(lineDetails, i);
+         hetEditor.updateCentreFrequency(centreFrequency, i);
+
+
+         if(lineDetails != null) {
+            lineButtons[i].setText( lineDetails.name       + "  " +
+                                    lineDetails.transition + "  " +
+                                    lineDetails.frequency);
+
+            lineButtons[i].setToolTipText(lineButtons[i].getText());
+         }
+         else {
+            lineButtons[i].setText(HeterodyneEditor.NO_LINE);
+         }
+      }
+   }
+
+
+   protected void lo1Changed() {
+      // Skip first button
+      for(int i = 1; i < lineButtons.length; i++) {
+         lineButtons[i].setText(HeterodyneEditor.NO_LINE);
+      }
+   }
+
+   class NumberedBandWidthListener implements ItemListener {
+      private int _number;
+
+      NumberedBandWidthListener(int number) {
+         _number = number;
+      }
+
+      public void itemStateChanged(ItemEvent e) {
+         hetEditor.updateBandWidth(samplers[_number].getBandWidth(), _number);
+         hetEditor.updateChannels(samplers[_number].getChannels(), _number);
+      }
+   }
+
+   class NumberedSideBandListener implements MouseListener, AdjustmentListener {
+      private int _number;
+      private boolean _mousePressed = false;
+
+      NumberedSideBandListener(int number) {
+         _number = number;
+      }
+
+      private void _processAdjustment(boolean isRightMouseButton) {
+         hetEditor.updateCentreFrequency(samplers[_number].getCentreFrequency(), _number);
+
+         boolean lineClamped = (_number == 0) && (!isRightMouseButton);
+
+         if(_number == 0) {
+	    if(lineClamped) {
+               // Skip top subsystem because it is clamped and keeps its line.
+               // All other subsystem loose their lines.
+               for(int i = 1; i < samplers.length; i++) {
+                  hetEditor.updateLineDetails(null, i);
+                  lineButtons[i].setText(HeterodyneEditor.NO_LINE);
+               }
 	    }
-         });
-      }
-      //catch(SAXException e) { //ParserConfigurationException e) {
-      catch(Exception e) { //ParserConfigurationException e) {
-          JOptionPane.showMessageDialog(this,
-	                                "Could not initialize XMLReader: " + e,
-					"XMLReader problem.", JOptionPane.ERROR_MESSAGE);
-
-      }
-
-   }
-
-   /**
-    * Not used anymore.
-    *
-    * @deprecated replaced by {@link #toXML()}.
-    */
-   public void saveAsASCII ( PrintWriter out )
-   {
-      int j;
-
-      out.println ( "  <bandSystem" );
-
-      out.println ( "    count=" + samplers.length );
-      out.println ( "  >" );
-
-      for ( j=0; j<samplers.length; j++ )
-      {
-         out.println ( "    <subSystem" );
-
-         out.println ( "      bandNum=" + j );
-         out.println ( "      centreFrequency=" +
-           ((Sampler)samplers[j]).getCentreFrequency() );
-         out.println ( "      bandWidth=" + 
-           ((Sampler)samplers[j]).getBandWidth() );
-         out.println ( "      channels=" + 
-           ((Sampler)samplers[j]).getChannels() );
-
-         out.println ( "    />" );
-      }
-      out.println ( "  </bandSystem>" );
-   }
-
-
-   public String toXML()
-   {
-      StringBuffer stringBuffer = new StringBuffer();
-   
-      int j;
-
-      stringBuffer.append ( "  <" + XML_ELEMENT_BANDSYSTEM);
-
-      stringBuffer.append ( " " + XML_ATTRIBUTE_COUNT + "=\"" + samplers.length + "\">\n");
-
-      for ( j=0; j<samplers.length; j++ )
-      {
-         stringBuffer.append ( "    <" + XML_ELEMENT_SUBSYSTEM);
-
-         stringBuffer.append ( " " + XML_ATTRIBUTE_BANDNUM + "=\"" + j + "\"");
-         stringBuffer.append ( " " + XML_ATTRIBUTE_CENTRE_FREQUENCY + "=\"" +
-           ((Sampler)samplers[j]).getCentreFrequency() + "\"");
-         stringBuffer.append ( " " + XML_ATTRIBUTE_BANDWIDTH + "=\"" + 
-           ((Sampler)samplers[j]).getBandWidth() + "\"");
-         stringBuffer.append ( " " + XML_ATTRIBUTE_CHANNELS + "=\"" + 
-           ((Sampler)samplers[j]).getChannels() + "\"/>\n" );
-
-      }
-      stringBuffer.append ( "  </" + XML_ELEMENT_BANDSYSTEM + ">" );
-   
-      return stringBuffer.toString();
-   }
-
-   public String toConfigXML(String restFrequencyId, int sideband, String dataReductionXML, String indent) {
-
-      StringBuffer xmlBuffer = new StringBuffer();
-
-      for(int i = 0; i < samplers.length; i++) {
-         xmlBuffer.append(spectralWindowToXML(i, restFrequencyId, sideband, dataReductionXML, indent) + "\n");
+	    else {
+               hetEditor.updateLineDetails(null, _number);
+            }
+         }
+         else {
+            hetEditor.updateLineDetails(null, _number);
+	    lineButtons[_number].setText(HeterodyneEditor.NO_LINE);
+	 }
       }
 
-      return xmlBuffer.toString();
+      public void mouseClicked(MouseEvent e)  { }
+      public void mouseEntered(MouseEvent e)  { }
+      public void mouseExited(MouseEvent e)   { }
+
+      public void mousePressed(MouseEvent e)  {
+         _mousePressed = true;
+      }
+
+      public void mouseReleased(MouseEvent e) {
+         _processAdjustment(SwingUtilities.isRightMouseButton(e));
+
+	 _mousePressed = false;
+      }
+
+      public void adjustmentValueChanged(AdjustmentEvent e) {
+         if(!_mousePressed) {
+            _processAdjustment(false);
+	 }
+      }
    }
-
-   public String spectralWindowToXML(int subsystemIndex,
-				     String restFrequencyId,
-				     int sideband,
-				     String dataReductionXML,
-				     String indent) {
-      return 
-         indent + "<spectral_window id=\"SPW" + (subsystemIndex + 1) + "\">\n" +
-         indent + "  <spw_bandwidth_mode mode=\"1GHzx1024\"/>\n" +
-         indent + "  <spw_window type=\"truncate\"/>\n" +
-         indent + "  <rest_frequency_ref ref=\"" + restFrequencyId +"\"/>\n" +
-         indent + "  <front_end_sideband sideband=\"" + sideband + "\"/>\n" +
-         indent + "  <spw_if_coordinate>\n" +
-         indent + "    <spw_reference_if_frequency units=\"GHz\">" +
-           ((Sampler)samplers[subsystemIndex]).getCentreFrequency() +
-           "</spw_reference_if_frequency>\n" +
-         indent + "    <spw_reference_pixel>" +
-	   "4064.0???" +
-           "</spw_reference_pixel>\n" +
-         indent + "    <spw_if_channel_width units=\"Hz\">" +
-           ((Sampler)samplers[subsystemIndex]).getBandWidth() +
-           "</spw_if_channel_width>\n" +
-         indent + "    <spw_number_if_channel>" +
-           ((Sampler)samplers[subsystemIndex]).getChannels() +
-           "</spw_number_if_channel>\n" +
-         indent + "  </spw_if_coordinate>\n" +
-         indent + dataReductionXML + "\n" +
-         indent + "</spectral_window>\n";
-   }
-
-
-   /**
-    * Set FrequencyTable from XML.
-    *
-    * Added by Martin Folger (14 November 2001)
-    */
-   public void update(String xml) throws Exception
-   {
-      //try {
-         _xmlReader.parse(new InputSource(new StringReader(xml)));
-      //}
-      //catch(ClassCastException e) {
-         // There seems to be a class cast exception at the first attempt to parse which
-	 // disappears at the second attempt.
-	 // This problem will disappear with jdk1.4
-	// System.out.println("FrontEnd.update(): SAXParser.parse bug in jdk1.3. Ignore.");
-	//_parser.parse(new InputSource(new StringReader(xml)));
-      //}
-   }
-
 }
