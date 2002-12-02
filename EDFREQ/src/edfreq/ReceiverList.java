94a308e3fe1e6894f25968754ff35e4b8420939b
Martin Folger <mfo@roe.ac.uk>
2002-12-02 15:04:05 +0000

    Default overlaps added.

--- EDFREQ/src/edfreq/ReceiverList.java
+++ EDFREQ/src/edfreq/ReceiverList.java
@@ -29,6 +29,7 @@ public class ReceiverList
       r = new Receiver ( "HARP-B", 325.0E9, 375.0E9, 5.0E9, 2.0E9 );
 
       r.bandspecs.add ( new BandSpec ( "1-system", 1, new double[] { 0.25E9, 0.5E9, 1.0E9, 2.0E9 },     // band widths
+                                                      new double[] { 0.25E8, 0.5E8, 1.0E8, 2.0E8 },     // default overlaps
                                                       new int[]    {   8192,  8192,  2048,  2048 },     // channels
                                                       new int[]    {      1,     2,     1,     2 } ) ); // hybrid sub bands
       r.bandspecs.add ( new BandSpec ( "2-system", 2, new double[] { 0.25E9, 1.0E9 }, new int[] { 4096, 1024 } ) );
@@ -39,6 +40,7 @@ public class ReceiverList
       r = new Receiver ( "A3", 215.0E9, 272.0E9, 4.0E9, 1.8E9 );
 
       r.bandspecs.add ( new BandSpec ( "1-system", 1, new double[] { 1.0E9,  4.0E9 },
+                                                      new double[] { 1.0E8,  4.0E8 },
 						      new int[]    { 32768,   8192 },
 						      new int[]    {     4,      4 }) );
       r.bandspecs.add ( new BandSpec ( "4-system", 4, new double[] { 0.25E9, 1.0E9 }, new int[] {  8192, 2048 } ) );
@@ -50,6 +52,7 @@ public class ReceiverList
       r = new Receiver ( "B3", 322.0E9, 373.0E9, 4.0E9, 1.8E9 );
 
       r.bandspecs.add ( new BandSpec ( "1-system", 1, new double[] { 1.0E9,  4.0E9 },
+                                                      new double[] { 1.0E8,  4.0E8 },
 						      new int[]    { 32768,   8192 },
 						      new int[]    {     4,      4 }) );
       r.bandspecs.add ( new BandSpec ( "4-system", 4, new double[] { 0.25E9, 1.0E9 }, new int[] {  8192, 2048 } ) );
@@ -61,6 +64,7 @@ public class ReceiverList
       r = new Receiver ( "WC", 430.0E9, 510.0E9, 4.0E9, 1.8E9 );
 
       r.bandspecs.add ( new BandSpec ( "1-system", 1, new double[] { 1.0E9,  4.0E9 },
+                                                      new double[] { 1.0E8,  4.0E8 },
 						      new int[]    { 32768,   8192 },
 						      new int[]    {     4,      4 }) );
       r.bandspecs.add ( new BandSpec ( "4-system", 4, new double[] { 0.25E9, 1.0E9 }, new int[] {  8192, 2048 } ) );
@@ -72,6 +76,7 @@ public class ReceiverList
       r = new Receiver ( "WD", 630.0E9, 710.0E9, 4.0E9, 1.8E9 );
 
       r.bandspecs.add ( new BandSpec ( "1-system", 1, new double[] { 1.0E9,  4.0E9 },
+                                                      new double[] { 1.0E8,  4.0E8 },
 						      new int[]    { 32768,   8192 },
 						      new int[]    {     4,      4 }) );
       r.bandspecs.add ( new BandSpec ( "4-system", 4, new double[] { 0.25E9, 1.0E9 }, new int[] {  8192, 2048 } ) );
