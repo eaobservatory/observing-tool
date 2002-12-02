5baea1abcdfd0f08ea74a492de4fb281eae06482
Martin Folger <mfo@roe.ac.uk>
2002-12-02 18:35:47 +0000

    velocity definition and frame added. Fonts changed.

--- OT/src/ot/jcmt/inst/editor/HeterodyneGUI.java
+++ OT/src/ot/jcmt/inst/editor/HeterodyneGUI.java
@@ -20,6 +20,7 @@ import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.FlowLayout;
 import java.awt.Container;
+import java.awt.Font;
 
 /**
  * This GUI class is based on the GUI part of the edfreq.FrontEnd class which is not used anymore.
@@ -33,8 +34,10 @@ public class HeterodyneGUI extends JPanel {
   private String currentFE = "";
   private JPanel fePanel;
   private JPanel displayPanel;
-  private JPanel rangePanel;
-  private Box linePanel;
+  private JPanel overlapBandwidthPanel;
+  private JPanel velocityPanel;
+//  private JPanel rangePanel;
+//  private Box linePanel;
   private JPanel mol1Panel;
   private JPanel mol2Panel;
   private JPanel northPanel;
@@ -42,6 +45,8 @@ public class HeterodyneGUI extends JPanel {
   JLabel lowFreq;
   JLabel highFreq;
   JTextField velocity;
+  JComboBox velocityDefinition;
+  JComboBox velocityFrame;
   JTextField overlap;
   JComboBox feBand;
   JComboBox feMode;
@@ -53,15 +58,9 @@ public class HeterodyneGUI extends JPanel {
   JComboBox transitionChoice2;
   JComboBox bandWidthChoice;
   JButton freqEditorButton = new JButton("Show Frequency Editor");
-  private JScrollPane scrollPanel;
-  private double redshift = 0.0;
-  private double subBandWidth = 0.25E9;
-  private double loMin;
-  private double loMax;
-  private double feIF;
-  private double feBandWidth;
-  private double feOverlap = 0.0;
-  private String defaultStoreFile = "hetsetup.txt";
+  JButton hideFreqEditorButton = new JButton("Hide Frequency Editor");
+  JLabel label = null;
+//  private JScrollPane scrollPanel;
   private FlowLayout flowLayoutLeft  = new FlowLayout(FlowLayout.LEFT);
   private FlowLayout flowLayoutRight = new FlowLayout(FlowLayout.RIGHT);
   // Changed to avoid security exception in applet.
@@ -79,57 +78,126 @@ public class HeterodyneGUI extends JPanel {
 /* Create the choice of frontends */
 
       fePanel = new JPanel(flowLayoutLeft);
-      fePanel.add ( new JLabel ( "Choose Front End" ) );
+      label = new JLabel("Front End");
+      label.setFont(new Font("Dialog", 0, 12));
+      label.setForeground(Color.black);
+      fePanel.add(label);
 
       feChoice = new JComboBox (); //cfg.frontEnds );
+      feChoice.setFont(new Font("Dialog", 0, 12));
+      feChoice.setForeground(Color.black);
       lowFreq = new JLabel ( "215" );
+      lowFreq.setFont(new Font("Dialog", 0, 12));
+      lowFreq.setForeground(Color.black);
       lowFreq.setBorder ( new BevelBorder ( BevelBorder.LOWERED ) );
       highFreq = new JLabel ( "272" );
+      highFreq.setFont(new Font("Dialog", 0, 12));
+      highFreq.setForeground(Color.black);
       highFreq.setBorder ( new BevelBorder ( BevelBorder.LOWERED ) );
 
 //      feChoice.addActionListener ( this );
 
       feMode = new JComboBox (); // cfg.frontEndModes );
+      feMode.setFont(new Font("Dialog", 0, 12));
+      feMode.setForeground(Color.black);
       feBandModeChoice = new JComboBox ( );
+      feBandModeChoice.setFont(new Font("Dialog", 0, 12));
+      feBandModeChoice.setForeground(Color.black);
       //feBandModeChoice.addActionListener ( this );
+
+      fePanel.add ( feChoice );
+      fePanel.add ( feMode );
+      fePanel.add ( feBandModeChoice );
+
+/* Create the display */
+
+      displayPanel = new JPanel(flowLayoutLeft);
+      label = new JLabel("Low Limit (GHz)");
+      label.setFont(new Font("Dialog", 0, 12));
+      label.setForeground(Color.black);
+      displayPanel.add ( label );
+      displayPanel.add ( lowFreq );
+      label = new JLabel("    High Limit (GHz)");
+      label.setFont(new Font("Dialog", 0, 12));
+      label.setForeground(Color.black);
+      displayPanel.add ( label );
+      displayPanel.add ( highFreq );
+
+
+      // Bandwidth and Overlap
+      overlapBandwidthPanel = new JPanel(flowLayoutLeft);
+
+      label = new JLabel("Band");
+      label.setFont(new Font("Dialog", 0, 12));
+      label.setForeground(Color.black);
+      overlapBandwidthPanel.add(label);
+
+      feBand = new JComboBox(); // new String[] { "usb", "lsb", "optimum" } );
+      feBand.setFont(new Font("Dialog", 0, 12));
+      feBand.setForeground(Color.black);
+//      feBand.addActionListener ( this );
+      overlapBandwidthPanel.add(feBand);
+
+      label = new JLabel("    Bandwidth");
+      label.setFont(new Font("Dialog", 0, 12));
+      label.setForeground(Color.black);
+      overlapBandwidthPanel.add(label);
+      
+      bandWidthChoice = new JComboBox();
+      bandWidthChoice.setFont(new Font("Dialog", 0, 12));
+      bandWidthChoice.setForeground(Color.black);
+      overlapBandwidthPanel.add(bandWidthChoice);
+
+      label = new JLabel("    Overlap (MHz)");
+      label.setFont(new Font("Dialog", 0, 12));
+      label.setForeground(Color.black);
+      overlapBandwidthPanel.add (label);
 
       overlap = new JTextField();
       overlap.setColumns(10);
       overlap.setText ( "0.0" );
 //      overlap.addActionListener ( this );
+      overlapBandwidthPanel.add(overlap);
 
-      fePanel.add ( feChoice );
-      fePanel.add ( feMode );
-      fePanel.add ( feBandModeChoice );
-      fePanel.add ( new JLabel ( "    Overlap (MHz)" ) );
-      fePanel.add ( overlap );
+      // Velocity
+      velocityPanel = new JPanel(flowLayoutLeft);
 
-/* Create the display */
+      label = new JLabel("Velocity");
+      label.setFont(new Font("Dialog", 0, 12));
+      label.setForeground(Color.black);
+      velocityPanel.add(label);
 
-      displayPanel = new JPanel(flowLayoutLeft);
-      displayPanel.add ( new JLabel ( "Low Limit (GHz)" ) );
-      displayPanel.add ( lowFreq );
-      displayPanel.add ( new JLabel ( "    High Limit (GHz)" ) );
-      displayPanel.add ( highFreq );
       velocity = new JTextField();
       velocity.setColumns(10);
       velocity.setText ( "0.0" );
 //      velocity.addActionListener ( this );
+      velocityPanel.add(velocity);
 
-      displayPanel.add ( new JLabel ( "    Velocity (Km/s)" ) );
-      displayPanel.add ( velocity );
+      velocityDefinition = new JComboBox();
+      velocityDefinition.setFont(new Font("Dialog", 0, 12));
+      velocityPanel.add(velocityDefinition);
 
-      rangePanel = new JPanel();
-      feBand = new JComboBox(); // new String[] { "usb", "lsb", "optimum" } );
-//      feBand.addActionListener ( this );
+      label = new JLabel("    Frame");
+      label.setFont(new Font("Dialog", 0, 12));
+      label.setForeground(Color.black);
+      velocityPanel.add(label);
+
+      velocityFrame = new JComboBox();
+      velocityFrame.setFont(new Font("Dialog", 0, 12));
+      velocityPanel.add(velocityFrame);
+
+//      rangePanel = new JPanel(flowLayoutLeft);
+
 
 /* Main molecular line choice - used to set front-end LO1 to put the line
    in the nominated sideband */
 
       moleculeChoice = new JComboBox();
+      moleculeChoice.setFont(new Font("Dialog", 0, 12));
       moleculeChoice.setForeground ( Color.red );
 //      moleculeChoice.addActionListener ( this );
       transitionChoice = new JComboBox();
+      transitionChoice.setFont(new Font("Dialog", 0, 12));
       transitionChoice.setForeground ( Color.red );
 //      transitionChoice.addActionListener ( this );
       moleculeFrequency = new JTextField();
@@ -137,23 +205,24 @@ public class HeterodyneGUI extends JPanel {
       //moleculeFrequency.setText ( "0.0000" );
       moleculeFrequency.setForeground ( Color.red );
 //      moleculeFrequency.addActionListener(this);
-      bandWidthChoice = new JComboBox();
 //      bandWidthChoice.addActionListener( this );
-      mol1Panel = new JPanel(flowLayoutRight);
-      mol1Panel.add ( feBand );
+      mol1Panel = new JPanel(flowLayoutLeft);
       mol1Panel.add ( moleculeChoice );
       mol1Panel.add ( transitionChoice );
       mol1Panel.add ( moleculeFrequency );
-      mol1Panel.add ( new JLabel("MHz    BW:") );
-      mol1Panel.add ( bandWidthChoice );
-
+      label = new JLabel("MHz");
+      label.setFont(new Font("Dialog", 0, 12));
+      label.setForeground(Color.black);
+      mol1Panel.add (label);
 /* Secondary moleculare line choice - displayed just for convenience of
    astronomer */
 
       moleculeChoice2 = new JComboBox();
+      moleculeChoice2.setFont(new Font("Dialog", 0, 12));
       moleculeChoice2.setForeground ( Color.magenta );
 //      moleculeChoice2.addActionListener ( this );
       transitionChoice2 = new JComboBox();
+      transitionChoice2.setFont(new Font("Dialog", 0, 12));
       transitionChoice2.setForeground ( Color.magenta );
 //      transitionChoice2.addActionListener ( this );
       moleculeFrequency2 = new JTextField();
@@ -165,10 +234,16 @@ public class HeterodyneGUI extends JPanel {
       mol2Panel.add ( moleculeChoice2 );
       mol2Panel.add ( transitionChoice2 );
       mol2Panel.add ( moleculeFrequency2 );
-      mol2Panel.add ( new JLabel("MHz") );
+      label = new JLabel("MHz");
+      label.setFont(new Font("Dialog", 0, 12));
+      label.setForeground(Color.black);
+      mol2Panel.add(label);
 
-      linePanel = Box.createVerticalBox();
-      linePanel.add ( mol1Panel );
+      freqEditorButton.setFont(new Font("Dialog", 0, 12));
+      hideFreqEditorButton.setFont(new Font("Dialog", 0, 12));
+
+//      linePanel = Box.createVerticalBox();
+//      linePanel.add ( mol1Panel );
 
       // MFO (May 03, 2002): mol2Panel is currently not used.
       //linePanel.add ( mol2Panel );
@@ -179,25 +254,45 @@ public class HeterodyneGUI extends JPanel {
 
 /* Assemble the display */
 
-      rangePanel.add ( linePanel );
+//      rangePanel.add ( linePanel );
 
       northPanel = new JPanel();
       northPanel.setLayout ( new BoxLayout ( northPanel, 
         BoxLayout.Y_AXIS ) );
 
+      Dimension preferredSize = fePanel.getPreferredSize();
+      preferredSize.height += 10;
+      fePanel.setPreferredSize(preferredSize);
+      preferredSize = displayPanel.getPreferredSize();
+      preferredSize.height += 10;
+      displayPanel.setPreferredSize(preferredSize);
+      preferredSize = overlapBandwidthPanel.getPreferredSize();
+      preferredSize.height += 10;
+      overlapBandwidthPanel.setPreferredSize(preferredSize);
+      preferredSize = velocityPanel.getPreferredSize();
+      preferredSize.height += 10;
+      velocityPanel.setPreferredSize(preferredSize);
+      preferredSize = mol1Panel.getPreferredSize();
+      preferredSize.height += 10;
+      mol1Panel.setPreferredSize(preferredSize);
+
+
       northPanel.add ( fePanel );
       northPanel.add ( displayPanel );
-      northPanel.add ( rangePanel );
+      northPanel.add ( overlapBandwidthPanel );
+      northPanel.add ( velocityPanel );
+      northPanel.add ( mol1Panel );
       add ( northPanel, BorderLayout.NORTH );
 
-      scrollPanel = new JScrollPane();
+//      scrollPanel = new JScrollPane();
       //scrollPanel.setPreferredSize ( new Dimension ( 600, 150 ) );
 
-      add ( scrollPanel, BorderLayout.CENTER );
+//      add ( scrollPanel, BorderLayout.CENTER );
 
 
       southPanel = new JPanel();
       southPanel.add(freqEditorButton);
+      southPanel.add(hideFreqEditorButton);
 
       add(southPanel, BorderLayout.SOUTH);
   }
