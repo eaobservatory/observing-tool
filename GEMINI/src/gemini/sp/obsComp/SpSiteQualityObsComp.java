ead1ced0730c12b6f180b534a9b839c6ebabbeab
Shaun De Witt <s.dewitt@jach.hawaii.edu>
2003-01-16 23:06:20 +0000

    Updated for new interface

--- GEMINI/src/gemini/sp/obsComp/SpSiteQualityObsComp.java
+++ GEMINI/src/gemini/sp/obsComp/SpSiteQualityObsComp.java
@@ -8,12 +8,15 @@ package gemini.sp.obsComp;
 
 import gemini.sp.SpFactory;
 import gemini.sp.SpType;
+import gemini.util.Format;
 
 /**
  * Site Quality observation component.
  */
 public class SpSiteQualityObsComp extends SpObsComp
 {
+   public static final String ATTR_TAU_BAND_ALLOCATED = "tauBandAllocated";
+   public static final String ATTR_SEEING_ALLOCATED = "seeingAllocated";
    public static final String ATTR_SEEING_MIN   = "seeing.min";
    public static final String ATTR_SEEING_MAX   = "seeing.max";
    public static final String ATTR_CSO_TAU_MIN  = "csoTau.min";
@@ -21,10 +24,10 @@ public class SpSiteQualityObsComp extends SpObsComp
    public static final String ATTR_MOON         = "moon";
    public static final String ATTR_CLOUD        = "cloud";
 
-   public static final int SEEING_EXCELLENT     = 0;
-   public static final int SEEING_GOOD          = 1;
-   public static final int SEEING_POOR          = 2;
-   public static final int SEEING_ANY           = 3;
+//    public static final int SEEING_EXCELLENT     = 0;
+//    public static final int SEEING_GOOD          = 1;
+//    public static final int SEEING_POOR          = 2;
+//    public static final int SEEING_ANY           = 3;
 
    protected static final String XML_SEEING     = "seeing";
    protected static final String XML_CSO_TAU    = "csoTau";
@@ -35,18 +38,18 @@ public class SpSiteQualityObsComp extends SpObsComp
 
    private String _previousXmlElement = "";
 
-   public static final double [][] SEEING_RANGES = {
-      {0.0, 0.4},
-      {0.0, 0.6},
-      {0.0, 0.8}
-   };
+//    public static final double [][] SEEING_RANGES = {
+//       {0.0, 0.4},
+//       {0.0, 0.6},
+//       {0.0, 0.8}
+//    };
 
-   public static final int CSO_TAU_VERY_DRY = 0;
-   public static final int CSO_TAU_ANY      = 1;
+//    public static final int CSO_TAU_VERY_DRY = 0;
+//    public static final int CSO_TAU_ANY      = 1;
 
-   public static final double [][] CSO_TAU_RANGES = {
-      {0.0, 0.09}
-   };
+//    public static final double [][] CSO_TAU_RANGES = {
+//       {0.0, 0.09}
+//    };
 
    public static final int MOON_DARK = 0;
    public static final int MOON_GREY = 1;
@@ -63,6 +66,7 @@ public class SpSiteQualityObsComp extends SpObsComp
    public static final int CLOUD_OPTIONS_LENGTH = 3;
 
    public static final String SUBTYPE = "schedInfo";
+   public static final int NO_VALUE              = 0;
 
    public static final SpType SP_TYPE =
    	SpType.create(SpType.OBSERVATION_COMPONENT_TYPE, SUBTYPE, "Site Quality");
@@ -80,6 +84,10 @@ public SpSiteQualityObsComp()
 {
    // MFO: Changed because UKIRT and JCMT use different site quality components.
    super(SP_TYPE);
+   _avTable.noNotifySet(ATTR_TAU_BAND_ALLOCATED, "true", 0);
+   _avTable.noNotifySet(ATTR_SEEING_ALLOCATED, "true", 0);
+   _avTable.noNotifySet(ATTR_MOON, "2", 0);
+   _avTable.noNotifySet(ATTR_CLOUD, "2", 0);
    //super( SpType.OBSERVATION_COMPONENT_SITE_QUALITY );
 }
 
@@ -88,57 +96,104 @@ public SpSiteQualityObsComp()
  *
  * @param seeing One of {@link #SEEING_EXCELLENT}, {@link #SEEING_GOOD}, {@link #SEEING_POOR}, {@link #SEEING_ANY}.
  */
-public void
-setSeeing(int seeing)
-{
-   if ((seeing < 0) || (seeing >= (SEEING_RANGES.length) || (seeing == SEEING_ANY))) {
-      _avTable.rm(ATTR_SEEING_MIN);
-      _avTable.rm(ATTR_SEEING_MAX);
-   }
-   else {
-      _avTable.set(ATTR_SEEING_MIN, SEEING_RANGES[seeing][0]);
-      _avTable.set(ATTR_SEEING_MAX, SEEING_RANGES[seeing][1]);
-   }
-}
+// public void
+// setSeeing(int seeing)
+// {
+//    if ((seeing < 0) || (seeing >= (SEEING_RANGES.length) || (seeing == SEEING_ANY))) {
+//       _avTable.rm(ATTR_SEEING_MIN);
+//       _avTable.rm(ATTR_SEEING_MAX);
+//    }
+//    else {
+//       _avTable.set(ATTR_SEEING_MIN, SEEING_RANGES[seeing][0]);
+//       _avTable.set(ATTR_SEEING_MAX, SEEING_RANGES[seeing][1]);
+//    }
+// }
 
 /**
  * Get Seeing index.
  *
  * @return One of {@link #SEEING_EXCELLENT}, {@link #SEEING_GOOD}, {@link #SEEING_POOR}, {@link #SEEING_ANY}.
  */
-public int
-getSeeing()
-{
-   if(_avTable.get(ATTR_SEEING_MAX) == null) {
-      return SEEING_ANY;
-   }
+// public int
+// getSeeing()
+// {
+//    if(_avTable.get(ATTR_SEEING_MAX) == null) {
+//       return SEEING_ANY;
+//    }
    
-   // Default -1.0 ensure that no range maximum matches and SEEING_ANY is returned.
-   double max = _avTable.getDouble(ATTR_SEEING_MAX, -1.0);
+//    // Default -1.0 ensure that no range maximum matches and SEEING_ANY is returned.
+//    double max = _avTable.getDouble(ATTR_SEEING_MAX, -1.0);
 
-   if(max == SEEING_RANGES[SEEING_EXCELLENT][1]) return SEEING_EXCELLENT;
-   if(max == SEEING_RANGES[SEEING_GOOD][1])      return SEEING_GOOD;
-   if(max == SEEING_RANGES[SEEING_POOR][1])      return SEEING_POOR;
+//    if(max == SEEING_RANGES[SEEING_EXCELLENT][1]) return SEEING_EXCELLENT;
+//    if(max == SEEING_RANGES[SEEING_GOOD][1])      return SEEING_GOOD;
+//    if(max == SEEING_RANGES[SEEING_POOR][1])      return SEEING_POOR;
    
-   return SEEING_ANY;
-}
+//    return SEEING_ANY;
+// }
 
 /**
  * Get Seeing range as array of two doubles.
  */
-public double []
-getSeeingRange()
-{
-   double [] result = new double[]{
-      _avTable.getDouble(ATTR_SEEING_MIN, -1), _avTable.getDouble(ATTR_SEEING_MAX, -1)
-   };
+// public double []
+// getSeeingRange()
+// {
+//    double [] result = new double[]{
+//       _avTable.getDouble(ATTR_SEEING_MIN, -1), _avTable.getDouble(ATTR_SEEING_MAX, -1)
+//    };
 
-   if((result[0] == -1) || (result[1] == -1)) {
-      return null;
-   }
-   else {
-      return result;
-   }
+//    if((result[0] == -1) || (result[1] == -1)) {
+//       return null;
+//    }
+//    else {
+//       return result;
+//    }
+// }
+public void
+setSeeingAllocated(boolean value)
+{
+   _avTable.set(ATTR_SEEING_ALLOCATED, value);
+}
+
+public boolean
+seeingAllocated()
+{
+   return _avTable.getBool(ATTR_SEEING_ALLOCATED);
+}
+
+public void
+setMinSeeing(double value)
+{
+   _avTable.set(ATTR_SEEING_MIN, value);
+}
+
+public void
+setMinSeeing(String value)
+{
+   setMinSeeing(Format.toDouble(value));
+}
+
+public double
+getMinSeeing()
+{
+   return _avTable.getDouble(ATTR_SEEING_MIN, NO_VALUE);
+}
+
+public void
+setMaxSeeing(double value)
+{
+   _avTable.set(ATTR_SEEING_MAX, value);
+}
+
+public void
+setMaxSeeing(String value)
+{
+   setMaxSeeing(Format.toDouble(value));
+}
+
+public double
+getMaxSeeing()
+{
+   return _avTable.getDouble(ATTR_SEEING_MAX, NO_VALUE);
 }
 
 //--
@@ -148,55 +203,105 @@ getSeeingRange()
  *
  * @param csoTau One of {@link #CSO_TAU_VERY_DRY}, {@link #CSO_TAU_ANY}.
  */
-public void
-setCsoTau(int csoTau)
-{
-   if ((csoTau < 0) || (csoTau >= (CSO_TAU_RANGES.length) || (csoTau == CSO_TAU_ANY))) {
-      _avTable.rm(ATTR_CSO_TAU_MIN);
-      _avTable.rm(ATTR_CSO_TAU_MAX);
-   }
-   else {
-      _avTable.set(ATTR_CSO_TAU_MIN, CSO_TAU_RANGES[csoTau][0]);
-      _avTable.set(ATTR_CSO_TAU_MAX, CSO_TAU_RANGES[csoTau][1]);
-   }
-}
+// public void
+// setCsoTau(int csoTau)
+// {
+//    if ((csoTau < 0) || (csoTau >= (CSO_TAU_RANGES.length) || (csoTau == CSO_TAU_ANY))) {
+//       _avTable.rm(ATTR_CSO_TAU_MIN);
+//       _avTable.rm(ATTR_CSO_TAU_MAX);
+//    }
+//    else {
+//       _avTable.set(ATTR_CSO_TAU_MIN, CSO_TAU_RANGES[csoTau][0]);
+//       _avTable.set(ATTR_CSO_TAU_MAX, CSO_TAU_RANGES[csoTau][1]);
+//    }
+// }
 
 /**
  * Get CSO Tau index.
  *
  * @return One of {@link #CSO_TAU_VERY_DRY}, {@link #CSO_TAU_ANY}.
  */
-public int
-getCsoTau()
-{
-   if(_avTable.get(ATTR_CSO_TAU_MIN) == null) {
-      return CSO_TAU_ANY;
-   }
+// public int
+// getCsoTau()
+// {
+//    if(_avTable.get(ATTR_CSO_TAU_MIN) == null) {
+//       return CSO_TAU_ANY;
+//    }
 
-   // Default -1.0 ensure that no range minimum matches and CSO_TAU_ANY is returned.
-   double min = _avTable.getDouble(ATTR_SEEING_MIN, -1.0);
+//    // Default -1.0 ensure that no range minimum matches and CSO_TAU_ANY is returned.
+//    double min = _avTable.getDouble(ATTR_SEEING_MIN, -1.0);
 
-   if(min == CSO_TAU_RANGES[CSO_TAU_VERY_DRY][0]) return CSO_TAU_VERY_DRY;
+//    if(min == CSO_TAU_RANGES[CSO_TAU_VERY_DRY][0]) return CSO_TAU_VERY_DRY;
    
-   return CSO_TAU_ANY;
-}
+//    return CSO_TAU_ANY;
+// }
 
 /**
  * Get CSO Tau range as array of two doubles.
  */
-public double []
-getCsoTauRange()
-{
-   double [] result = new double[]{
-      _avTable.getDouble(ATTR_CSO_TAU_MIN, -1), _avTable.getDouble(ATTR_CSO_TAU_MAX, -1)
-   };
+// public double []
+// getCsoTauRange()
+// {
+//    double [] result = new double[]{
+//       _avTable.getDouble(ATTR_CSO_TAU_MIN, -1), _avTable.getDouble(ATTR_CSO_TAU_MAX, -1)
+//    };
 
-   if((result[0] == -1) || (result[1] == -1)) {
-      return null;
-   }
-   else {
-      return result;
-   }
+//    if((result[0] == -1) || (result[1] == -1)) {
+//       return null;
+//    }
+//    else {
+//       return result;
+//    }
+// }
+
+
+public void
+setTauBandAllocated(boolean value)
+{
+   _avTable.set(ATTR_TAU_BAND_ALLOCATED, value);
+}
+
+public boolean
+tauBandAllocated()
+{
+   return _avTable.getBool(ATTR_TAU_BAND_ALLOCATED);
+}
+
+public void
+setMinTau(double value)
+{
+   _avTable.set(ATTR_CSO_TAU_MIN, value);
+}
+
+public void
+setMinTau(String value)
+{
+   setMinTau(Format.toDouble(value));
+}
+
+public double
+getMinTau()
+{
+   return _avTable.getDouble(ATTR_CSO_TAU_MIN, NO_VALUE);
+}
+
+public void
+setMaxTau(double value)
+{
+   _avTable.set(ATTR_CSO_TAU_MAX, value);
+}
+
+public void
+setMaxTau(String value)
+{
+   setMaxTau(Format.toDouble(value));
+}
+
+
+public double
+getMaxTau()
+{
+   return _avTable.getDouble(ATTR_CSO_TAU_MAX, NO_VALUE);
 }
 
 
@@ -259,29 +364,29 @@ getCloud()
 protected void
 processAvAttribute(String avAttr, String indent, StringBuffer xmlBuffer)
 {
-   if(avAttr.equals(ATTR_SEEING_MAX)) {
+   if(avAttr.equals(ATTR_SEEING_ALLOCATED) && (!seeingAllocated())) {
       xmlBuffer.append("\n  "   + indent + "<"  + XML_SEEING  + ">");
-      xmlBuffer.append("\n    " + indent + "<"  + XML_MIN     + ">" + getSeeingRange()[0] + "</" + XML_MIN + ">");
-      xmlBuffer.append("\n    " + indent + "<"  + XML_MAX     + ">" + getSeeingRange()[1] + "</" + XML_MAX + ">");
+      xmlBuffer.append("\n    " + indent + "<"  + XML_MIN     + ">" + getMinSeeing() + "</" + XML_MIN + ">");
+      xmlBuffer.append("\n    " + indent + "<"  + XML_MAX     + ">" + getMaxSeeing() + "</" + XML_MAX + ">");
       xmlBuffer.append("\n  "   + indent + "</" + XML_SEEING  + ">");
 
       return;
    }
 
-   if(avAttr.equals(ATTR_SEEING_MIN)) {
+   if(avAttr.equals(ATTR_SEEING_MIN) || avAttr.equals(ATTR_SEEING_MAX) || avAttr.equals(ATTR_SEEING_ALLOCATED)) {
       return;
    }
    
-   if(avAttr.equals(ATTR_CSO_TAU_MAX)) {
+   if(avAttr.equals(ATTR_TAU_BAND_ALLOCATED) && (!tauBandAllocated())) {
       xmlBuffer.append("\n  "   + indent + "<"  + XML_CSO_TAU + ">");
-      xmlBuffer.append("\n    " + indent + "<"  + XML_MIN     + ">" + getCsoTauRange()[0] + "</" + XML_MIN + ">");
-      xmlBuffer.append("\n    " + indent + "<"  + XML_MAX     + ">" + getCsoTauRange()[1] + "</" + XML_MAX + ">");
+      xmlBuffer.append("\n    " + indent + "<"  + XML_MIN     + ">" + getMinTau() + "</" + XML_MIN + ">");
+      xmlBuffer.append("\n    " + indent + "<"  + XML_MAX     + ">" + getMaxTau() + "</" + XML_MAX + ">");
       xmlBuffer.append("\n  "   + indent + "</" + XML_CSO_TAU + ">");
 
       return;
    }
   
-   if(avAttr.equals(ATTR_CSO_TAU_MIN)) {
+   if(avAttr.equals(ATTR_CSO_TAU_MIN) || avAttr.equals(ATTR_CSO_TAU_MAX) || avAttr.equals(ATTR_TAU_BAND_ALLOCATED)) {
       return;
    }
 
@@ -320,11 +425,13 @@ processXmlElementContent(String name, String value)
       if(name.equals(XML_MAX)) {
          if(_previousXmlElement.equals(XML_SEEING)) {
             _avTable.noNotifySet(ATTR_SEEING_MAX, value, 0);
+	    _avTable.noNotifySet(ATTR_SEEING_ALLOCATED, "false", 0);
 	    return;
 	 }
 
          if(_previousXmlElement.equals(XML_CSO_TAU)) {
             _avTable.noNotifySet(ATTR_CSO_TAU_MAX, value, 0);
+	    _avTable.noNotifySet(ATTR_TAU_BAND_ALLOCATED, "false", 0);
 	    return;
 	 }
       }
--- OT/src/jsky/app/ot/editor/EdCompSiteQuality.java
+++ OT/src/jsky/app/ot/editor/EdCompSiteQuality.java
@@ -12,6 +12,8 @@ import java.awt.event.ActionListener;
 import javax.swing.ButtonGroup;
 
 import jsky.app.ot.gui.OptionWidgetExt;
+import jsky.app.ot.gui.TextBoxWidgetExt;
+import jsky.app.ot.gui.TextBoxWidgetWatcher;
 
 import gemini.sp.obsComp.SpSiteQualityObsComp;
 
@@ -20,7 +22,7 @@ import gemini.sp.obsComp.SpSiteQualityObsComp;
  * This is the editor for Site Quality component.
  */
 public final class EdCompSiteQuality extends OtItemEditor 
-    implements ActionListener {
+    implements ActionListener, TextBoxWidgetWatcher {
 
     private SiteQualityGUI _w;       // the GUI layout panel
 
@@ -36,24 +38,30 @@ public final class EdCompSiteQuality extends OtItemEditor
 	// add action listeners and group the buttons
 	ButtonGroup grp;
 
-	_w.seeingExcellent.addActionListener(this);
-	_w.seeingGood.addActionListener(this);
-	_w.seeingPoor.addActionListener(this);
-	_w.seeingAny.addActionListener(this);
+// 	_w.seeingExcellent.addActionListener(this);
+// 	_w.seeingGood.addActionListener(this);
+// 	_w.seeingPoor.addActionListener(this);
+// 	_w.seeingAny.addActionListener(this);
+	_w.seeingAllocated.addActionListener(this);
+	_w.seeingUserDefined.addActionListener(this);
+	_w.minSeeing.addWatcher(this);
+	_w.maxSeeing.addWatcher(this);
 
 	grp = new ButtonGroup();
-	grp.add(_w.seeingExcellent);
-	grp.add(_w.seeingGood);
-	grp.add(_w.seeingPoor);
-	grp.add(_w.seeingAny);
+	grp.add(_w.seeingAllocated);
+	grp.add(_w.seeingUserDefined);
 
 
-	_w.csoVeryDry.addActionListener(this);
-	_w.csoAny.addActionListener(this);
+// 	_w.csoVeryDry.addActionListener(this);
+// 	_w.csoAny.addActionListener(this);
+	_w.tauBandAllocated.addActionListener(this);
+	_w.tauBandUserDefined.addActionListener(this);
+	_w.minTau.addWatcher(this);
+	_w.maxTau.addWatcher(this);
 
 	grp = new ButtonGroup();
-	grp.add(_w.csoVeryDry);
-	grp.add(_w.csoAny);
+	grp.add(_w.tauBandAllocated);
+	grp.add(_w.tauBandUserDefined);
 
 
 	_w.moonDark.addActionListener(this);
@@ -86,28 +94,56 @@ public final class EdCompSiteQuality extends OtItemEditor
 	int i;
 
 	// Image Quality
-	i = sq.getSeeing();
-	switch (i) {
-	case SpSiteQualityObsComp.SEEING_EXCELLENT:
-	    ow = _w.seeingExcellent; break;
-	case SpSiteQualityObsComp.SEEING_GOOD:
-	    ow = _w.seeingGood; break;
-	case SpSiteQualityObsComp.SEEING_POOR:
-	    ow = _w.seeingPoor; break;
-	default:
-	    ow = _w.seeingAny; break;
+// 	i = sq.getSeeing();
+// 	switch (i) {
+// 	case SpSiteQualityObsComp.SEEING_EXCELLENT:
+// 	    ow = _w.seeingExcellent; break;
+// 	case SpSiteQualityObsComp.SEEING_GOOD:
+// 	    ow = _w.seeingGood; break;
+// 	case SpSiteQualityObsComp.SEEING_POOR:
+// 	    ow = _w.seeingPoor; break;
+// 	default:
+// 	    ow = _w.seeingAny; break;
+// 	}
+	if (sq.tauBandAllocated()) {
+	    _w.tauBandAllocated.setValue(true);
+	    _w.minTau.setValue("");
+	    _w.maxTau.setValue("");
+	    _w.minTau.setEnabled(false);
+	    _w.maxTau.setEnabled(false);
 	}
-	ow.setValue(true);
+	else {
+	    _w.tauBandUserDefined.setValue(true);
+	    _w.minTau.setValue(sq.getMinTau());
+	    _w.maxTau.setValue(sq.getMaxTau());
+	    _w.minTau.setEnabled(true);
+	    _w.maxTau.setEnabled(true);
+	}
+// 	ow.setValue(true);
 
 	// IR Background
-	i = sq.getCsoTau();
-	switch (i) {
-	case SpSiteQualityObsComp.CSO_TAU_VERY_DRY:
-	    ow = _w.csoVeryDry; break;
-	default:
-	    ow = _w.csoAny; break;
+// 	i = sq.getCsoTau();
+// 	switch (i) {
+// 	case SpSiteQualityObsComp.CSO_TAU_VERY_DRY:
+// 	    ow = _w.csoVeryDry; break;
+// 	default:
+// 	    ow = _w.csoAny; break;
+// 	}
+// 	ow.setValue(true);
+	if (sq.seeingAllocated()) {
+	    _w.seeingAllocated.setValue(true);
+	    _w.minSeeing.setValue("");
+	    _w.maxSeeing.setValue("");
+	    _w.minSeeing.setEnabled(false);
+	    _w.maxSeeing.setEnabled(false);
 	}
-	ow.setValue(true);
+	else {
+	    _w.seeingUserDefined.setValue(true);
+	    _w.minSeeing.setValue(sq.getMinSeeing());
+	    _w.maxSeeing.setValue(sq.getMaxSeeing());
+	    _w.minSeeing.setEnabled(true);
+	    _w.maxSeeing.setEnabled(true);
+	}
 
 	// Moon
 	i = sq.getMoon();
@@ -144,31 +180,62 @@ public final class EdCompSiteQuality extends OtItemEditor
 	SpSiteQualityObsComp sq = (SpSiteQualityObsComp) _spItem;
 
 	// Seeing
-	if (w == _w.seeingExcellent) {
-	    sq.setSeeing(SpSiteQualityObsComp.SEEING_EXCELLENT);
-	    return;
+// 	if (w == _w.seeingExcellent) {
+// 	    sq.setSeeing(SpSiteQualityObsComp.SEEING_EXCELLENT);
+// 	    return;
+// 	}
+// 	if (w == _w.seeingGood) {
+// 	    sq.setSeeing(SpSiteQualityObsComp.SEEING_GOOD);
+// 	    return;
+// 	}
+// 	if (w == _w.seeingPoor) {
+// 	    sq.setSeeing(SpSiteQualityObsComp.SEEING_POOR);
+// 	    return;
+// 	}
+// 	if (w == _w.seeingAny) {
+// 	    sq.setSeeing(SpSiteQualityObsComp.SEEING_ANY);
+// 	    return;
+// 	}
+	if (w == _w.seeingAllocated) {
+	    sq.setSeeingAllocated(true);
+	    _w.minSeeing.setValue("");
+	    _w.maxSeeing.setValue("");
+	    _w.minSeeing.setEnabled(false);
+	    _w.maxSeeing.setEnabled(false);
 	}
-	if (w == _w.seeingGood) {
-	    sq.setSeeing(SpSiteQualityObsComp.SEEING_GOOD);
-	    return;
-	}
-	if (w == _w.seeingPoor) {
-	    sq.setSeeing(SpSiteQualityObsComp.SEEING_POOR);
-	    return;
-	}
-	if (w == _w.seeingAny) {
-	    sq.setSeeing(SpSiteQualityObsComp.SEEING_ANY);
-	    return;
+	
+	if (w == _w.seeingUserDefined) {
+	    sq.setSeeingAllocated(false);
+	    _w.minSeeing.setValue(sq.getMinSeeing());
+	    _w.maxSeeing.setValue(sq.getMaxSeeing());
+	    _w.minSeeing.setEnabled(true);
+	    _w.maxSeeing.setEnabled(true);
 	}
 
 	// CSO Tau
-	if (w == _w.csoVeryDry) {
-	    sq.setCsoTau(SpSiteQualityObsComp.CSO_TAU_VERY_DRY);
-	    return;
+// 	if (w == _w.csoVeryDry) {
+// 	    sq.setCsoTau(SpSiteQualityObsComp.CSO_TAU_VERY_DRY);
+// 	    return;
+// 	}
+// 	if (w == _w.csoAny) {
+// 	    sq.setCsoTau(SpSiteQualityObsComp.CSO_TAU_ANY);
+// 	    return;
+// 	}
+	if (w == _w.tauBandAllocated) {
+	    sq.setTauBandAllocated(true);
+	    _w.minTau.setValue("");
+	    _w.maxTau.setValue("");
+	    _w.minTau.setEnabled(false);
+	    _w.maxTau.setEnabled(false);
+
 	}
-	if (w == _w.csoAny) {
-	    sq.setCsoTau(SpSiteQualityObsComp.CSO_TAU_ANY);
-	    return;
+
+	if (w == _w.tauBandUserDefined) {
+	    sq.setTauBandAllocated(false);
+	    _w.minTau.setValue(sq.getMinTau());
+	    _w.maxTau.setValue(sq.getMaxTau());
+	    _w.minTau.setEnabled(true);
+	    _w.maxTau.setEnabled(true);
 	}
 
 	// Moon
@@ -199,4 +266,36 @@ public final class EdCompSiteQuality extends OtItemEditor
 	    return;
 	}
     }
+
+    /**
+     * Watch changes to min text boxes.
+     */
+    public void	textBoxKeyPress(TextBoxWidgetExt tbwe) {
+	if (tbwe == _w.minTau) {
+	    ((SpSiteQualityObsComp)_spItem).setMinTau(_w.minTau.getValue());
+	    return;
+	}
+
+	if (tbwe == _w.maxTau) {
+	    ((SpSiteQualityObsComp)_spItem).setMaxTau(_w.maxTau.getValue());
+	    return;
+	}
+
+	if (tbwe == _w.minSeeing) {
+	    System.out.println("Updating min seeing to "+_w.minSeeing.getValue());
+	    ((SpSiteQualityObsComp)_spItem).setMinSeeing(_w.minSeeing.getValue());
+	    return;
+	}
+
+	if (tbwe == _w.maxSeeing) {
+	    System.out.println("Updating max seeing to "+_w.maxSeeing.getValue());
+	    ((SpSiteQualityObsComp)_spItem).setMaxSeeing(_w.maxSeeing.getValue());
+	    return;
+	}
+    }
+
+    /**
+     * Text box action, ignore.
+     */
+    public void	textBoxAction(TextBoxWidgetExt tbwe) { }
 }
