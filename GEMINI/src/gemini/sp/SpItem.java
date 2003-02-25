5d4e996222740605ce3045a5761b9d5a89481aef
Shaun De Witt <s.dewitt@jach.hawaii.edu>
2003-02-25 19:13:09 +0000

    Backed out last change - there were some very definitely undesirable sideeffects.

--- GEMINI/src/gemini/sp/SpItem.java
+++ GEMINI/src/gemini/sp/SpItem.java
@@ -1008,11 +1008,7 @@ print(String indentStr)
    *
    * @see #processXmlElementContent(java.lang.String,java.lang.String,int)
    */
-  public void processXmlElementStart(String name) { 
-      if ( _avTable != null ) {
-	  _avTable.noNotifyRmAll();
-      }
-  }
+  public void processXmlElementStart(String name) { }
 
   /**
    * This method can be called by an external XML parser when an XML element is parsed.
