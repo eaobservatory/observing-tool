3558787ec9f034bb65f3419752db94ad71328376
Shaun De Witt <s.dewitt@jach.hawaii.edu>
2003-04-15 19:32:52 +0000

    SpIterSky.java

--- GEMINI/src/gemini/sp/iter/SpIterSky.java
+++ GEMINI/src/gemini/sp/iter/SpIterSky.java
@@ -18,31 +18,31 @@ import java.util.Enumeration;
 //
 class SpIterSkyEnumeration extends SpIterEnumeration
 {
-    private int     _curCount = 0;
-    private int     _maxCount;
+   private int     _curCount = 0;
+   private int     _maxCount;
 
-    SpIterSkyEnumeration(SpIterSky iterSky)
-    {
-	super(iterSky);
-	_maxCount    = iterSky.getCount();
-    }
+SpIterSkyEnumeration(SpIterSky iterSky)
+{
+   super(iterSky);
+   _maxCount    = iterSky.getCount();
+}
 
-    protected boolean
-	_thisHasMoreElements()
-    {
-	return (_curCount < _maxCount);
-    }
-    protected SpIterStep
-	_thisFirstElement()
-    {
-	return _thisNextElement();
-    }
+protected boolean
+_thisHasMoreElements()
+{
+   return (_curCount < _maxCount);
+}
+protected SpIterStep
+_thisFirstElement()
+{
+   return _thisNextElement();
+}
 
-    protected SpIterStep
-	_thisNextElement()
-    {
-	return new SpIterStep("sky", _curCount++, _iterComp, (SpIterValue) null);
-    }
+protected SpIterStep
+_thisNextElement()
+{
+   return new SpIterStep("sky", _curCount++, _iterComp, (SpIterValue) null);
+}
    
 }
 
@@ -53,112 +53,43 @@ class SpIterSkyEnumeration extends SpIterEnumeration
 public class SpIterSky extends SpIterObserveBase
 {
 
-    public static final String ATTR_FOLLOW_PATTERN   = "followPattern";
-    public static final String ATTR_OFFSET_SCALE     = "offsetScaleFactor";
-    public static final String ATTR_OFFSET_BOX_SIZE  = "offsetBounds";
-
-    public static final SpType SP_TYPE =
+   public static final SpType SP_TYPE =
         SpType.create(SpType.ITERATOR_COMPONENT_TYPE, "sky", "Sky");
 
-    // Register the prototype.
-    static {
-	SpFactory.registerPrototype(new SpIterSky());
-    }
+// Register the prototype.
+static {
+   SpFactory.registerPrototype(new SpIterSky());
+}
 
-    /**
-     * Default constructor.
-     */
-    public SpIterSky()
-    {
-	super(SP_TYPE);
-	_avTable.noNotifySet(ATTR_FOLLOW_PATTERN, "true", 0);
-	//   super(SpType.ITERATOR_COMPONENT_SKY);
-    }
+/**
+ * Default constructor.
+ */
+public SpIterSky()
+{
+   super(SP_TYPE);
+//   super(SpType.ITERATOR_COMPONENT_SKY);
+}
 
-    /**
-     * Override getTitle to return the sky count.
-     */
-    public String
-	getTitle()
-    {
-	if (getTitleAttr() != null) {
-	    return super.getTitle();
-	}
+/**
+ * Override getTitle to return the sky count.
+ */
+public String
+getTitle()
+{
+   if (getTitleAttr() != null) {
+      return super.getTitle();
+   }
 
-	return "Sky (" + getCount() + "X)";
-    }
+   return "Sky (" + getCount() + "X)";
+}
 
-    /**
-     * Get the Enumation of the iteration steps.
-     */
-    public SpIterEnumeration
-	elements()
-    {
-	return new SpIterSkyEnumeration(this);
-    }
+/**
+ * Get the Enumation of the iteration steps.
+ */
+public SpIterEnumeration
+elements()
+{
+   return new SpIterSkyEnumeration(this);
+}
 
-    /**
-     * Set whether we are following the current offset pattern for the observes
-     */
-    public void setFollowPattern(boolean value) {
-	_avTable.set(ATTR_FOLLOW_PATTERN, value);
-    }
-
-    /**
-     * Get whether "FollowPattern" is set
-     */
-    public boolean getFollowPattern() {
-	return _avTable.getBool(ATTR_FOLLOW_PATTERN);
-    }
-
-    /**
-     * Set the scaling of the offset values for the sky relative to the offset size
-     * of the target.
-     */
-    public void setOffsetScale(String value) {
-	_avTable.set(ATTR_OFFSET_SCALE, value);
-    }
-
-    /**
-     * Delete the offste scale factor
-     */
-    public void unsetOffsetScale() {
-	_avTable.rm(ATTR_OFFSET_SCALE);
-    }
-
-    /**
-     * Get the offset scaling factor.
-     */
-    public String getOffsetScale() {
- 	String rtn = "";
-	if ( _avTable.get( ATTR_OFFSET_SCALE ) != null ) {
-	    rtn =  _avTable.get( ATTR_OFFSET_SCALE, 0 );
-	}
-	return rtn;
-    }
-
-    /**
-     * Set the box size for pseudo-random offset patterns
-     */
-    public void setOffsetBoxSize(String value) {
-	_avTable.set(ATTR_OFFSET_BOX_SIZE, value);
-    }
-
-    /**
-     * Delete the offste box size
-     */
-    public void unsetOffsetBoxSize() {
-	_avTable.rm(ATTR_OFFSET_BOX_SIZE);
-    }
-
-    /**
-     * Get the offset scaling factor.
-     */
-    public String getOffsetBoxSize() {
-	String rtn = "";
-	if ( _avTable.get( ATTR_OFFSET_BOX_SIZE ) != null ) {
-	    rtn =  _avTable.get( ATTR_OFFSET_BOX_SIZE, 0 );
-	}
-	return rtn;
-    }
 }
