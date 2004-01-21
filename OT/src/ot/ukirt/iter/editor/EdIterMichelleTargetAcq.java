7ac714208dfaabb791b22985476f327d71706c9f
Russell Kackley <r.kackley@jach.hawaii.edu>
2004-01-21 20:09:26 +0000

    Changes for Michelle target acqusition

---
+++ OT/src/ot/ukirt/iter/editor/EdIterMichelleTargetAcq.java
@@ -0,0 +1,143 @@
+// Copyright 1997 Association for Universities for Research in Astronomy, Inc.,
+// Observatory Control System, Gemini Telescopes Project.
+// See the file COPYRIGHT for complete details.
+//
+// $Id$
+//
+package ot.ukirt.iter.editor;
+
+import orac.ukirt.iter.SpIterMichelleTargetAcq;
+
+import jsky.app.ot.gui.TextBoxWidgetExt;
+import jsky.app.ot.gui.TextBoxWidgetWatcher;
+import jsky.app.ot.gui.CommandButtonWidgetExt;
+import jsky.app.ot.gui.CommandButtonWidgetWatcher;
+import jsky.app.ot.gui.DropDownListBoxWidgetExt;
+import jsky.app.ot.gui.DropDownListBoxWidgetWatcher;
+
+import gemini.sp.SpItem;
+import gemini.sp.SpAvTable;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+import javax.swing.JComboBox;
+
+import ot.util.DialogUtil;
+import jsky.app.ot.editor.OtItemEditor;
+
+/**
+ * This is the editor for Michelle Spectroscopy Target Acquistion iterator component.
+ */
+public final class EdIterMichelleTargetAcq extends OtItemEditor
+                              implements TextBoxWidgetWatcher, ActionListener
+{
+
+    private SpIterMichelleTargetAcq  _ita;
+    private boolean haveInitialised = false;
+    private IterMichelleTargetAcqGUI _w;
+   
+   /**
+    * If true, ignore action events.
+    */
+   private boolean ignoreActions = false;
+
+/**
+ * The constructor initializes the title, description, and presentation source.
+ */
+public EdIterMichelleTargetAcq()
+{
+   _title       ="Michelle Spectroscopy Target Acquisition Iterator";
+   _presSource  = _w = new IterMichelleTargetAcqGUI();
+   _description ="Configure Michelle spectroscopy camera for target acquisition.";
+
+   _w.defaultAcquisition.addActionListener(this);
+}
+
+/**
+ */
+protected void
+_init()
+{
+
+    SpIterMichelleTargetAcq iterObs = (SpIterMichelleTargetAcq) _spItem;
+
+   super._init();
+}
+
+/**
+ * Override setup to store away a reference to the SpInstMichelle item.
+ */
+public void
+setup(SpItem spItem)
+{
+   _ita = (SpIterMichelleTargetAcq) spItem;
+
+   haveInitialised = false;
+   super.setup(spItem);
+}
+
+
+/**
+ * Implements the _updateWidgets method from OtItemEditor in order to
+ * setup the widgets to show the current values of the item.
+ */
+protected void
+_updateWidgets()
+{
+   ignoreActions = true;
+
+   _ita.useDefaultDisperser();
+   
+   try {
+       // Exposure Time
+       _w.exposureTime.setValue( _ita.getExposureTimeString() );
+   } catch (NullPointerException ex) {
+       DialogUtil.error(_w, "Can't set exposure time: probably because Michelle instrument component is in imaging mode");
+       return;
+   }
+
+   // Observation Time
+   _w.observationTime.setValue( _ita.getObservationTime() );
+
+   // Disperser
+   _w.disperser.setValue( _ita.getDisperser() );
+
+   // Update data acquisition config
+   _ita.updateDAConf();
+
+   ignoreActions = false;
+}
+
+/**
+ * Watch changes to text boxes
+ */
+public void
+textBoxKeyPress(TextBoxWidgetExt tbwe) {}
+  
+/**
+ * Text box action.
+ */
+public void
+textBoxAction(TextBoxWidgetExt tbwe) {}
+ 
+/**
+ *
+ */
+public void
+actionPerformed(ActionEvent evt)
+{
+   if(ignoreActions)
+      return;
+ 
+   Object w = evt.getSource();
+
+   if (w == _w.defaultAcquisition) {
+      _ita.useDefaultAcquisition();
+      _updateWidgets();
+      return;
+   }
+}
+
+}
+
---
+++ OT/src/ot/ukirt/iter/editor/IterMichelleTargetAcqGUI.java
@@ -0,0 +1,90 @@
+
+/**
+ * Title:        JSky<p>
+ * Description:  <p>
+ * Copyright:    Copyright (c) Allan Brighton<p>
+ * Company:      <p>
+ * @author Allan Brighton
+ * @version 1.0
+ */
+package ot.ukirt.iter.editor;
+
+import java.awt.*;
+import javax.swing.*;
+import jsky.app.ot.gui.*;
+
+public class IterMichelleTargetAcqGUI extends JPanel {
+    GridBagLayout gridBagLayout1 = new GridBagLayout();
+    JLabel jLabel2 = new JLabel();
+    JLabel jLabel1 = new JLabel();
+    JLabel jLabel3 = new JLabel();
+    JLabel jLabel5 = new JLabel();
+    JLabel jLabel6 = new JLabel();
+    TextBoxWidgetExt exposureTime = new TextBoxWidgetExt();
+    TextBoxWidgetExt observationTime = new TextBoxWidgetExt();
+    TextBoxWidgetExt disperser = new TextBoxWidgetExt();
+    CommandButtonWidgetExt defaultAcquisition = new CommandButtonWidgetExt();
+
+    public IterMichelleTargetAcqGUI() {
+        try {
+            jbInit();
+        }
+        catch(Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    void jbInit() throws Exception {
+        jLabel1.setText("Obs. Time");
+        jLabel1.setForeground(Color.black);
+        jLabel1.setFont(new java.awt.Font("Dialog", 0, 12));
+        jLabel2.setText("(sec)");
+        jLabel2.setForeground(Color.black);
+        jLabel2.setFont(new java.awt.Font("Dialog", 0, 10));
+        jLabel3.setText("Grating");
+        jLabel3.setForeground(Color.black);
+        jLabel3.setFont(new java.awt.Font("Dialog", 0, 12));
+        this.setMinimumSize(new Dimension(280, 278));
+        this.setPreferredSize(new Dimension(280, 278));
+        this.setLayout(gridBagLayout1);
+        jLabel5.setText("(sec)");
+        jLabel5.setForeground(Color.black);
+        jLabel5.setFont(new java.awt.Font("Dialog", 0, 10));
+        jLabel6.setText("Exp. Time");
+        jLabel6.setForeground(Color.black);
+        jLabel6.setFont(new java.awt.Font("Dialog", 0, 12));
+    
+	exposureTime.setBackground(new Color(220, 220, 220));
+	exposureTime.setBorder(BorderFactory.createLoweredBevelBorder());
+	exposureTime.setEditable(false);
+
+	observationTime.setBackground(new Color(220, 220, 220));
+	observationTime.setBorder(BorderFactory.createLoweredBevelBorder());
+	observationTime.setEditable(false);
+
+	disperser.setBackground(new Color(220, 220, 220));
+	disperser.setBorder(BorderFactory.createLoweredBevelBorder());
+	disperser.setEditable(false);
+
+        defaultAcquisition.setText("Reset to Default");
+	this.add(jLabel2, new GridBagConstraints(2, 2, 1, 1, 0.0, 0.0
+            ,GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 0, 0));
+        this.add(jLabel1, new GridBagConstraints(0, 2, 1, 1, 0.0, 0.0
+            ,GridBagConstraints.EAST, GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 0, 0));
+         this.add(jLabel3, new GridBagConstraints(0, 3, 1, 1, 0.0, 0.0
+            ,GridBagConstraints.EAST, GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 0, 0));
+        this.add(jLabel5, new GridBagConstraints(2, 1, 1, 1, 0.0, 0.0
+            ,GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 0, 0));
+        this.add(jLabel6, new GridBagConstraints(0, 1, 1, 1, 0.0, 0.0
+            ,GridBagConstraints.EAST, GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 0, 0));
+
+        this.add(exposureTime, new GridBagConstraints(1, 1, 1, 1, 0.0, 0.0
+            ,GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 0), 0, 0));
+        this.add(observationTime, new GridBagConstraints(1, 2, 1, 1, 0.0, 0.0
+            ,GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 0), 0, 0));
+        this.add(disperser, new GridBagConstraints(1, 3, 1, 1, 0.0, 0.0
+            ,GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 0), 0, 0));
+    this.add(defaultAcquisition, new GridBagConstraints(0, 4, 3, 1, 0.0, 0.0
+            ,GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(10, 0, 0, 0), 0, 0));
+    }
+}
