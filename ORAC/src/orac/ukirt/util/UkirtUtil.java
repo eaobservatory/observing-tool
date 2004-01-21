3932b6047399a35a78d3b17b48219f9d9f51bc4d
Shaun De Witt <s.dewitt@jach.hawaii.edu>
2004-01-21 19:25:04 +0000

    Allowed proper motion for UKIRT

--- ORAC/src/orac/ukirt/util/UkirtUtil.java
+++ ORAC/src/orac/ukirt/util/UkirtUtil.java
@@ -54,7 +54,7 @@ public class UkirtUtil implements TelescopeUtil {
     switch(feature) {
       case FEATURE_TARGET_INFO_CHOP:        return true;
       case FEATURE_FLAG_AS_STANDARD:        return true;
-      case FEATURE_TARGET_INFO_PROP_MOTION: return false;
+      case FEATURE_TARGET_INFO_PROP_MOTION: return true;
       case FEATURE_TARGET_INFO_TRACKING:    return false;
       default:                              return false;
     }
