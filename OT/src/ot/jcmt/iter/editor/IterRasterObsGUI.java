63fdd2bde057decbc694f7acc866bd656f0be476
Sam Hart <s.hart@jach.hawaii.edu>
2006-07-18 20:45:28 +0000

    Formatting -> SH

--- OT/src/ot/jcmt/iter/editor/IterRasterObsGUI.java
+++ OT/src/ot/jcmt/iter/editor/IterRasterObsGUI.java
@@ -7,25 +7,24 @@
 /*                                                              */
 /*==============================================================*/
 // $Id$
-
 package ot.jcmt.iter.editor;
 
-import javax.swing.JPanel ;
-import javax.swing.JLabel ;
-import javax.swing.BorderFactory ;
-import java.awt.GridBagLayout ;
-import java.awt.BorderLayout ;
-import java.awt.Color ;
-import java.awt.GridBagConstraints ;
-import java.awt.Insets ; 
-import jsky.app.ot.gui.DropDownListBoxWidgetExt ;
-import jsky.app.ot.gui.TextBoxWidgetExt ;
-import jsky.app.ot.gui.CommandButtonWidgetExt ;
-import jsky.app.ot.gui.OptionWidgetExt ;
-import jsky.app.ot.gui.CheckBoxWidgetExt ;
-import javax.swing.border.Border ;
-import javax.swing.border.BevelBorder ;
-import javax.swing.border.TitledBorder ;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.BorderFactory;
+import java.awt.GridBagLayout;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import jsky.app.ot.gui.DropDownListBoxWidgetExt;
+import jsky.app.ot.gui.TextBoxWidgetExt;
+import jsky.app.ot.gui.CommandButtonWidgetExt;
+import jsky.app.ot.gui.OptionWidgetExt;
+import jsky.app.ot.gui.CheckBoxWidgetExt;
+import javax.swing.border.Border;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.TitledBorder;
 
 import orac.util.JThermometer;
 
@@ -38,65 +37,64 @@ import orac.util.JThermometer;
  * @version 1.0
  */
 
+public class IterRasterObsGUI extends IterJCMTGenericGUI
+{
+	JPanel scubaAcsisPanel = new JPanel();
+	JPanel areaPanel = new JPanel();
+	JPanel scanPanel = new JPanel();
+	GridBagLayout gridBagLayout2 = new GridBagLayout();
+	GridBagLayout gridBagLayout3 = new GridBagLayout();
+	JLabel jLabel1 = new JLabel();
+	JLabel jLabel2 = new JLabel();
+	JLabel jLabel3 = new JLabel();
+	JLabel scanSystemLabel = new JLabel();
+	DropDownListBoxWidgetExt scanSystem = new DropDownListBoxWidgetExt();
+	TextBoxWidgetExt dx = new TextBoxWidgetExt();
+	TextBoxWidgetExt dy = new TextBoxWidgetExt();
+	TextBoxWidgetExt width = new TextBoxWidgetExt();
+	JLabel arcSecsLabel1 = new JLabel();
+	JLabel arcSecsLabel2 = new JLabel();
+	JLabel arcSecsLabel3 = new JLabel();
+	TextBoxWidgetExt height = new TextBoxWidgetExt();
+	JLabel arcSecsLabel4 = new JLabel();
+	JLabel jLabel21 = new JLabel();
+	TextBoxWidgetExt posAngle = new TextBoxWidgetExt();
+	JLabel jLabel23 = new JLabel();
+	Border border1;
+	JLabel jLabel14 = new JLabel();
+	DropDownListBoxWidgetExt sampleTime = new DropDownListBoxWidgetExt();
+	TextBoxWidgetExt acsisSampleTime = new TextBoxWidgetExt();
+	TextBoxWidgetExt secsPerObservation = new TextBoxWidgetExt();
+	GridBagLayout gridBagLayout1 = new GridBagLayout();
+	TextBoxWidgetExt secsPerRow = new TextBoxWidgetExt();
+	JLabel jLabel13 = new JLabel();
+	JLabel jLabel10 = new JLabel();
+	JPanel heterodynePanel = new JPanel();
+	OptionWidgetExt alongRow = new OptionWidgetExt();
+	CommandButtonWidgetExt defaultButton = new CommandButtonWidgetExt();
+	OptionWidgetExt interleaved = new OptionWidgetExt();
+	TextBoxWidgetExt rowsPerCal = new TextBoxWidgetExt();
+	CheckBoxWidgetExt rowReversal = new CheckBoxWidgetExt();
+	JLabel jLabel9 = new JLabel();
+	JLabel jLabel8 = new JLabel();
+	TextBoxWidgetExt rowsPerRef = new TextBoxWidgetExt();
+	JLabel jLabel16 = new JLabel();
+	DropDownListBoxWidgetExt scanAngle = new DropDownListBoxWidgetExt();
+	JLabel jLabel17 = new JLabel() ;
+	JThermometer thermometer = new JThermometer();
+	JLabel estimationLabel = new JLabel();
 
-public class IterRasterObsGUI extends IterJCMTGenericGUI {
-
-  JPanel scubaAcsisPanel = new JPanel();
-  JPanel areaPanel = new JPanel();
-  JPanel scanPanel = new JPanel();
-  GridBagLayout gridBagLayout2 = new GridBagLayout();
-  GridBagLayout gridBagLayout3 = new GridBagLayout();
-  JLabel jLabel1 = new JLabel();
-  JLabel jLabel2 = new JLabel();
-  JLabel jLabel3 = new JLabel();
-  JLabel scanSystemLabel = new JLabel();
-  DropDownListBoxWidgetExt scanSystem = new DropDownListBoxWidgetExt();
-  TextBoxWidgetExt dx = new TextBoxWidgetExt();
-  TextBoxWidgetExt dy = new TextBoxWidgetExt();
-  TextBoxWidgetExt width = new TextBoxWidgetExt();
-  JLabel arcSecsLabel1 = new JLabel();
-  JLabel arcSecsLabel2 = new JLabel();
-  JLabel arcSecsLabel3 = new JLabel();
-  TextBoxWidgetExt height = new TextBoxWidgetExt();
-  JLabel arcSecsLabel4 = new JLabel();
-  JLabel jLabel21 = new JLabel();
-  TextBoxWidgetExt posAngle = new TextBoxWidgetExt();
-  JLabel jLabel23 = new JLabel();
-  Border border1;
-  JLabel jLabel14 = new JLabel();
-  DropDownListBoxWidgetExt sampleTime = new DropDownListBoxWidgetExt();
-  TextBoxWidgetExt acsisSampleTime = new TextBoxWidgetExt();
-  TextBoxWidgetExt secsPerObservation = new TextBoxWidgetExt();
-  GridBagLayout gridBagLayout1 = new GridBagLayout();
-  TextBoxWidgetExt secsPerRow = new TextBoxWidgetExt();
-  JLabel jLabel13 = new JLabel();
-  JLabel jLabel10 = new JLabel();
-  JPanel heterodynePanel = new JPanel();
-  OptionWidgetExt alongRow = new OptionWidgetExt();
-  CommandButtonWidgetExt defaultButton = new CommandButtonWidgetExt();
-  OptionWidgetExt interleaved = new OptionWidgetExt();
-  TextBoxWidgetExt rowsPerCal = new TextBoxWidgetExt();
-  CheckBoxWidgetExt rowReversal = new CheckBoxWidgetExt();
-  JLabel jLabel9 = new JLabel();
-  JLabel jLabel8 = new JLabel();
-  TextBoxWidgetExt rowsPerRef = new TextBoxWidgetExt();
-  JLabel jLabel16 = new JLabel();
-  DropDownListBoxWidgetExt scanAngle = new DropDownListBoxWidgetExt();
-  JLabel jLabel17 = new JLabel();
-//  CheckBoxWidgetExt continuumMode = new CheckBoxWidgetExt();
-
-  JThermometer thermometer = new JThermometer();
-  
-  JLabel estimationLabel = new JLabel();
-
-  public IterRasterObsGUI() {
-    try {
-      jbInit();
-    }
-    catch(Exception e) {
-      e.printStackTrace();
-    }
-  }
+	public IterRasterObsGUI()
+	{
+		try
+		{
+			jbInit();
+		}
+		catch( Exception e )
+		{
+			e.printStackTrace();
+		}
+	}
 
 	private void jbInit() throws Exception
 	{
@@ -155,9 +153,9 @@ public class IterRasterObsGUI extends IterJCMTGenericGUI {
 		jLabel10.setFont( new java.awt.Font( "Dialog" , 0 , 12 ) );
 		jLabel10.setForeground( Color.black );
 		jLabel10.setText( "Secs/Row" );
-		estimationLabel.setFont( new java.awt.Font( "Dialog" , 0 , 12 ) ) ;
-		estimationLabel.setForeground( Color.black ) ;
-		estimationLabel.setText( "(estimated)" ) ;
+		estimationLabel.setFont( new java.awt.Font( "Dialog" , 0 , 12 ) );
+		estimationLabel.setForeground( Color.black );
+		estimationLabel.setText( "(estimated)" );
 		heterodynePanel.setLayout( gridBagLayout1 );
 		heterodynePanel.setBorder( new TitledBorder( BorderFactory.createLineBorder( new Color( 153 , 153 , 153 ) , 2 ) , "Heterodyne Details" ) );
 		alongRow.setText( "Along Row" );
@@ -180,11 +178,6 @@ public class IterRasterObsGUI extends IterJCMTGenericGUI {
 		jLabel17.setForeground( Color.black );
 		jLabel17.setText( "(degrees)" );
 		scanAngle.setFont( new java.awt.Font( "Dialog" , 0 , 12 ) );
-/*
-		continuumMode.setText( "Continuum Mode" );
-		continuumMode.setFont( new java.awt.Font( "Dialog" , 0 , 12 ) );
-		continuumMode.setForeground( Color.black );
-*/
 		this.add( rasterPanel , BorderLayout.CENTER );
 		rasterPanel.add( scubaAcsisPanel , BorderLayout.WEST );
 		scubaAcsisPanel.add( areaPanel , BorderLayout.CENTER );
@@ -200,10 +193,10 @@ public class IterRasterObsGUI extends IterJCMTGenericGUI {
 		areaPanel.add( dx , new GridBagConstraints( 1 , 3 , 1 , 1 , 1.0 , 0.0 , GridBagConstraints.CENTER , GridBagConstraints.HORIZONTAL , new Insets( 2 , 2 , 2 , 2 ) , 0 , 0 ) );
 		areaPanel.add( dy , new GridBagConstraints( 1 , 5 , 1 , 1 , 0.0 , 0.0 , GridBagConstraints.CENTER , GridBagConstraints.HORIZONTAL , new Insets( 2 , 2 , 2 , 2 ) , 0 , 0 ) );
 		areaPanel.add( width , new GridBagConstraints( 1 , 0 , 1 , 1 , 0.0 , 0.0 , GridBagConstraints.CENTER , GridBagConstraints.HORIZONTAL , new Insets( 2 , 2 , 2 , 2 ) , 0 , 0 ) );
-		areaPanel.add( arcSecsLabel1 , new GridBagConstraints( 2 , 3 , 1 , 1 , 0.0 , 0.0 , GridBagConstraints.CENTER , GridBagConstraints.NONE , new Insets( 0 , 0 , 0 , 0 ) , 0 , 0 ) );		
+		areaPanel.add( arcSecsLabel1 , new GridBagConstraints( 2 , 3 , 1 , 1 , 0.0 , 0.0 , GridBagConstraints.CENTER , GridBagConstraints.NONE , new Insets( 0 , 0 , 0 , 0 ) , 0 , 0 ) );
 		areaPanel.add( arcSecsLabel2 , new GridBagConstraints( 2 , 0 , 1 , 1 , 0.0 , 0.0 , GridBagConstraints.CENTER , GridBagConstraints.NONE , new Insets( 0 , 0 , 0 , 0 ) , 0 , 0 ) );
 		areaPanel.add( arcSecsLabel3 , new GridBagConstraints( 2 , 1 , 1 , 1 , 0.0 , 0.0 , GridBagConstraints.CENTER , GridBagConstraints.NONE , new Insets( 0 , 0 , 0 , 0 ) , 0 , 0 ) );
-		areaPanel.add( height , new GridBagConstraints( 1 , 1 , 1 , 1 , 0.0 , 0.0 , GridBagConstraints.CENTER , GridBagConstraints.HORIZONTAL , new Insets( 2 , 2 , 2 , 2 ) , 0 , 0 ) );		
+		areaPanel.add( height , new GridBagConstraints( 1 , 1 , 1 , 1 , 0.0 , 0.0 , GridBagConstraints.CENTER , GridBagConstraints.HORIZONTAL , new Insets( 2 , 2 , 2 , 2 ) , 0 , 0 ) );
 		areaPanel.add( arcSecsLabel4 , new GridBagConstraints( 2 , 5 , 1 , 1 , 0.0 , 0.0 , GridBagConstraints.CENTER , GridBagConstraints.NONE , new Insets( 0 , 0 , 0 , 0 ) , 0 , 0 ) );
 		areaPanel.add( jLabel21 , new GridBagConstraints( 0 , 1 , 1 , 1 , 0.0 , 0.0 , GridBagConstraints.WEST , GridBagConstraints.NONE , new Insets( 0 , 5 , 0 , 0 ) , 0 , 0 ) );
 		areaPanel.add( posAngle , new GridBagConstraints( 1 , 6 , 1 , 1 , 0.0 , 0.0 , GridBagConstraints.CENTER , GridBagConstraints.HORIZONTAL , new Insets( 2 , 2 , 2 , 2 ) , 0 , 0 ) );
@@ -216,11 +209,10 @@ public class IterRasterObsGUI extends IterJCMTGenericGUI {
 		heterodynePanel.add( jLabel9 , new GridBagConstraints( 2 , 4 , 1 , 1 , 0.0 , 0.0 , GridBagConstraints.CENTER , GridBagConstraints.NONE , new Insets( 0 , 0 , 0 , 5 ) , 0 , 0 ) );
 		heterodynePanel.add( defaultButton , new GridBagConstraints( 1 , 5 , 2 , 1 , 0.0 , 0.0 , GridBagConstraints.WEST , GridBagConstraints.NONE , new Insets( 5 , 5 , 5 , 5 ) , 0 , 0 ) );
 		heterodynePanel.add( jLabel10 , new GridBagConstraints( 0 , 6 , 1 , 1 , 0.0 , 0.0 , GridBagConstraints.WEST , GridBagConstraints.NONE , new Insets( 0 , 5 , 0 , 0 ) , 0 , 0 ) );
-		heterodynePanel.add( estimationLabel , new GridBagConstraints( 0 , 7 , 1 , 1 , 0.0 , 0.0 , GridBagConstraints.WEST , GridBagConstraints.NONE , new Insets( 0 , 5 , 0 , 0 ) , 0 , 0 ) );		
+		heterodynePanel.add( estimationLabel , new GridBagConstraints( 0 , 7 , 1 , 1 , 0.0 , 0.0 , GridBagConstraints.WEST , GridBagConstraints.NONE , new Insets( 0 , 5 , 0 , 0 ) , 0 , 0 ) );
 		heterodynePanel.add( jLabel13 , new GridBagConstraints( 0 , 9 , 1 , 1 , 0.0 , 0.0 , GridBagConstraints.WEST , GridBagConstraints.NONE , new Insets( 0 , 5 , 0 , 0 ) , 0 , 0 ) );
 		heterodynePanel.add( secsPerRow , new GridBagConstraints( 1 , 6 , 1 , 1 , 0.0 , 0.0 , GridBagConstraints.CENTER , GridBagConstraints.HORIZONTAL , new Insets( 5 , 5 , 5 , 5 ) , 0 , 0 ) );
 		heterodynePanel.add( secsPerObservation , new GridBagConstraints( 1 , 9 , 1 , 1 , 0.0 , 0.0 , GridBagConstraints.CENTER , GridBagConstraints.HORIZONTAL , new Insets( 5 , 5 , 5 , 5 ) , 0 , 0 ) );
-//		heterodynePanel.add( continuumMode , new GridBagConstraints( 1 , 10 , 1 , 1 , 0.0 , 0.0 , GridBagConstraints.WEST , GridBagConstraints.NONE , new Insets( 5 , 5 , 5 , 5 ) , 0 , 0 ) );
 		heterodynePanel.add( thermometer , new GridBagConstraints( 1 , 11 , 1 , 1 , 0.0 , 0.0 , GridBagConstraints.WEST , GridBagConstraints.HORIZONTAL , new Insets( 0 , 0 , 0 , 0 ) , 0 , 0 ) );
 	}
 }
