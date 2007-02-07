0e1c4ea48acc653596c296dfc9f5fb4fb645bec4
Sam Hart <s.hart@jach.hawaii.edu>
2007-02-07 21:17:58 +0000

    Get those sliders closer to reality -> SH

--- EDFREQ/src/edfreq/SideBandDisplay.java
+++ EDFREQ/src/edfreq/SideBandDisplay.java
@@ -139,7 +139,7 @@ public class SideBandDisplay extends JFrame implements ChangeListener, MouseList
 		// System.out.println("\tredshift="+redshift);
 		// System.out.println("\tdisplayWidth="+displayWidth);
 		// System.out.println("\tsamplerCount="+samplerCount);
-		el = new EmissionLines( lowIF , highIF , redshift , displayWidth , 20 , samplerCount );
+		el = new EmissionLines( lowIF , highIF , redshift , displayWidth + 6 , 20 , samplerCount );
 		// System.out.println("el=" + el);
 
 		jt = new FrequencyTable( feIF , feBandWidth , bandWidths , channels , samplerCount , displayWidth + 6 , this , hetEditor , el , nMixers );
