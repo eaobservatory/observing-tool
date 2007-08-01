94b111a7eb7256ea2df6f65f49bbd195d6134d02
Sam Hart <s.hart@jach.hawaii.edu>
2007-08-01 01:26:55 +0000

    Reformat and minor rewrite -> SH

--- OT/src/ot/jcmt/iter/editor/EdIterPointingObs.java
+++ OT/src/ot/jcmt/iter/editor/EdIterPointingObs.java
@@ -7,30 +7,18 @@
 /*                                                              */
 /*==============================================================*/
 // $Id$
-
 package ot.jcmt.iter.editor;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.CardLayout;
 
-import javax.swing.ButtonGroup;
-import javax.swing.DefaultComboBoxModel;
-import javax.swing.JComboBox;
-import javax.swing.JMenu;
 import javax.swing.JMenuItem;
-import javax.swing.MenuElement;
-import java.awt.Font;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 
 import jsky.app.ot.gui.CommandButtonWidgetExt;
 import jsky.app.ot.gui.CommandButtonWidgetWatcher;
-import jsky.app.ot.gui.TextBoxWidgetExt;
 import jsky.app.ot.gui.OptionWidgetExt;
 import jsky.app.ot.gui.OptionWidgetWatcher;
 
-import gemini.sp.SpAvTable;
 import gemini.sp.SpItem;
 import gemini.sp.obsComp.SpInstObsComp;
 import orac.jcmt.inst.SpInstHeterodyne;
@@ -41,125 +29,79 @@ import orac.jcmt.iter.SpIterPointingObs;
  *
  * @author modified for JCMT by Martin Folger ( M.Folger@roe.ac.uk )
  */
-public final class EdIterPointingObs extends EdIterJCMTGeneric
-  implements CommandButtonWidgetWatcher, OptionWidgetWatcher, ActionListener {
+public final class EdIterPointingObs extends EdIterJCMTGeneric implements CommandButtonWidgetWatcher , OptionWidgetWatcher , ActionListener
+{
 
-  private IterPointingObsGUI _w;       // the GUI layout panel
+	private IterPointingObsGUI _w; // the GUI layout panel
 
-  private SpIterPointingObs _iterObs;
+	private SpIterPointingObs _iterObs;
 
-    private static final String POINTING_METHOD = "5-point";
+	/**
+	 * The constructor initializes the title, description, and presentation source.
+	 */
+	public EdIterPointingObs()
+	{
+		super( new IterPointingObsGUI() );
 
-  /**
-   * The constructor initializes the title, description, and presentation source.
-   */
-  public EdIterPointingObs() {
-    super(new IterPointingObsGUI());
+		_title = "Pointing";
+		_presSource = _w = ( IterPointingObsGUI )super._w;
+		_description = "Pointing Observation Mode";
 
-    _title       ="Pointing";
-    _presSource  = _w = (IterPointingObsGUI)super._w;
-    _description ="Pointing Observation Mode";
+		_w.pointingPixelButton.addWatcher( this );
 
-//    ButtonGroup grp = new ButtonGroup();
-//    grp.add(_w.continuum);
-//    grp.add(_w.spectralLine);
+		_w.automaticTarget.setToolTipText( "Automatically determine pointing target at time of observation" );
 
-//    _w.continuum.setActionCommand(SpIterPointingObs.SPECTRAL_MODE_CONTINUUM);
-//    _w.spectralLine.setActionCommand(SpIterPointingObs.SPECTRAL_MODE_SPECTRAL_LINE);
+		_w.frequencyPanel.setVisible( false );
+		_w.switchingModeLabel.setVisible( false );
+		_w.switchingMode.setVisible( false );
+	}
 
-//     _w.pointingMethod.setChoices(SpIterPointingObs.POINTING_METHODS);
-//    _w.pointingMethod.addItem(POINTING_METHOD);
+	/**
+	 * Override setup to store away a reference to the Pointing Iterator.
+	 */
+	public void setup( SpItem spItem )
+	{
+		_iterObs = ( SpIterPointingObs )spItem;
+		super.setup( spItem );
+	}
 
-//    MenuElement [] menuElements = _w.pointingPixelPopupMenu.getSubElements();
-//    MenuElement [] subElements  = null;
+	protected void _updateWidgets()
+	{
+		super._updateWidgets();
+	}
 
-//    Font font = new Font("Dialog", Font.PLAIN, 12);
-//    JMenuItem menuItem = new JMenuItem(SpIterPointingObs.POINTING_PIXEL_AUTOMATIC);
-//    menuItem.setFont(font);
-//    menuItem.addActionListener(this);
-//    _w.pointingPixelPopupMenu.add(menuItem);
+	public void commandButtonAction( CommandButtonWidgetExt cbwe )
+	{
+		_w.pointingPixelPopupMenu.show( _w.pointingPixelButton , 0 , 0 );
+	}
 
-//    JMenu menu = new JMenu(SpIterPointingObs.POINTING_PIXEL_MANUAL);
-//    menu.setFont(font);
-//    menu.addActionListener(this);
-//    _w.pointingPixelPopupMenu.add(menu);
+	public void actionPerformed( ActionEvent e )
+	{
+		if( e.getSource() instanceof JMenuItem )
+		{
+			_w.pointingPixelButton.setText( ( ( JMenuItem )( e.getSource() ) ).getText() );
+			_iterObs.setPointingPixel( ( ( JMenuItem )( e.getSource() ) ).getText() );
+		}
+	}
 
-//    for(int i = 0; i < SpIterPointingObs.POINTING_PIXEL_MANUAL_CHOICES.length; i++) {
-//      menuItem = new JMenuItem(SpIterPointingObs.POINTING_PIXEL_MANUAL_CHOICES[i]);
-//      menuItem.setFont(font);
-//      menuItem.addActionListener(this);
-//      menu.add(menuItem);
-//    }
+	public void optionAction( OptionWidgetExt owe )
+	{
+		_iterObs.setSpectralMode( owe.getActionCommand() );
+	}
 
-    _w.pointingPixelButton.addWatcher(this);
-//    _w.continuum.addWatcher(this);
-//    _w.spectralLine.addWatcher(this);
+	public void setInstrument( SpInstObsComp spInstObsComp )
+	{
+		super.setInstrument( spInstObsComp );
 
-    _w.automaticTarget.setToolTipText("Automatically determine pointing target at time of observation");
-
-    _w.frequencyPanel.setVisible(false);
-    _w.switchingModeLabel.setVisible(false);
-    _w.switchingMode.setVisible(false);
-  }
-
-  /**
-   * Override setup to store away a reference to the Pointing Iterator.
-   */
-  public void setup(SpItem spItem) {
-    _iterObs = (SpIterPointingObs) spItem;
-    super.setup(spItem);
-  }
-
-  protected void _updateWidgets() {
-//    _w.pointingPixelButton.setText(_iterObs.getPointingPixel());
-
-//    if(SpIterPointingObs.SPECTRAL_MODE_CONTINUUM.equals(_iterObs.getSpectralMode())) {
-//      _w.continuum.setSelected(true);
-//    }
-//    else {
-//      _w.spectralLine.setSelected(true);
-//    }
-
-//    _w.switchingMode.setValue(IterJCMTGenericGUI.BEAM);
-//    _w.switchingMode.setEnabled(false);
-
-    super._updateWidgets();
-  }
-
-
-//  public void textBoxAction(TextBoxWidgetExt tbwe) {
-//    super.textBoxAction(tbwe);
-//  }
-
-  public void commandButtonAction(CommandButtonWidgetExt cbwe) {
-    _w.pointingPixelPopupMenu.show(_w.pointingPixelButton, 0, 0);
-  }
-
-  public void actionPerformed(ActionEvent e) {
-    if(e.getSource() instanceof JMenuItem) {
-      _w.pointingPixelButton.setText(((JMenuItem)(e.getSource())).getText());
-      _iterObs.setPointingPixel(((JMenuItem)(e.getSource())).getText());
-      return;
-    }
-//    System.out.println("ChangeEvent.toString() = " + e.toString());
-//    System.out.println("PopupMenuEvent.getSource() = " + e.getSource());
-  }
- 
-  public void optionAction(OptionWidgetExt owe) {
-    _iterObs.setSpectralMode(owe.getActionCommand());
-  }
-
-  public void setInstrument(SpInstObsComp spInstObsComp) {
-    super.setInstrument(spInstObsComp);
-
-    if((spInstObsComp != null) && (spInstObsComp instanceof SpInstHeterodyne)) {
-      _w.switchingMode.setVisible(false);
-      _w.switchingModeLabel.setVisible(false);
-      _w.acsisPanel.setVisible(false);
-    }
-    else {
-      _w.acsisPanel.setVisible(false);
-    }
-  }
+		if( ( spInstObsComp != null ) && ( spInstObsComp instanceof SpInstHeterodyne ) )
+		{
+			_w.switchingMode.setVisible( false );
+			_w.switchingModeLabel.setVisible( false );
+			_w.acsisPanel.setVisible( false );
+		}
+		else
+		{
+			_w.acsisPanel.setVisible( false );
+		}
+	}
 }
-
