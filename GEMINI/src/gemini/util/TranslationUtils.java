7c93a89f3f7ffb3fe8e46decbeea0a38d12f2469
Sam Hart <s.hart@jach.hawaii.edu>
2008-04-01 02:03:03 +0000

    Missed from last commit, which would have broken compilation. Whoops -> SH

---
+++ GEMINI/src/gemini/util/TranslationUtils.java
@@ -0,0 +1,41 @@
+package gemini.util ;
+
+import java.util.Vector ;
+import java.util.Enumeration ;
+import gemini.sp.SpTranslatable ;
+import gemini.sp.SpTranslationNotSupportedException ;
+import gemini.sp.SpItem ;
+
+public class TranslationUtils
+{
+	public static void recurse( Enumeration e , Vector<String> v ) throws SpTranslationNotSupportedException
+	{
+			if( e == null )
+				throw new SpTranslationNotSupportedException() ;
+			if( v == null )
+				throw new SpTranslationNotSupportedException() ;
+			
+			SpTranslatable translatable = null ;
+			SpTranslatable previous = null ;
+			while( e.hasMoreElements() )
+			{
+				SpItem child = ( SpItem )e.nextElement();
+				if( child instanceof SpTranslatable )
+				{
+					translatable = ( SpTranslatable )child ;
+					if( !translatable.equals( previous ) )
+					{
+						if( previous != null )
+						{
+							previous.translateEpilog( v ) ;
+							previous = translatable ;
+						}
+						translatable.translateProlog( v ) ;
+					}
+					translatable.translate( v ) ;
+				}
+			}
+			if( translatable != null  )
+				translatable.translateEpilog( v ) ;
+	}
+}
