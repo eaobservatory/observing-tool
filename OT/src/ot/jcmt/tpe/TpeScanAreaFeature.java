9c2c9bb2db5e24c8be10c978490e94c1fcb0229b
Sam Hart <s.hart@jach.hawaii.edu>
2008-06-12 21:13:27 +0000

    Scan/Raster for TPE -> SH

--- OT/src/ot/jcmt/tpe/TpeScanAreaFeature.java
+++ OT/src/ot/jcmt/tpe/TpeScanAreaFeature.java
@@ -13,25 +13,30 @@
 //
 // $Id$
 //
-package ot.jcmt.tpe;
+package ot.jcmt.tpe ;
 
-import java.awt.Color;
-import java.awt.Graphics;
+import java.awt.Color ;
+import java.awt.Graphics ;
 
-import jsky.app.ot.gui.DrawUtil;
+import jsky.app.ot.gui.DrawUtil ;
 
-import orac.jcmt.iter.SpIterRasterObs;
+import orac.jcmt.iter.SpIterRasterObs ;
 
-import jsky.app.ot.fits.gui.FitsImageInfo;
-import jsky.app.ot.fits.gui.FitsMouseEvent;
+import jsky.app.ot.fits.gui.FitsImageInfo ;
+import jsky.app.ot.fits.gui.FitsMouseEvent ;
 
-import jsky.app.ot.util.Angle;
-import jsky.app.ot.util.PolygonD;
+import jsky.app.ot.util.Angle ;
+import jsky.app.ot.util.PolygonD ;
 
-import jsky.app.ot.tpe.TpeImageFeature;
-import jsky.app.ot.tpe.TpeDraggableFeature;
-import jsky.app.ot.tpe.TpeImageWidget;
-import java.awt.geom.Point2D;
+import jsky.app.ot.tpe.TpeImageFeature ;
+import jsky.app.ot.tpe.TpeDraggableFeature ;
+import jsky.app.ot.tpe.TpeImageWidget ;
+import java.awt.geom.Point2D ;
+
+import gemini.sp.SpItem ;
+import gemini.sp.SpTreeMan ;
+
+import java.util.Vector ;
 
 /**
  * An implementation class used to simplify the job of rotating scan
@@ -39,71 +44,71 @@ import java.awt.geom.Point2D;
  */
 final class TpeScanAreaDragObject
 {
-	int _xb , _yb;
-	double _angle = 0;
+	int _xb , _yb ;
+	double _angle = 0 ;
 
 	/**
 	 * Construct with the base position at (xb,yb).
 	 */
 	public TpeScanAreaDragObject( int xb , int yb , int x , int y )
 	{
-		_xb = xb;
-		_yb = yb;
-		_angle = getAngle( x , y );
+		_xb = xb ;
+		_yb = yb ;
+		_angle = getAngle( x , y ) ;
 	}
 
 	public double nextAngleDiff( int x , int y )
 	{
-		double angle = getAngle( x , y );
+		double angle = getAngle( x , y ) ;
 
-		double val = angle - _angle;
-		_angle = angle;
+		double val = angle - _angle ;
+		_angle = angle ;
 
-		return val;
+		return val ;
 	}
 
 	public double getAngle( int x , int y )
 	{
-		double angle;
+		double angle ;
 
 		// All the points are in screen coordinates, which means y increases down
 		// This makes x and y relative to the origin in a right side up frame.
-		int xp = x - _xb;
-		int yp = _yb - y;
+		int xp = x - _xb ;
+		int yp = _yb - y ;
 
-		int xa = Math.abs( xp );
-		int ya = Math.abs( yp );
+		int xa = Math.abs( xp ) ;
+		int ya = Math.abs( yp ) ;
 
 		if( xa == 0 )
 		{
 			if( yp >= 0 )
-				return Math.PI * 0.5;
+				return Math.PI * 0.5 ;
 			else
-				return Math.PI * 1.5;
+				return Math.PI * 1.5 ;
 		}
 
-		angle = Angle.atanRadians( ( ( double )ya ) / ( ( double )xa ) );
+		angle = Angle.atanRadians( ( ( double )ya ) / ( ( double )xa ) ) ;
 
 		if( ( xp > 0 ) && ( yp >= 0 ) )
-			return angle;
+			return angle ;
 
 		if( ( xp < 0 ) && ( yp >= 0 ) )
-			return Math.PI - angle;
+			return Math.PI - angle ;
 
 		if( ( xp < 0 ) && ( yp < 0 ) )
-			return Math.PI + angle;
+			return Math.PI + angle ;
 
-		return Math.PI * 2. - angle;
+		return Math.PI * 2. - angle ;
 	}
 
 	public int getBaseX()
 	{
-		return _xb;
+		return _xb ;
 	}
 
 	public int getBaseY()
 	{
-		return _yb;
+		return _yb ;
 	}
 }
 
@@ -114,28 +119,28 @@ final class TpeScanAreaDragObject
  */
 public class TpeScanAreaFeature extends TpeImageFeature implements TpeDraggableFeature
 {
-	private TpeScanArea _scanArea;
-	private PolygonD _scanAreaPD;
-	private PolygonD _tickMarkPD;
-	private SpIterRasterObs _iterRaster;
-	private boolean _valid = false;
+	private TpeScanArea _scanArea ;
+	private PolygonD _scanAreaPD ;
+	private PolygonD _tickMarkPD ;
+	private SpIterRasterObs _iterRaster ;
+	private boolean _valid = false ;
 
 	/**
 	 * Indicates whether scan arae should be shifted or rotated.
 	 */
-	private boolean _shift = false;
+	private boolean _shift = false ;
 	
-	private TpeScanAreaDragObject _dragObject;
-	private boolean _dragging = false;
-	private int _dragX;
-	private int _dragY;
+	private TpeScanAreaDragObject _dragObject ;
+	private boolean _dragging = false ;
+	private int _dragX ;
+	private int _dragY ;
 
 	/**
 	 * Construct the feature with its name and description. 
 	 */
 	public TpeScanAreaFeature()
 	{
-		super( "Scan Area" , "Scan/Raster area." );
+		super( "Scan Area" , "Scan/Raster area." ) ;
 	}
 
 	/**
@@ -143,9 +148,9 @@ public class TpeScanAreaFeature extends TpeImageFeature implements TpeDraggableF
 	 */
 	public void reinit( TpeImageWidget iw , FitsImageInfo fii )
 	{
-		super.reinit( iw , fii );
-		_valid = false;
-		return;
+		super.reinit( iw , fii ) ;
+		_valid = false ;
+		return ;
 	}
 
 	/**
@@ -153,7 +158,7 @@ public class TpeScanAreaFeature extends TpeImageFeature implements TpeDraggableF
 	 */
 	public void posAngleUpdate( FitsImageInfo fii )
 	{
-		_valid = false;
+		_valid = false ;
 	}
 
 	/**
@@ -162,56 +167,63 @@ public class TpeScanAreaFeature extends TpeImageFeature implements TpeDraggableF
 	private boolean _calc( FitsImageInfo fii )
 	{
 		// Need the instrument to get the scan area
-		SpIterRasterObs iterRaster = ( SpIterRasterObs )_iw.getBaseItem();
+		SpIterRasterObs iterRaster = null ;
+		SpItem item = _iw.getBaseItem() ;
+		
+		Vector rasters = SpTreeMan.findAllItems( item , SpIterRasterObs.class.getName() ) ;
+		
+		if( rasters.size() == 1 )
+			iterRaster = ( SpIterRasterObs )rasters.elementAt( 0 ) ;
+
 		if( iterRaster == null )
-			return false;
+			return false ;
 
 		if( _scanArea == null )
 		{
-			_scanArea = new TpeScanArea();
+			_scanArea = new TpeScanArea() ;
 			if( _scanArea == null )
-				return false;
+				return false ;
 		}
 
-		boolean updated = _scanArea.update( iterRaster , fii );
+		boolean updated = _scanArea.update( iterRaster , fii ) ;
 
 		// Already have the current values
 		if( _valid && !updated && ( _scanAreaPD != null ) && ( iterRaster == _iterRaster ) )
-			return true;
+			return true ;
 
-		_iterRaster = iterRaster;
+		_iterRaster = iterRaster ;
 
-		double xBase = ( double )fii.baseScreenPos.x;
-		double yBase = ( double )fii.baseScreenPos.y;
-		_scanAreaPD = _scanArea.getPolygonDAt( xBase , yBase );
+		double xBase = ( double )fii.baseScreenPos.x ;
+		double yBase = ( double )fii.baseScreenPos.y ;
+		_scanAreaPD = _scanArea.getPolygonDAt( xBase , yBase ) ;
 
 		// Init the _tickMarkPD
 		if( _tickMarkPD == null )
 		{
-			double[] xpoints = new double[ 4 ];
-			double[] ypoints = new double[ 4 ];
-			_tickMarkPD = new PolygonD( xpoints , ypoints , 4 );
+			double[] xpoints = new double[ 4 ] ;
+			double[] ypoints = new double[ 4 ] ;
+			_tickMarkPD = new PolygonD( xpoints , ypoints , 4 ) ;
 		}
 
-		double x = xBase;
-		double y = yBase - _scanArea.height / 2.0;
+		double x = xBase ;
+		double y = yBase - _scanArea.height / 2.0 ;
 
-		_tickMarkPD.xpoints[ 0 ] = x;
-		_tickMarkPD.ypoints[ 0 ] = y - MARKER_SIZE * 2;
+		_tickMarkPD.xpoints[ 0 ] = x ;
+		_tickMarkPD.ypoints[ 0 ] = y - MARKER_SIZE * 2 ;
 
-		_tickMarkPD.xpoints[ 1 ] = x - MARKER_SIZE;
-		_tickMarkPD.ypoints[ 1 ] = y - 2;
+		_tickMarkPD.xpoints[ 1 ] = x - MARKER_SIZE ;
+		_tickMarkPD.ypoints[ 1 ] = y - 2 ;
 
-		_tickMarkPD.xpoints[ 2 ] = x + MARKER_SIZE;
-		_tickMarkPD.ypoints[ 2 ] = y - 2;
+		_tickMarkPD.xpoints[ 2 ] = x + MARKER_SIZE ;
+		_tickMarkPD.ypoints[ 2 ] = y - 2 ;
 
-		_tickMarkPD.xpoints[ 3 ] = _tickMarkPD.xpoints[ 0 ];
-		_tickMarkPD.ypoints[ 3 ] = _tickMarkPD.ypoints[ 0 ];
+		_tickMarkPD.xpoints[ 3 ] = _tickMarkPD.xpoints[ 0 ] ;
+		_tickMarkPD.ypoints[ 3 ] = _tickMarkPD.ypoints[ 0 ] ;
 
-		_iw.skyRotate( _tickMarkPD , _scanArea.posAngleRadians );
+		_iw.skyRotate( _tickMarkPD , _scanArea.posAngleRadians ) ;
 
-		_valid = true;
-		return true;
+		_valid = true ;
+		return true ;
 	}
 
 	/**
@@ -220,26 +232,26 @@ public class TpeScanAreaFeature extends TpeImageFeature implements TpeDraggableF
 	public void draw( Graphics g , FitsImageInfo fii )
 	{
 		if( !_calc( fii ) )
-			return;
+			return ;
 
-		g.setColor( Color.pink );
-		g.drawPolygon( _scanAreaPD.getAWTPolygon() );
-		g.fillPolygon( _tickMarkPD.getAWTPolygon() );
+		g.setColor( Color.pink ) ;
+		g.drawPolygon( _scanAreaPD.getAWTPolygon() ) ;
+		g.fillPolygon( _tickMarkPD.getAWTPolygon() ) ;
 
 		if( _dragging )
 		{
 			// Draw a little above the mouse
-			int baseX = _dragX;
-			int baseY = _dragY - 10;
+			int baseX = _dragX ;
+			int baseY = _dragY - 10 ;
 
 			// Draw a string displaying the rotation angle
-			String s = "";
+			String s = "" ;
 			if( _shift )
-				s = "" + _iterRaster.getWidth() + ", " + _iterRaster.getHeight();
+				s = "" + _iterRaster.getWidth() + ", " + _iterRaster.getHeight() ;
 			else
-				s = "" + _iterRaster.getPosAngle();
+				s = "" + _iterRaster.getPosAngle() ;
 			
-			DrawUtil.drawString( g , s , Color.pink , Color.black , baseX , baseY );
+			DrawUtil.drawString( g , s , Color.pink , Color.black , baseX , baseY ) ;
 		}
 	}
 
@@ -249,60 +261,60 @@ public class TpeScanAreaFeature extends TpeImageFeature implements TpeDraggableF
 	public boolean dragStart( FitsMouseEvent fme , FitsImageInfo fii )
 	{
 		if( ( _scanAreaPD == null ) || ( _tickMarkPD == null ) )
-			return false;
+			return false ;
 
-		_dragObject = null;
+		_dragObject = null ;
 
 		// See if dragging by the corner
 		for( int i = 0 ; i < ( _scanAreaPD.npoints - 1 ) ; ++i )
 		{
-			int cornerx = ( int )( _scanAreaPD.xpoints[ i ] + 0.5 );
-			int cornery = ( int )( _scanAreaPD.ypoints[ i ] + 0.5 );
+			int cornerx = ( int )( _scanAreaPD.xpoints[ i ] + 0.5 ) ;
+			int cornery = ( int )( _scanAreaPD.ypoints[ i ] + 0.5 ) ;
 
-			int dx = Math.abs( cornerx - fme.xWidget );
+			int dx = Math.abs( cornerx - fme.xWidget ) ;
 			if( dx > MARKER_SIZE )
-				continue;
+				continue ;
 
-			int dy = Math.abs( cornery - fme.yWidget );
+			int dy = Math.abs( cornery - fme.yWidget ) ;
 			if( dy > MARKER_SIZE )
-				continue;
+				continue ;
 
-			Point2D.Double p = fii.baseScreenPos;
-			_dragObject = new TpeScanAreaDragObject( ( int )p.x , ( int )p.y , cornerx , cornery );
+			Point2D.Double p = fii.baseScreenPos ;
+			_dragObject = new TpeScanAreaDragObject( ( int )p.x , ( int )p.y , cornerx , cornery ) ;
 
-			_shift = true;
+			_shift = true ;
 		}
 
 		// See if dragging by the tick mark (give a couple extra pixels to make it
 		// easier to grab)
 		if( _dragObject == null )
 		{
-			int x = ( int )( _tickMarkPD.xpoints[ 0 ] + 0.5 );
-			int dx = Math.abs( x - fme.xWidget );
+			int x = ( int )( _tickMarkPD.xpoints[ 0 ] + 0.5 ) ;
+			int dx = Math.abs( x - fme.xWidget ) ;
 			if( dx <= MARKER_SIZE + 2 )
 			{
-				int y0 = ( int )( _tickMarkPD.ypoints[ 0 ] + 0.5 );
-				int y1 = ( int )( _tickMarkPD.ypoints[ 1 ] + 0.5 );
-				int y = ( y0 + y1 ) / 2;
-				int dy = Math.abs( y - fme.yWidget );
+				int y0 = ( int )( _tickMarkPD.ypoints[ 0 ] + 0.5 ) ;
+				int y1 = ( int )( _tickMarkPD.ypoints[ 1 ] + 0.5 ) ;
+				int y = ( y0 + y1 ) / 2 ;
+				int dy = Math.abs( y - fme.yWidget ) ;
 				if( dy <= MARKER_SIZE + 2 )
 				{
-					Point2D.Double p = fii.baseScreenPos;
-					_dragObject = new TpeScanAreaDragObject( ( int )p.x , ( int )p.y , x , y );
+					Point2D.Double p = fii.baseScreenPos ;
+					_dragObject = new TpeScanAreaDragObject( ( int )p.x , ( int )p.y , x , y ) ;
 				}
 			}
 
-			_shift = false;
+			_shift = false ;
 		}
 
-		_dragging = ( _dragObject != null );
+		_dragging = ( _dragObject != null ) ;
 		if( _dragging )
 		{
-			_dragX = fme.xWidget;
-			_dragY = fme.yWidget;
-			_iterRaster.getAvEditFSM().setEachEditNotifies( false );
+			_dragX = fme.xWidget ;
+			_dragY = fme.yWidget ;
+			_iterRaster.getAvEditFSM().setEachEditNotifies( false ) ;
 		}
-		return _dragging;
+		return _dragging ;
 	}
 
 	/**
@@ -312,25 +324,25 @@ public class TpeScanAreaFeature extends TpeImageFeature implements TpeDraggableF
 	{
 		if( _dragObject != null )
 		{
-			_dragX = fme.xWidget;
-			_dragY = fme.yWidget;
+			_dragX = fme.xWidget ;
+			_dragY = fme.yWidget ;
 
 			if( _shift )
 			{
-				double pa = ( _iterRaster.getPosAngle() * Math.PI ) / 180.0;
-				double w = Math.abs( 2.0 * ( ( fme.xOffset * Math.cos( -pa ) ) + ( fme.yOffset * Math.sin( -pa ) ) ) );
-				double h = Math.abs( 2.0 * ( ( fme.xOffset * ( -Math.sin( -pa ) ) ) + ( fme.yOffset * Math.cos( -pa ) ) ) );
+				double pa = ( _iterRaster.getPosAngle() * Math.PI ) / 180.0 ;
+				double w = Math.abs( 2.0 * ( ( fme.xOffset * Math.cos( -pa ) ) + ( fme.yOffset * Math.sin( -pa ) ) ) ) ;
+				double h = Math.abs( 2.0 * ( ( fme.xOffset * ( -Math.sin( -pa ) ) ) + ( fme.yOffset * Math.cos( -pa ) ) ) ) ;
 
-				_iterRaster.setWidth( w );
-				_iterRaster.setHeight( h );
+				_iterRaster.setWidth( w ) ;
+				_iterRaster.setHeight( h ) ;
 			}
 			else
 			{
-				double diff = _dragObject.nextAngleDiff( fme.xWidget , fme.yWidget );
-				_iterRaster.setPosAngle( _iterRaster.getPosAngle() + ( ( diff * 180.0 ) / Math.PI ) );
+				double diff = _dragObject.nextAngleDiff( fme.xWidget , fme.yWidget ) ;
+				_iterRaster.setPosAngle( _iterRaster.getPosAngle() + ( ( diff * 180.0 ) / Math.PI ) ) ;
 			}
 
-			_iw.reset( _iterRaster );
+			_iw.reset( _iterRaster ) ;
 		}
 	}
 
@@ -341,10 +353,10 @@ public class TpeScanAreaFeature extends TpeImageFeature implements TpeDraggableF
 	{
 		if( _dragObject != null )
 		{
-			_iterRaster.getAvEditFSM().setEachEditNotifies( true );
-			_dragging = false;
-			drag( fme );
-			_dragObject = null;
+			_iterRaster.getAvEditFSM().setEachEditNotifies( true ) ;
+			_dragging = false ;
+			drag( fme ) ;
+			_dragObject = null ;
 		}
 	}
 }
