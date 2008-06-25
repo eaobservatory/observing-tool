6fa1b1b8bf8cd667e5bc1c99d5546a1b5da054da
Sam Hart <s.hart@jach.hawaii.edu>
2008-06-25 21:03:45 +0000

    Don't allow ACSIS obs to add the SCUBA-2 array test eye -> SH

--- OT/src/jsky/app/ot/OtTreeWidget.java
+++ OT/src/jsky/app/ot/OtTreeWidget.java
@@ -1054,7 +1054,7 @@ public final class OtTreeWidget extends MultiSelTreeWidget implements OtGuiAttri
 	private boolean canAddEyeToNonACSIS( SpItem target , SpItem[] items )
 	{
 		boolean canAdd = true ;
-		String[] eyes = { "dream" } ;
+		String[] eyes = { "dream" , "arrayTest" } ;
 		Class[] tabooClass = new Class[]{ SpInstHeterodyne.class } ;
 		for( int index = 0 ; index < eyes.length && canAdd ; index++ )
 		{
