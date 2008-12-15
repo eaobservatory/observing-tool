86d1761891bb18dd88f677c3cfef3756f4135621
Sam Hart <s.hart@jach.hawaii.edu>
2008-12-15 21:16:40 +0000

    Update noise for Heterodyne to match HITEC following emails from Ming and Per -> SH

--- ORAC/src/orac/jcmt/util/HeterodyneNoise.java
+++ ORAC/src/orac/jcmt/util/HeterodyneNoise.java
@@ -242,9 +242,9 @@ public class HeterodyneNoise
 		if( ssb )
 		{
 			if( "HARP".equals( fe ) || "WD".equals( fe ) )
-				tSys = ( getTrx( fe , freq ) + nuTel * tSky + tTel + 35 ) / ( nuSky * nuTel ) ;
+				tSys = ( getTrx( fe , freq ) + nuTel * tSky + tTel ) / ( nuSky * nuTel ) ;
 			else
-				tSys = ( 2. * getTrx( fe , freq ) + nuTel * tSky + tTel + 35 ) / ( nuSky * nuTel ) ;
+				tSys = ( 2. * getTrx( fe , freq ) + nuTel * tSky + tTel ) / ( nuSky * nuTel ) ;
 		}
 		else
 		{
