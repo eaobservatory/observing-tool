78a7d2eda30dba2d09b29fd0da98ac7e1a8118ba
Sam Hart <s.hart@jach.hawaii.edu>
2009-01-23 22:37:17 +0000

    Fix for 20090123.004, only check ??MMSS on base positions -> SH

--- ORAC/src/orac/jcmt/validation/JcmtSpValidation.java
+++ ORAC/src/orac/jcmt/validation/JcmtSpValidation.java
@@ -302,13 +302,16 @@ public class JcmtSpValidation extends SpValidation
 						}
 					}
 					else
-					{						
-		    			if( !HHMMSS.validate( pos.getXaxisAsString() ) )
-		    				report.add( new ErrorMessage( ErrorMessage.ERROR , "Telescope target " + pos.getName() + titleString , "RA" , "range 0:00:00 .. 24:00:00" , pos.getXaxisAsString() ) ) ;
-		    
-		    			if( !DDMMSS.validate( pos.getYaxisAsString() , -50 , 90 ) )
-		    				report.add( new ErrorMessage( ErrorMessage.ERROR , "Telescope target " + pos.getName() + titleString , "Dec" , "range -50:00:00 .. 90:00:00" , pos.getYaxisAsString() ) ) ;
-		    
+					{	
+						if( pos.isBasePosition() )
+						{
+    		    			if( !HHMMSS.validate( pos.getXaxisAsString() ) )
+    		    				report.add( new ErrorMessage( ErrorMessage.ERROR , "Telescope target " + pos.getName() + titleString , "RA" , "range 0:00:00 .. 24:00:00" , pos.getXaxisAsString() ) ) ;
+    		    
+    		    			if( !DDMMSS.validate( pos.getYaxisAsString() , -50 , 90 ) )
+    		    				report.add( new ErrorMessage( ErrorMessage.ERROR , "Telescope target " + pos.getName() + titleString , "Dec" , "range -50:00:00 .. 90:00:00" , pos.getYaxisAsString() ) ) ;
+						}
+
 		    			// checking whether both RA and Dec are 0:00:00
 		    			if( pos.getXaxis() == 0 && pos.getYaxis() == 0 )
 		    				report.add( new ErrorMessage( ErrorMessage.WARNING , "Telescope target " + pos.getName() + titleString , "Both Dec and RA are 0:00:00" ) ) ;
