e941ec76dd90665badb8a314570f3465a6672c12
Sam Hart <s.hart@jach.hawaii.edu>
2009-06-30 14:12:34 -1000

    Flat cannot be added to an ACSIS observation.

--- OT/src/jsky/app/ot/OtTreeWidget.java
+++ OT/src/jsky/app/ot/OtTreeWidget.java
@@ -1057,7 +1057,7 @@ public final class OtTreeWidget extends MultiSelTreeWidget implements OtGuiAttri
 	private boolean canAddEyeToNonACSIS( SpItem target , SpItem[] items )
 	{
 		boolean canAdd = true ;
-		String[] eyes = { "dream" , "arrayTest" , "noise" } ;
+		String[] eyes = { "dream" , "arrayTest" , "noise", "flat" } ;
 		Class[] tabooClass = new Class[]{ SpInstHeterodyne.class } ;
 		for( int index = 0 ; index < eyes.length && canAdd ; index++ )
 		{
