2fba416f5d449bed1f88233edcadc679146a3cd8
Sam Hart <s.hart@jach.hawaii.edu>
2009-08-04 15:54:55 -1000

    Update script that checks wether jars are signed or not to be useful.

--- releaseFiles/webstart/work/issign.pl
+++ releaseFiles/webstart/work/issign.pl
@@ -1,7 +1,41 @@
 @jars = `find . -name "*.jar"` ;
+@signed = () ;
+@unsigned = () ;
+$count = 0 ;
 foreach $jar ( @jars )
 {
+	print "\r" . $count++ . " of " . $#jars ;
 	chomp( $jar ) ;
-	print $jar . "\n" ;
-	system( 'jarsigner' , ( '-verify' , $jar ) ) ;
+	$output = `jarsigner -verify $jar` ;
+	if( $output =~ "missing" )
+	{
+		push( @unsigned , $jar ) ;
+	}
+	else
+	{
+		push( @signed , $jar ) ;
+	}
 }
+print "\n" . "-" x 20 . "\n" ;
+print "Unsigned jars\n" ;
+print "-" x 20 . "\n" ;
+foreach $entry ( @unsigned )
+{
+	print $entry . "\n" ;
+}
+print "-" x 20 . "\n" ;
+print "Signed jars\n" ;
+foreach $entry ( @signed )
+{
+	print "-" x 20 . "\n" ;
+        print $entry . "\n" ;
+	@verified = `jarsigner -verify -verbose -certs $entry` ;
+	foreach $line ( @verified )
+	{
+		chomp( $line ) ;
+		$line =~ /\s+X.509, CN=(.+),/ ;
+		if( defined $1 ){ $signer = $1 ; }
+	}
+	print $signer . "\n" ;
+}
+print "-" x 20 . "\n" ;
