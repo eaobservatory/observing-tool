234d56b19abbf6c814744264946c3b7a584dd897
Sam Hart <s.hart@jach.hawaii.edu>
2009-10-28 14:43:25 -1000

    Left over from SCUBA-2. (cherry picked from commit a9551dd4d3faa6550a1eb25d0f5774010f69e555)

--- OT/src/ot/jcmt/iter/editor/EdIterRasterObs.java
+++ OT/src/ot/jcmt/iter/editor/EdIterRasterObs.java
@@ -356,7 +356,6 @@ public final class EdIterRasterObs extends EdIterJCMTGeneric implements Observer
 		_w.scanningStrategies.setValue( strategy ) ;
 		
 		boolean pointSource = SCAN_PATTERN_POINT.equals( strategy ) ;
-		boolean allowScan = SCAN_PATTERN_BOUS.equals( strategy ) ;
 		
 		if( pointSource )
 			_w.pointSourceTime.setValue( _iterObs.getSampleTime() ) ;
@@ -364,9 +363,6 @@ public final class EdIterRasterObs extends EdIterJCMTGeneric implements Observer
 			_w.numberOfMapCycles.setValue( _iterObs.getIntegrations() ) ;
 		
 		_w.scanSpeed.setValue( _iterObs.getScanVelocity() ) ;
-		
-		_w.scanAngle.setEnabled( allowScan ) ;
-		_w.scanSystem.setEnabled( allowScan ) ;
 		
 		_w.mapCyclesPanel.setVisible( !pointSource ) ;
 		_w.pointSourcePanel.setVisible( pointSource ) ;
