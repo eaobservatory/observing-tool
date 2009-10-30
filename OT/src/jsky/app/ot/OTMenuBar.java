a1f1a63543c61f58ae467e3ca19e9052480a5ef1
Sam Hart <s.hart@jach.hawaii.edu>
2009-10-30 13:44:15 -1000

    Rejig menu items attempting to fix a bug, it didn't, but ...
    ... it has a smaller footprint and is noticibly quicker.

--- OT/src/jsky/app/ot/OTMenuBar.java
+++ OT/src/jsky/app/ot/OTMenuBar.java
@@ -16,7 +16,7 @@ import javax.swing.JComponent ;
 /** 
  * Implements the menubar for the OT application class. 
  */
-public class OTMenuBar extends JMenuBar
+public class OTMenuBar extends JMenuBar implements ActionListener
 {
 	/** Target class */
 	protected OT ot ;
@@ -29,6 +29,18 @@ public class OTMenuBar extends JMenuBar
 
 	/** Handle for the Help menu */
 	protected JMenu helpMenu ;
+
+	private static final String NEW_PROGRAM = "New Program" ;
+	private static final String NEW_PLAN = "New Plan" ;
+	private static final String NEW_LIBRARY = "New Library" ;
+	private static final String OPEN = "Open " ;
+	private static final String LIBRARY = " library" ;
+	private static final String PREFERENCES = "Preferences" ;
+	private static final String EXIT = "Exit" ;
+	private static final String FETCH_PROGRAM = "Fetch Program" ;
+	private static final String NEWS = "News ..." ;
+	private static final String ABOUT = "About ..." ;
+	private static final String OT_HELP = "OT Help ..." ;
 
 	/**
 	 * Create the menubar for the given JSkyCat instance
@@ -97,14 +109,8 @@ public class OTMenuBar extends JMenuBar
 	 */
 	protected JMenuItem createFileNewProgramMenuItem()
 	{
-		JMenuItem menuItem = new JMenuItem( "New Program" ) ;
-		menuItem.addActionListener( new ActionListener()
-		{
-			public void actionPerformed( ActionEvent ae )
-			{
-				OT.newProgram() ;
-			}
-		} ) ;
+		JMenuItem menuItem = new JMenuItem( NEW_PROGRAM ) ;
+		menuItem.addActionListener( this ) ;
 		return menuItem ;
 	}
 
@@ -113,14 +119,8 @@ public class OTMenuBar extends JMenuBar
 	 */
 	protected JMenuItem createFileNewPlanMenuItem()
 	{
-		JMenuItem menuItem = new JMenuItem( "New Plan" ) ;
-		menuItem.addActionListener( new ActionListener()
-		{
-			public void actionPerformed( ActionEvent ae )
-			{
-				ot.newPlan() ;
-			}
-		} ) ;
+		JMenuItem menuItem = new JMenuItem( NEW_PLAN ) ;
+		menuItem.addActionListener( this ) ;
 
 		// MFO 23 May 2001: "New Plan" menu item disabled.
 		menuItem.setEnabled( false ) ;
@@ -133,14 +133,8 @@ public class OTMenuBar extends JMenuBar
 	 */
 	protected JMenuItem createFileNewLibraryMenuItem()
 	{
-		JMenuItem menuItem = new JMenuItem( "New Library" ) ;
-		menuItem.addActionListener( new ActionListener()
-		{
-			public void actionPerformed( ActionEvent ae )
-			{
-				OT.newLibrary() ;
-			}
-		} ) ;
+		JMenuItem menuItem = new JMenuItem( NEW_LIBRARY ) ;
+		menuItem.addActionListener( this ) ;
 		return menuItem ;
 	}
 
@@ -149,14 +143,8 @@ public class OTMenuBar extends JMenuBar
 	 */
 	protected JMenuItem createFileOpenMenuItem()
 	{
-		JMenuItem menuItem = new JMenuItem( "Open" ) ;
-		menuItem.addActionListener( new ActionListener()
-		{
-			public void actionPerformed( ActionEvent ae )
-			{
-				OT.open() ;
-			}
-		} ) ;
+		JMenuItem menuItem = new JMenuItem( OPEN ) ;
+		menuItem.addActionListener( this ) ;
 		return menuItem ;
 	}
 
@@ -179,14 +167,8 @@ public class OTMenuBar extends JMenuBar
 				menuItems[ i ] = null ;
 				continue ;
 			}
-			menuItems[ i ] = new JMenuItem( "Open " + libs[ i ] + " library" ) ;
-			menuItems[ i ].addActionListener( new ActionListener()
-			{
-				public void actionPerformed( ActionEvent e )
-				{
-					ot.openLibrary( libname + ".xml" ) ;
-				}
-			} ) ;
+			menuItems[ i ] = new JMenuItem( OPEN + libs[ i ] + LIBRARY ) ;
+			menuItems[ i ].addActionListener( this ) ;
 		}
 
 		return menuItems ;
@@ -197,16 +179,8 @@ public class OTMenuBar extends JMenuBar
 	 */
 	protected JMenuItem createFilePreferencesMenuItem()
 	{
-		JMenuItem menuItem = new JMenuItem( "Preferences" ) ;
-		menuItem.addActionListener( new ActionListener()
-		{
-			public void actionPerformed( ActionEvent ae )
-			{
-				// I suspect it should be possible to define OT.preferences() as non-static method and use ot.preferences(). 
-				// But it did cause an java.lang.IncompatibleClassChangeError in the past.
-				OT.preferences() ;
-			}
-		} ) ;
+		JMenuItem menuItem = new JMenuItem( PREFERENCES ) ;
+		menuItem.addActionListener( this ) ;
 		return menuItem ;
 	}
 
@@ -215,16 +189,8 @@ public class OTMenuBar extends JMenuBar
 	 */
 	protected JMenuItem createFileExitMenuItem()
 	{
-		JMenuItem menuItem = new JMenuItem( "Exit" ) ;
-		menuItem.addActionListener( new ActionListener()
-		{
-			public void actionPerformed( ActionEvent ae )
-			{
-				// I suspect it should be possible to define OT.exit() as non-static method and use ot.exit(). 
-				// But it did cause an java.lang.IncompatibleClassChangeError in the past.
-				OT.exit() ;
-			}
-		} ) ;
+		JMenuItem menuItem = new JMenuItem( EXIT ) ;
+		menuItem.addActionListener( this ) ;
 		return menuItem ;
 	}
 
@@ -233,14 +199,8 @@ public class OTMenuBar extends JMenuBar
 	 */
 	protected JMenuItem createObservingDatabaseFetchProgramMenuItem()
 	{
-		JMenuItem menuItem = new JMenuItem( "Fetch Program" ) ;
-		menuItem.addActionListener( new ActionListener()
-		{
-			public void actionPerformed( ActionEvent ae )
-			{
-				OT.fetchProgram() ;
-			}
-		} ) ;
+		JMenuItem menuItem = new JMenuItem( FETCH_PROGRAM ) ;
+		menuItem.addActionListener( this ) ;
 		return menuItem ;
 	}
 
@@ -249,14 +209,8 @@ public class OTMenuBar extends JMenuBar
 	 */
 	protected JMenuItem createHelpNewsMenuItem()
 	{
-		JMenuItem menuItem = new JMenuItem( "News ..." ) ;
-		menuItem.addActionListener( new ActionListener()
-		{
-			public void actionPerformed( ActionEvent ae )
-			{
-				OT.showNews() ;
-			}
-		} ) ;
+		JMenuItem menuItem = new JMenuItem( NEWS ) ;
+		menuItem.addActionListener( this ) ;
 		return menuItem ;
 	}
 
@@ -265,14 +219,8 @@ public class OTMenuBar extends JMenuBar
 	 */
 	protected JMenuItem createHelpAboutMenuItem()
 	{
-		JMenuItem menuItem = new JMenuItem( "About ..." ) ;
-		menuItem.addActionListener( new ActionListener()
-		{
-			public void actionPerformed( ActionEvent ae )
-			{
-				OT.showSplashScreen() ;
-			}
-		} ) ;
+		JMenuItem menuItem = new JMenuItem( ABOUT ) ;
+		menuItem.addActionListener( this ) ;
 		return menuItem ;
 	}
 
@@ -281,14 +229,35 @@ public class OTMenuBar extends JMenuBar
 	 */
 	protected JMenuItem createHelpOtHelpMenuItem()
 	{
-		JMenuItem menuItem = new JMenuItem( "OT Help ..." ) ;
-		menuItem.addActionListener( new ActionListener()
-		{
-			public void actionPerformed( ActionEvent ae )
-			{
-				OT.launchHelp() ;
-			}
-		} ) ;
+		JMenuItem menuItem = new JMenuItem( OT_HELP ) ;
+		menuItem.addActionListener( this ) ;
 		return menuItem ;
+	}
+
+	public void actionPerformed( ActionEvent ae )
+	{
+		String actionCommand = ae.getActionCommand() ;
+		if( OT_HELP.equals( actionCommand ) )
+			OT.launchHelp() ;
+		else if( ABOUT.equals( actionCommand ) )
+			OT.showSplashScreen() ;
+		else if( NEWS.equals( actionCommand ) )
+			OT.showNews() ;
+		else if( FETCH_PROGRAM.equals( actionCommand ) )
+			OT.fetchProgram() ;
+		else if( EXIT.equals( actionCommand ) )
+			OT.exit() ;
+		else if( PREFERENCES.equals( actionCommand ) )
+			OT.preferences() ;
+		else if( actionCommand.startsWith( OPEN ) && actionCommand.endsWith( LIBRARY ) )
+			ot.openLibrary( actionCommand.substring( OPEN.length() , actionCommand.length() - LIBRARY.length() ) + ".xml" ) ;
+		else if( actionCommand.startsWith( OPEN ) && !actionCommand.endsWith( LIBRARY ) )
+			OT.open() ;
+		else if( NEW_LIBRARY.equals( actionCommand ) )
+			OT.newLibrary() ;
+		else if( NEW_PLAN.equals( actionCommand ) )
+			ot.newPlan() ;
+		else if( NEW_PROGRAM.endsWith( actionCommand ) )
+			OT.newProgram() ;
 	}
 }
