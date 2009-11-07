31e16f54728d3a44082e873c0f7b10230db4b830
Sam Hart <s.hart@jach.hawaii.edu>
2009-11-06 15:51:24 -1000

    Whitespace.

--- OT/src/jsky/app/ot/editor/EdIteratorFolder.java
+++ OT/src/jsky/app/ot/editor/EdIteratorFolder.java
@@ -10,10 +10,10 @@ import java.awt.event.ActionEvent ;
 import java.awt.event.ActionListener ;
 
 import java.util.List ;
-import java.util.Vector;
+import java.util.Vector ;
 
 import gemini.sp.iter.SpIterFolder ;
-import gemini.sp.iter.SpIterStep;
+import gemini.sp.iter.SpIterStep ;
 
 /**
  * This is the editor for the "iterator folder" or "Sequence" item.
