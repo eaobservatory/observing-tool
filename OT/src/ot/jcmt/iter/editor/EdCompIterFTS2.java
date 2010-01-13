81a135d02efbe8223cf41bef8aff7a89c218d181
Sam Hart <s.hart@jach.hawaii.edu>
2010-01-12 16:28:03 -1000

    Add FTS-2 as an eye. (cherry picked from commit 795387432ef5c7ad719ce7330715d157366c18a4)

---
+++ OT/src/ot/jcmt/iter/editor/EdCompIterFTS2.java
@@ -0,0 +1,83 @@
+// $Id$
+
+package ot.jcmt.iter.editor ;
+
+import java.awt.event.ActionListener ;
+import java.awt.event.ActionEvent ;
+
+import gemini.sp.SpItem ;
+import orac.jcmt.iter.SpIterFTS2 ;
+import jsky.app.ot.editor.OtItemEditor ;
+import jsky.app.ot.gui.DropDownListBoxWidgetExt;
+
+public final class EdCompIterFTS2 extends OtItemEditor implements ActionListener
+{
+	private SpIterFTS2 _inst ;
+
+	private FTS2GUI _w ;
+
+	public EdCompIterFTS2()
+	{
+		_title = "JCMT FTS-2" ;
+		_presSource = _w = new FTS2GUI() ;
+		_description = "FTS-2" ;
+		_w.specialModes.setChoices( SpIterFTS2.SPECIAL_MODES ) ;
+		
+		_w.specialModes.addActionListener( this ) ;
+		_w.dual.addActionListener( this ) ;
+		_w.single.addActionListener( this ) ;
+		_w.port1.addActionListener( this ) ;
+		_w.port2.addActionListener( this ) ;
+	}
+
+	/**
+	 * Override setup to store away a reference to the SpInstSCUBA2 item.
+	 */
+	public void setup( SpItem spItem )
+	{
+		_inst = ( SpIterFTS2 )spItem ;
+		super.setup( spItem ) ;
+	}
+
+	/**
+	 * Implements the _updateWidgets method from OtItemEditor in order to
+	 * setup the widgets to show the current values of the item.
+	 */
+	protected void _updateWidgets()
+	{
+		_w.specialModes.setSelectedItem( _inst.getSpecialMode() ) ;
+		boolean isPort1 = _inst.getTrackingPort() == 1 ;
+		_w.port1.setSelected( isPort1 ) ;
+		_w.port2.setSelected( !isPort1 ) ;
+		boolean isDualPort = _inst.isDualPort() ;
+		_w.dual.setSelected( isDualPort ) ;
+		_w.single.setSelected( !isDualPort ) ;
+	}
+
+    public void actionPerformed( ActionEvent e )
+    {
+	    Object source = e.getSource() ;
+	    if( _w.specialModes.equals( source ) )
+	    {
+	    	Object item = (( DropDownListBoxWidgetExt )source).getSelectedItem() ;
+	    	if( item instanceof String )
+	    		_inst.setSpecialMode( ( String )item ) ;
+	    }
+	    else if( _w.dual.equals( source ) )
+	    {
+	    	_inst.setIsDualPort( true ) ;
+	    }
+	    else if( _w.single.equals( source ) )
+	    {
+	    	_inst.setIsDualPort( false ) ;
+	    }
+	    else if( _w.port1.equals( source ) )
+	    {
+	    	_inst.setTrackingPort( 1 ) ;
+	    }
+	    else if( _w.port2.equals( source ) )
+	    {
+	    	_inst.setTrackingPort( 2 ) ;
+	    }    
+    }
+}
