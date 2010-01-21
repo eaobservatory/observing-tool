56768c8d0117462545afd9d61da984b6b70f0a2d
Sam Hart <s.hart@jach.hawaii.edu>
2010-01-21 08:34:32 -1000

    Remove unused methods.

--- OT/src/jsky/app/ot/util/Assert.java
+++ OT/src/jsky/app/ot/util/Assert.java
@@ -26,16 +26,4 @@ public class Assert
 		if( obj == null )
 			throw new IllegalArgumentException( "null argument" ) ;
 	}
-
-	static public void notFalse( boolean b , String s ) throws IllegalArgumentException
-	{
-		if( b == false )
-			throw new IllegalArgumentException( s ) ;
-	}
-
-	static public void notNull( Object obj , String s ) throws IllegalArgumentException
-	{
-		if( obj == null )
-			throw new IllegalArgumentException( s ) ;
-	}
 }
