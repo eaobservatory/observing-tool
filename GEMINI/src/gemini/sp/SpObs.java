39d0935e6ce1a809b3ded18949994534d0ab2a0c
Sam Hart <s.hart@jach.hawaii.edu>
2010-02-01 14:46:20 -1000

    Better wording.

--- GEMINI/src/gemini/sp/SpObs.java
+++ GEMINI/src/gemini/sp/SpObs.java
@@ -542,7 +542,7 @@ public class SpObs extends SpMSB implements SpTranslatable , SpTranslationConsta
 		{
 			logger.error( "DR Recipes are missing from this observation." ) ;
 			String[] options = { "Continue translating" , "Abort translation" } ;
-			int choice = javax.swing.JOptionPane.showOptionDialog( null , "No DR Recipes have been found for this observation. This is probably an error, the QT should be restarted and a fault filed." , "Translation Error" , javax.swing.JOptionPane.ERROR_MESSAGE , javax.swing.JOptionPane.YES_NO_OPTION , null , options , options[ 0 ] ) ;		
+			int choice = javax.swing.JOptionPane.showOptionDialog( null , "No DR Recipes have been found for this observation. This is probably an error, the software should be restarted and a fault filed." , "Translation Error" , javax.swing.JOptionPane.ERROR_MESSAGE , javax.swing.JOptionPane.YES_NO_OPTION , null , options , options[ 0 ] ) ;		
 			if( choice == 1 )
 				return ;
 		}
