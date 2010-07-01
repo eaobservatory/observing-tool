80269f2b24f897ca24e579213c152518eedbbdc0
Sam Hart <s.hart@jach.hawaii.edu>
2010-07-01 11:40:21 -1000

    Initial update for FTS-2 based on requirements document.

--- OT/src/ot/jcmt/iter/editor/EdIterFTS2Obs.java
+++ OT/src/ot/jcmt/iter/editor/EdIterFTS2Obs.java
@@ -1,16 +1,18 @@
-// $Id$
-
 package ot.jcmt.iter.editor ;
 
 import java.awt.event.ActionListener ;
 import java.awt.event.ActionEvent ;
 
+import javax.swing.event.ChangeEvent ;
+import javax.swing.event.ChangeListener ;
+
 import gemini.sp.SpItem ;
+import gemini.util.MathUtil ;
 import orac.jcmt.iter.SpIterFTS2 ;
 import jsky.app.ot.editor.OtItemEditor ;
-import jsky.app.ot.gui.DropDownListBoxWidgetExt;
+import jsky.app.ot.gui.DropDownListBoxWidgetExt ;
 
-public final class EdIterFTS2Obs extends OtItemEditor implements ActionListener
+public final class EdIterFTS2Obs extends OtItemEditor implements ActionListener , ChangeListener
 {
 	private SpIterFTS2 _inst ;
 
@@ -28,6 +30,15 @@ public final class EdIterFTS2Obs extends OtItemEditor implements ActionListener
 		_w.single.addActionListener( this ) ;
 		_w.port1.addActionListener( this ) ;
 		_w.port2.addActionListener( this ) ;
+
+		_w.resolutionFOV.setMinimum( 56 ) ;
+		_w.resolutionFOV.setMaximum( 900 ) ;
+		_w.resolutionFOV.addChangeListener( this ) ;
+
+		_w.scanSpeedNyquist.setMinimum( 40 ) ;
+		_w.scanSpeedNyquist.setMaximum( 800 ) ;
+		_w.scanSpeedNyquist.setMajorTickSpacing( 10 ) ;
+		_w.scanSpeedNyquist.addChangeListener( this ) ;
 	}
 
 	/**
@@ -52,6 +63,15 @@ public final class EdIterFTS2Obs extends OtItemEditor implements ActionListener
 		boolean isDualPort = _inst.isDualPort() ;
 		_w.dual.setSelected( isDualPort ) ;
 		_w.single.setSelected( !isDualPort ) ;
+
+		double fov = _inst.getFOV() ;
+		double speed = _inst.getScanSpeed() ;
+		_w.resolutionFOV.setValue( ( int )( fov * 100 ) ) ;
+		_w.scanSpeedNyquist.setValue( ( int )( speed * 100 ) ) ;
+		_w.FOV.setText( "" + fov ) ;
+		_w.resolution.setText( "" + MathUtil.round( _inst.getResolution() , 4 ) ) ;
+		_w.scanSpeed.setText( "" + speed ) ;
+		_w.nyquist.setText( "" + MathUtil.round( _inst.getNyquist() , 4 ) ) ;
 	}
 
     public void actionPerformed( ActionEvent e )
@@ -61,7 +81,7 @@ public final class EdIterFTS2Obs extends OtItemEditor implements ActionListener
 	    {
 	    	Object item = (( DropDownListBoxWidgetExt )source).getSelectedItem() ;
 	    	if( item instanceof String )
-	    		_inst.setSpecialMode( ( String )item ) ;
+			_inst.setSpecialMode( ( String )item ) ;
 	    }
 	    else if( _w.dual.equals( source ) )
 	    {
@@ -79,5 +99,24 @@ public final class EdIterFTS2Obs extends OtItemEditor implements ActionListener
 	    {
 	    	_inst.setTrackingPort( 2 ) ;
 	    }    
+    }
+
+    public void stateChanged( ChangeEvent e )
+    {
+	Object source = e.getSource() ;
+	if( source == _w.resolutionFOV )
+	{
+		int fov = _w.resolutionFOV.getValue() ;
+		double fieldOfView = fov / 100. ;
+		_inst.setFOV( fieldOfView ) ;
+		_updateWidgets() ;
+	}
+	else if( source == _w.scanSpeedNyquist )
+	{
+		int scanSpeed = _w.scanSpeedNyquist.getValue() ;
+		double speed = scanSpeed / 100. ;
+		_inst.setScanSpeed( speed ) ;
+		_updateWidgets() ;
+	}
     }
 }
