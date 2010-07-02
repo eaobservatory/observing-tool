a7593c4634e44a7a3715c58ece6610fcbe7d7f1c
Sam Hart <s.hart@jach.hawaii.edu>
2010-07-01 14:54:12 -1000

    Output in mm/s because that is what FTS-2 uses internally. (cherry picked from commit 35991c262f37e63bb395c5483277f85181075e3d)

--- ORAC/src/orac/jcmt/iter/SpIterFTS2.java
+++ ORAC/src/orac/jcmt/iter/SpIterFTS2.java
@@ -2,6 +2,7 @@ package orac.jcmt.iter ;
 
 import gemini.sp.SpFactory ;
 import gemini.sp.SpType ;
+import gemini.util.MathUtil ;
 
 public class SpIterFTS2 extends SpIterJCMTObs
 {
@@ -96,11 +97,14 @@ public class SpIterFTS2 extends SpIterJCMTObs
 
     public double getScanSpeed()
     {
-	return _avTable.getDouble( SCAN_SPEED , 0.4 ) ;
+	double scanSpeed = _avTable.getDouble( SCAN_SPEED , 0.4 ) ;
+	scanSpeed = MathUtil.round( scanSpeed / 2.5 , 5 ) ;
+	return scanSpeed ;
     }
 
     public void setScanSpeed( double scanSpeed )
     {
+	scanSpeed *= 2.5 ;
 	_avTable.set( SCAN_SPEED , scanSpeed ) ;
     }
 
