7b4f5caa291dda380947eb5b52e71ed443f6cb40
Graham Bell <g.bell@jach.hawaii.edu>
2013-10-25 12:15:45 -1000

    Revert "Slight restructuring, retaining logic."
    This reverts commit 1fdc67f8a1512db01a7f4ce673a4a54a1008e2f6.

--- ORAC/src/orac/helptool/JHLauncher.java
+++ ORAC/src/orac/helptool/JHLauncher.java
@@ -118,19 +118,20 @@ final public class JHLauncher implements java.io.Serializable
 
 	protected void initialize( URL url , ClassLoader loader )
 	{
-		/* The JavaHelp can't be added to a BorderLayout because it
-		 * isnt' a component. For this demo we'll use the embeded method
-		 * since we don't want a Frame to be created.
-		 */
 		try
 		{
 			hs = new HelpSet( loader , url ) ;
-			jh = new JHelp( hs ) ;
 		}
 		catch( Exception ee )
 		{
 			JOptionPane.showMessageDialog( null , "HelpSet not found" , "Error" , JOptionPane.ERROR_MESSAGE ) ;
+			return ;
 		}
+		// The JavaHelp can't be added to a BorderLayout because it
+		// isnt' a component. For this demo we'll use the embeded method
+		// since we don't want a Frame to be created.
+
+		jh = new JHelp( hs ) ;
 	}
 
 	private class OpenPageListener implements ActionListener
@@ -301,6 +302,7 @@ final public class JHLauncher implements java.io.Serializable
 
 		private void initSetFontComponents()
 		{
+
 			Border border ;
 			DefaultMutableTreeNode topNode = new DefaultMutableTreeNode() ;
 			Font[] fonts ;
@@ -421,20 +423,20 @@ final public class JHLauncher implements java.io.Serializable
 
 			TreePath path = fontTree.getSelectionPath() ;
 			// If the path is null then the selection has been removed.
-			if( path != null )
+			if( path == null )
+				return ;
+
+			DefaultMutableTreeNode node = ( DefaultMutableTreeNode )path.getLastPathComponent() ;
+
+			// only get set the font "Font" objects
+			Object obj = node.getUserObject() ;
+			if( obj instanceof MyFont )
 			{
-				DefaultMutableTreeNode node = ( DefaultMutableTreeNode )path.getLastPathComponent() ;
+				MyFont myf = ( MyFont )obj ;
+				String size = ( String )cb.getSelectedItem() ;
+				font = myf.getFont().deriveFont( Integer.valueOf( size ).floatValue() ) ;
 
-				// only get set the font "Font" objects
-				Object obj = node.getUserObject() ;
-				if( obj instanceof MyFont )
-				{
-					MyFont myf = ( MyFont )obj ;
-					String size = ( String )cb.getSelectedItem() ;
-					font = myf.getFont().deriveFont( Integer.valueOf( size ).floatValue() ) ;
-
-					preview.setFont( font ) ;
-				}
+				preview.setFont( font ) ;
 			}
 		}
 
@@ -482,31 +484,35 @@ final public class JHLauncher implements java.io.Serializable
 	 */
 	class ShowElementTreeListener implements ActionListener
 	{
+
 		public void actionPerformed( ActionEvent e )
 		{
 			elementTreeFrame = null ;
-			// Create a frame containing an instance of ElementTreePanel.
-			try
+			if( elementTreeFrame == null )
 			{
-				String title = "Element Tree For Current Document" ;
-				elementTreeFrame = new JFrame( title ) ;
-			}
-			catch( MissingResourceException mre )
-			{
-				elementTreeFrame = new JFrame() ;
-			}
-
-			elementTreeFrame.addWindowListener( new WindowAdapter()
-			{
-				public void windowClosing( WindowEvent weeee )
+				// Create a frame containing an instance of ElementTreePanel.
+				try
 				{
-					elementTreeFrame.setVisible( false ) ;
+					String title = "Element Tree For Current Document" ;
+					elementTreeFrame = new JFrame( title ) ;
 				}
-			} ) ;
+				catch( MissingResourceException mre )
+				{
+					elementTreeFrame = new JFrame() ;
+				}
 
-			elementTreeFrame.setLayout( new BorderLayout() ) ;
-			elementTreeFrame.add( new ElementTreePanel( getEditor() ) ) ;
-			elementTreeFrame.pack() ;
+				elementTreeFrame.addWindowListener( new WindowAdapter()
+				{
+					public void windowClosing( WindowEvent weeee )
+					{
+						elementTreeFrame.setVisible( false ) ;
+					}
+				} ) ;
+
+				elementTreeFrame.setLayout( new BorderLayout() ) ;
+				elementTreeFrame.add( new ElementTreePanel( getEditor() ) ) ;
+				elementTreeFrame.pack() ;
+			}
 			elementTreeFrame.setVisible( true ) ;
 		}
 	}
@@ -791,22 +797,20 @@ final public class JHLauncher implements java.io.Serializable
 	// Added by MFO (April 25, 2002)
 	public void setup( URL hsURL )
 	{
-		if( hsURL != null )
-		{
-			initialize( hsURL , getClass().getClassLoader() ) ;
+		if( hsURL == null )
+			return ;
 
-			if( hs == null )
-			{
-				setHS = true ;
-				initializeSDGUI() ;
-				showSD() ;
-			}
-			else
-			{
-				createFrame( null , null ) ; // defaults
-				launch() ;
-			}
+		initialize( hsURL , getClass().getClassLoader() ) ;
+
+		if( hs == null )
+		{
+			setHS = true ;
+			initializeSDGUI() ;
+			showSD() ;
+			return ;
 		}
+		createFrame( null , null ) ; // defaults
+		launch() ;
 	}
 
 	// selection dialog code
