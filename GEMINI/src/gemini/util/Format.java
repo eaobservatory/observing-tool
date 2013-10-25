11a1bd20a6e5304695d85f65f058b8f6ccc904bd
Graham Bell <g.bell@jach.hawaii.edu>
2013-10-25 09:24:44 -1000

    Update authorship of gemini.util.Format
    It appears that all the original code was deleted from this
    class on 20 Jan 2010.  All that remain are the two methods
    added by Martin Folger on 28 Feb 2002.  I have attempted to
    update the header information accordingly.

--- GEMINI/src/gemini/util/Format.java
+++ GEMINI/src/gemini/util/Format.java
@@ -1,32 +1,11 @@
 // Copyright 1997 Association for Universities for Research in Astronomy, Inc.,
 // Observatory Control System, Gemini Telescopes Project.
 // See the file COPYRIGHT for complete details.
-//
-// $Id$
-//
-/*
- * Gary Cornell and Cay S. Horstmann, Core Java (Book/CD-ROM) Published By
- * SunSoft Press/Prentice-Hall Copyright (C) 1996 Sun Microsystems Inc. All
- * Rights Reserved. ISBN 0-13-565755-5
- * 
- * Permission to use, copy, modify, and distribute this software and its
- * documentation for NON-COMMERCIAL purposes and without fee is hereby granted
- * provided that this copyright notice appears in all copies.
- * 
- * THE AUTHORS AND PUBLISHER MAKE NO REPRESENTATIONS OR WARRANTIES ABOUT THE
- * SUITABILITY OF THE SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT
- * LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
- * PARTICULAR PURPOSE, OR NON-INFRINGEMENT. THE AUTHORS AND PUBLISHER SHALL NOT
- * BE LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING,
- * MODIFYING OR DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES.
- */
 
 /**
- * A class for formatting numbers that follows printf conventions. Also
- * implements C-like atoi and atof functions
+ * A class for parsing numbers which traps exceptions.
  * 
- * @version 1.01 15 Feb 1996
- * @author Cay Horstmann
+ * @author Martin Folger
  */
 package gemini.util ;
 
